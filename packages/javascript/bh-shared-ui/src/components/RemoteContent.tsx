4906eb24c8e136398529eb44b6591d3d2b9f6ba4
Ulises Rangel <urangel@specterops.io>
2023-08-08 17:33:53 -0500

    fix: patch the src in the img tag for remote content

--- packages/javascript/bh-shared-ui/src/components/RemoteContent.tsx
+++ packages/javascript/bh-shared-ui/src/components/RemoteContent.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import { Skeleton } from '@mui/material';
@@ -80,7 +80,7 @@ const COMPONENTS = {
     ),
     em: ({ node, ...props }: any) => <em {...props} />,
     hr: ({ node, ...props }: any) => <Divider {...props} />,
-    img: ({ node, ...props }: any) => <img {...props} alt={props.alt} src={props.src} style={{ maxWidth: '100%' }} />,
+    img: ({ node, ...props }: any) => <img {...props} alt={props.alt} src={`${import.meta.env.BASE_URL}/${props.src}`} style={{ maxWidth: '100%' }} />,
     li: ({ node, ordered, ...props }: any) => <li {...props} />,
     ol: ({ node, ordered, ...props }: any) => (
         <ol
