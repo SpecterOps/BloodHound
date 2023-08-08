38b1ee566faf7b3ac13e515bc5cc2a0484939af9
John Hopper <jhopper@specterops.io>
2023-08-08 13:50:26 -0700

    fix: add a guard statement for the asset group selector table unique constraint (#7)

--- cmd/api/src/database/migration/gorm.go
+++ cmd/api/src/database/migration/gorm.go
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 package migration
@@ -69,6 +69,10 @@ func ListBHModels() []any {
 func (s *Migrator) gormAutoMigrate(models []any) error {
 	return s.db.Transaction(func(tx *gorm.DB) error {
 		for _, currentModel := range models {
+			if result := tx.Exec(`alter table asset_group_selectors drop constraint if exists idx_asset_group_selectors_name;`); result.Error != nil {
+				return result.Error
+			}
+
 			if err := tx.Migrator().AutoMigrate(currentModel); err != nil {
 				return fmt.Errorf("failed to migrate model %T: %w", currentModel, err)
 			}
