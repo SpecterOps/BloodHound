df7780058e0d4bfd0fbc8a32d046c5921cf326ca
Ulises Rangel <urangel@specterops.io>
2023-08-08 14:47:42 -0500

    feat: make the panes displayed above the canvas responsive so they don't overlap

--- cmd/ui/src/views/Explore/InfoStyles/Pane.tsx
+++ cmd/ui/src/views/Explore/InfoStyles/Pane.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import makeStyles from '@mui/styles/makeStyles';
@@ -19,17 +19,11 @@ import { Theme } from '@mui/material';
 
 const usePaneStyles = makeStyles((theme: Theme) => ({
     container: {
-        bottom: 0,
         display: 'flex',
         flexDirection: 'column',
-        marginBottom: theme.spacing(2),
-        marginTop: theme.spacing(2),
-        maxWidth: theme.spacing(50),
         pointerEvents: 'none',
-        position: 'absolute',
-        right: theme.spacing(2),
-        top: 0,
-        width: theme.spacing(50),
+        margin: theme.spacing(0, 4, 2, 2),
+        maxHeight: '95%',
     },
     headerPaperRoot: {
         backgroundColor: theme.palette.background.paper,
@@ -45,6 +39,7 @@ const usePaneStyles = makeStyles((theme: Theme) => ({
         '& > div.node:nth-child(odd)': {
             background: '#eee',
         },
+        scrollbarGutter: 'stable',
     },
     breadcrumbs: {
         margin: '6px 10px 6px 10px',
