294dab1f72fb3fcbaf7d010fd7ee9301f6ba78fe
Ulises Rangel <uli.atx@gmail.com>
2023-08-08 16:54:36 -0500

    feat: make the panes displayed above the canvas responsive so they don't overlap (#6)

--- cmd/ui/src/views/Explore/ExploreSearchCombobox/ExploreSearchCombobox.tsx
+++ cmd/ui/src/views/Explore/ExploreSearchCombobox/ExploreSearchCombobox.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import { List, ListItem, ListItemText, Paper, TextField, useTheme } from '@mui/material';
@@ -103,7 +103,6 @@ const ExploreSearchCombobox: React.FC<{
                 style={{
                     position: 'absolute',
                     marginTop: '1rem',
-                    minWidth: '360px',
                     zIndex: 1300,
                 }}>
                 <Paper style={{ display: isOpen ? 'inherit' : 'none' }}>
