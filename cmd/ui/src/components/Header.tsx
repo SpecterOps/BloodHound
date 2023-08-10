6c0b40da9553f8d30c6e19dace3988aa32dcbcca
Ulises Rangel <urangel@specterops.io>
2023-08-10 10:52:00 -0500

    Remote content img src fix (#8)
    * fix: patch the src in the img tag for remote content
    
    * fix: revert changes to RemoteContent and instead update base property for url in vite config
    
    * fix: handle urls in remotecontent only available during run time
    
    * fix: remove extra slash in concatenation

--- cmd/ui/src/components/Header.tsx
+++ cmd/ui/src/components/Header.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import { faCog, faProjectDiagram } from '@fortawesome/free-solid-svg-icons';
@@ -124,7 +124,7 @@ const Header: React.FC = () => {
                 <Box height='100%' paddingY='6px' boxSizing='border-box'>
                     <Link component={RouterLink} to={routes.ROUTE_HOME} data-testid='global_header_nav-home'>
                         <img
-                            src={`${import.meta.env.BASE_URL}/img/logo-transparent-banner.svg`}
+                            src={'img/logo-transparent-banner.svg'}
                             alt='BloodHound CE Home'
                             style={{
                                 height: '100%',
