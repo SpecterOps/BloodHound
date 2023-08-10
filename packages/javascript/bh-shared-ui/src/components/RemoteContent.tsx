27ab99a47725588818e1e839ebb4eec94ea05461
Ulises Rangel <urangel@specterops.io>
2023-08-10 09:59:40 -0500

    fix: handle urls in remotecontent only available during run time

--- packages/javascript/bh-shared-ui/src/components/RemoteContent.tsx
+++ packages/javascript/bh-shared-ui/src/components/RemoteContent.tsx
@@ -20,108 +20,116 @@ import DOMPurify from 'dompurify';
 import { Divider, Link, Typography } from '@mui/material';
 import ReactMarkdown from 'react-markdown';
 
-const COMPONENTS = {
-    h1: ({ children }: any) => {
-        return (
-            <Typography variant='h1' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    h2: ({ children }: any) => {
-        return (
-            <Typography variant='h2' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    h3: ({ children }: any) => {
-        return (
-            <Typography variant='h3' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    h4: ({ children }: any) => {
-        return (
-            <Typography variant='h4' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    h5: ({ children }: any) => {
-        return (
-            <Typography variant='h5' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    h6: ({ children }: any) => {
-        return (
-            <Typography variant='h6' style={{ margin: '1rem 0' }}>
-                {children}
-            </Typography>
-        );
-    },
-    a: ({ node, ...props }: any) => (
-        <Link to='#' color='primary' target='_blank' rel='noopener noreferrer' {...props} />
-    ),
-    blockquote: ({ node, ...props }: any) => <blockquote style={{ margin: '1rem 0' }} {...props} />,
-    code: ({ node, inline, ...props }: any) => (
-        <code
-            component='code'
-            style={{
-                backgroundColor: '#e1e1e1',
-                borderRadius: '4px',
-                padding: inline ? '0 0.25em' : '0',
-            }}
-            {...props}
-        />
-    ),
-    em: ({ node, ...props }: any) => <em {...props} />,
-    hr: ({ node, ...props }: any) => <Divider {...props} />,
-    img: ({ node, ...props }: any) => <img {...props} alt={props.alt} src={props.src} style={{ maxWidth: '100%' }} />,
-    li: ({ node, ordered, ...props }: any) => <li {...props} />,
-    ol: ({ node, ordered, ...props }: any) => (
-        <ol
-            style={{
-                paddingLeft: '1em',
-                marginBottom: '1em',
-            }}
-            {...props}
-        />
-    ),
-    p: ({ node, ...props }: any) => <p style={{ margin: '1rem 0' }} {...props} />,
-    pre: ({ node, ...props }: any) => (
-        <pre
-            style={{
-                fontSize: '0.875rem',
-                backgroundColor: '#e1e1e1',
-                padding: '1rem',
-                borderRadius: '4px',
-                overflow: 'auto',
-                margin: '1rem 0',
-            }}
-            {...props}
-        />
-    ),
-    strong: ({ node, ...props }: any) => <strong {...props} />,
-    ul: ({ node, ordered, ...props }: any) => (
-        <ul
-            style={{
-                paddingLeft: '1em',
-                marginBottom: '1em',
-            }}
-            {...props}
-        />
-    ),
+const getComponents = (baseURL?: string) => {
+    const COMPONENTS = {
+        h1: ({ children }: any) => {
+            return (
+                <Typography variant='h1' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        h2: ({ children }: any) => {
+            return (
+                <Typography variant='h2' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        h3: ({ children }: any) => {
+            return (
+                <Typography variant='h3' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        h4: ({ children }: any) => {
+            return (
+                <Typography variant='h4' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        h5: ({ children }: any) => {
+            return (
+                <Typography variant='h5' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        h6: ({ children }: any) => {
+            return (
+                <Typography variant='h6' style={{ margin: '1rem 0' }}>
+                    {children}
+                </Typography>
+            );
+        },
+        a: ({ node, ...props }: any) => (
+            <Link to='#' color='primary' target='_blank' rel='noopener noreferrer' {...props} />
+        ),
+        blockquote: ({ node, ...props }: any) => <blockquote style={{ margin: '1rem 0' }} {...props} />,
+        code: ({ node, inline, ...props }: any) => (
+            <code
+                component='code'
+                style={{
+                    backgroundColor: '#e1e1e1',
+                    borderRadius: '4px',
+                    padding: inline ? '0 0.25em' : '0',
+                }}
+                {...props}
+            />
+        ),
+        em: ({ node, ...props }: any) => <em {...props} />,
+        hr: ({ node, ...props }: any) => <Divider {...props} />,
+        img: ({ node, ...props }: any) => {
+            const imgSrc = baseURL ? `${baseURL}/${props.src}` : props.src;
+            return <img {...props} alt={props.alt} src={imgSrc} style={{ maxWidth: '100%' }} />;
+        },
+        li: ({ node, ordered, ...props }: any) => <li {...props} />,
+        ol: ({ node, ordered, ...props }: any) => (
+            <ol
+                style={{
+                    paddingLeft: '1em',
+                    marginBottom: '1em',
+                }}
+                {...props}
+            />
+        ),
+        p: ({ node, ...props }: any) => <p style={{ margin: '1rem 0' }} {...props} />,
+        pre: ({ node, ...props }: any) => (
+            <pre
+                style={{
+                    fontSize: '0.875rem',
+                    backgroundColor: '#e1e1e1',
+                    padding: '1rem',
+                    borderRadius: '4px',
+                    overflow: 'auto',
+                    margin: '1rem 0',
+                }}
+                {...props}
+            />
+        ),
+        strong: ({ node, ...props }: any) => <strong {...props} />,
+        ul: ({ node, ordered, ...props }: any) => (
+            <ul
+                style={{
+                    paddingLeft: '1em',
+                    marginBottom: '1em',
+                }}
+                {...props}
+            />
+        ),
+    };
+
+    return COMPONENTS;
 };
 
 const RemoteContent: React.FC<{
     url: string;
+    baseURL?: string;
     markdown?: boolean;
     fallback?: string | React.ReactNode;
-}> = ({ url, markdown = false, fallback = 'An error has occurred.' }) => {
+}> = ({ url, baseURL, markdown = false, fallback = 'An error has occurred.' }) => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<any>(undefined);
     const [data, setData] = useState<string | undefined>(undefined);
@@ -148,7 +156,7 @@ const RemoteContent: React.FC<{
 
     if (error || data?.charAt(0) === '<') return <p style={{ margin: '1rem 0' }}>{fallback}</p>;
 
-    if (markdown) return <ReactMarkdown components={COMPONENTS}>{data!}</ReactMarkdown>;
+    if (markdown) return <ReactMarkdown components={getComponents(baseURL)}>{data!}</ReactMarkdown>;
 
     return <div>{DOMPurify.sanitize(data!)}</div>;
 };
