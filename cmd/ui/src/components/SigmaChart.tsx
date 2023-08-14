498665c94b5ff0bcdd5c5c6da78c41715044becc
Alyx Holms <aholms@specterops.io>
2023-08-14 15:01:06 -0600

    fix: additional accidental reverts

--- cmd/ui/src/components/SigmaChart.tsx
+++ cmd/ui/src/components/SigmaChart.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import { SigmaContainer } from '@react-sigma/core';
@@ -61,6 +61,9 @@ const SigmaChart: FC<Partial<SigmaChartProps>> = ({
         <SigmaContainer
             id='sigma-container'
             style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
                 height: '100%',
                 width: '100%',
                 background: 'linear-gradient(rgb(228, 233, 235) 0%, rgb(228, 233, 235) 100%)',
