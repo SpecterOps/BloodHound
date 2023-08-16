87c1982561be458751771614fc2ae921dfcdff6a
Ulises Rangel <urangel@specterops.io>
2023-08-16 15:53:44 -0500

    fix: gracefully handle selecting an unrecognized edge

--- cmd/ui/src/views/Explore/EdgeInfo/EdgeInfoContent.tsx
+++ cmd/ui/src/views/Explore/EdgeInfo/EdgeInfoContent.tsx
@@ -1,20 +1,20 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, Typography, useTheme } from '@mui/material';
 import { EdgeInfoComponents } from 'bh-shared-ui';
 import React from 'react';
 import { EdgeSections, SelectedEdge } from 'src/ducks/edgeinfo/edgeSlice';
@@ -22,13 +22,15 @@ import EdgeInfoCollapsibleSection from 'src/views/Explore/EdgeInfo/EdgeInfoColla
 import EdgeObjectInformation from 'src/views/Explore/EdgeInfo/EdgeObjectInformation';
 
 const EdgeInfoContent: React.FC<{ selectedEdge: NonNullable<SelectedEdge> }> = ({ selectedEdge }) => {
+    const theme = useTheme();
+
     const sections = EdgeInfoComponents[selectedEdge.name as keyof typeof EdgeInfoComponents];
     const { sourceNode, targetNode } = selectedEdge;
 
     return (
         <Box>
             <EdgeObjectInformation selectedEdge={selectedEdge} />
-            {
+            {sections ? (
                 <>
                     {Object.entries(sections).map((section, index) => {
                         const Section = section[1];
@@ -51,7 +53,22 @@ const EdgeInfoContent: React.FC<{ selectedEdge: NonNullable<SelectedEdge> }> = (
                         );
                     })}
                 </>
-            }
+            ) : (
+                <>
+                    <Box padding={1}>
+                        <Divider />
+                    </Box>
+                    <Box paddingLeft={theme.spacing(1)}>
+                        <Typography variant='body1' fontSize={'0.75rem'}>
+                            The edge{' '}
+                            <Typography component={'span'} variant='body1' fontWeight={'bold'} fontSize={'0.75rem'}>
+                                {selectedEdge.name}
+                            </Typography>{' '}
+                            does not have any additional contextual information at this time.
+                        </Typography>
+                    </Box>
+                </>
+            )}
         </Box>
     );
 };
