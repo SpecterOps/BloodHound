22592508e4f1b153531ed2c695d5afa04c69c8bc
Brandon Shearin <bshearin@specterops.io>
2023-08-23 09:27:24 -0700

    fix test

--- cmd/ui/src/views/Explore/ExploreSearch/ExploreSearch.test.tsx
+++ cmd/ui/src/views/Explore/ExploreSearch/ExploreSearch.test.tsx
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 import userEvent from '@testing-library/user-event';
@@ -46,7 +46,7 @@ describe('ExploreSearch rendering per tab', async () => {
         expect(screen.getByLabelText(/start node/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/destination node/i)).toBeInTheDocument();
 
-        expect(screen.getByRole('button', { name: /exchange-alt/i })).toBeDisabled();
+        expect(screen.getByRole('button', { name: /right-left/i })).toBeDisabled();
         expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument();
     });
 
@@ -171,7 +171,7 @@ describe('ExploreSearch interaction', () => {
         const pathfindingTab = screen.getByRole('tab', { name: /pathfinding/i });
         await user.click(pathfindingTab);
 
-        const swapButton = screen.getByRole('button', { name: /exchange-alt/i });
+        const swapButton = screen.getByRole('button', { name: /right-left/i });
         expect(swapButton).toBeDisabled();
 
         const startInput = screen.getByPlaceholderText(/start node/i);
@@ -189,7 +189,7 @@ describe('ExploreSearch interaction', () => {
         const pathfindingTab = screen.getByRole('tab', { name: /pathfinding/i });
         await user.click(pathfindingTab);
 
-        const swapButton = screen.getByRole('button', { name: /exchange-alt/i });
+        const swapButton = screen.getByRole('button', { name: /right-left/i });
         expect(swapButton).toBeDisabled();
 
         const startInput = screen.getByPlaceholderText(/start node/i);
