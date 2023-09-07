a7502683ce607920da232949f9d08b33bf669404
Ulises Rangel <urangel@specterops.io>
2023-09-07 12:38:14 -0500

    fix: use correct id for setting selected edge's target node data

--- cmd/ui/src/components/GraphEdgeEvents.tsx
+++ cmd/ui/src/components/GraphEdgeEvents.tsx
@@ -61,10 +61,10 @@ const GraphEdgeEvents: FC = () => {
                         type: graphState.chartProps.items?.[selectedItem.id1].data.nodetype,
                     },
                     targetNode: {
-                        name: graphState.chartProps.items?.[selectedItem.id1].data.name,
-                        id: selectedItem.id1,
-                        objectId: graphState.chartProps.items?.[selectedItem.id1].data.objectid,
-                        type: graphState.chartProps.items?.[selectedItem.id1].data.nodetype,
+                        name: graphState.chartProps.items?.[selectedItem.id2].data.name,
+                        id: selectedItem.id2,
+                        objectId: graphState.chartProps.items?.[selectedItem.id2].data.objectid,
+                        type: graphState.chartProps.items?.[selectedItem.id2].data.nodetype,
                     },
                 })
             );
