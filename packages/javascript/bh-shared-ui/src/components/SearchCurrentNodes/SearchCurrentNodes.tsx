f315509414be60587107c539091760bb6b562dd4
Wesley Robert Maffly-Kipp <wmaffly-kipp@specterops.io>
2023-09-11 19:53:18 -0500

    added virtualization to fix performance issues

--- packages/javascript/bh-shared-ui/src/components/SearchCurrentNodes/SearchCurrentNodes.tsx
+++ packages/javascript/bh-shared-ui/src/components/SearchCurrentNodes/SearchCurrentNodes.tsx
@@ -20,9 +20,13 @@ import { FC, useEffect, useRef, useState } from "react";
 import SearchResultItem from "../SearchResultItem";
 import { FlatNode, GraphNodes } from "./types";
 import { useOnClickOutside } from "../../hooks";
+import { FixedSizeList } from "react-window";
 
 export const PLACEHOLDER_TEXT = "Search Current Results";
 export const NO_RESULTS_TEXT = "No result found in current results";
+
+const LIST_ITEM_HEIGHT = 38;
+const MAX_CONTAINER_HEIGHT = 350;
 
 const SearchCurrentNodes: FC<{
     sx?: SxProps;
@@ -33,19 +37,36 @@ const SearchCurrentNodes: FC<{
     
     const containerRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const [flatNodeList, setFlatNodeList] = useState<FlatNode[]>([]);
     const [items, setItems] = useState<FlatNode[]>([]);
-    const [selectedNode, setSelectedNode] = useState<FlatNode | null | undefined>(null)
+    const [selectedNode, setSelectedNode] = useState<FlatNode | null | undefined>(null);
+    const [virtualizationHeight, setVirtualizationHeight] = useState<number>(0);
 
     // Node data is a lot easier to work with in the combobox if we transform to an array of flat objects
-    const flatNodeList: FlatNode[] = Object.entries(currentNodes).map(([key, value]) => {
-        return { id: key, ...value }
-    });
+    useEffect(() => {
+        const flatNodeList: FlatNode[] = Object.entries(currentNodes).map(([key, value]) => {
+            return { id: key, ...value }
+        });
+        setFlatNodeList(flatNodeList);
+    }, [currentNodes])
 
     useEffect(() => inputRef.current?.focus(), []);
 
     useEffect(() => {
         if (selectedNode) onSelect(selectedNode);
     }, [selectedNode, onSelect]);
+
+    // Since we are using a virtualized results container, we need to calculate the height for shorter 
+    // lists to avoid whitespace
+    useEffect(() => {
+        const resultsHeight = LIST_ITEM_HEIGHT * items.length;
+        if (resultsHeight > MAX_CONTAINER_HEIGHT) {
+            setVirtualizationHeight(400);
+        } else {
+            setVirtualizationHeight(resultsHeight);
+        }
+    }, [items]);
 
     useOnClickOutside(containerRef, () => onClose && onClose());
 
@@ -55,10 +76,9 @@ const SearchCurrentNodes: FC<{
             const filteredNodes = flatNodeList.filter(node => {
                 const label = node.label.toLowerCase();
                 const objectId = node.objectId.toLowerCase();
-                const lowercaseInputValue = inputValue?.toLowerCase() || '';
-
+                const lowercaseInputValue = inputValue?.toLowerCase() || '';  
+                
                 if (inputValue === '') return false;
-
                 return label.includes(lowercaseInputValue) || objectId.includes(lowercaseInputValue);
             });
             setItems(filteredNodes);
@@ -75,26 +95,37 @@ const SearchCurrentNodes: FC<{
         }
     });
 
+    const Row = ({ index, style }: any) => {
+        return (
+            <Box style={style}>
+                <SearchResultItem
+                    item={items[index]}
+                    index={index}
+                    key={index}
+                    highlightedIndex={0}
+                    keyword={inputValue}
+                    getItemProps={getItemProps}
+                />
+            </Box>
+        )
+    }
+
     return (
         <div ref={containerRef}>
             <Box component={Paper} {...sx} {...getComboboxProps()}>
-                <Box overflow={"auto"} maxHeight={350} marginBottom={items.length === 0 ? 0 : 1}>
+                <Box overflow={"auto"} maxHeight={MAX_CONTAINER_HEIGHT} marginBottom={items.length === 0 ? 0 : 1}>
                     <List data-testid={"current-results-list"} dense {...getMenuProps({
                         hidden: items.length === 0 && !inputValue,
                         style: { paddingTop: 0 }
                     })}>
-                        {items.map((node, index) => {
-                            return (
-                                <SearchResultItem
-                                    item={node}
-                                    index={index}
-                                    key={index}
-                                    highlightedIndex={0}
-                                    keyword={inputValue}
-                                    getItemProps={getItemProps}
-                                />
-                            );
-                        })}
+                        {<FixedSizeList
+                            height={virtualizationHeight}
+                            width={"100%"}
+                            itemSize={LIST_ITEM_HEIGHT}
+                            itemCount={items.length}
+                        >
+                            {Row}
+                        </FixedSizeList>}
                         {items.length === 0 && inputValue && <ListItem disabled sx={{ fontSize: 14 }}>{NO_RESULTS_TEXT}</ListItem>}
                     </List>
                 </Box>
