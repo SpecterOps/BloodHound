e8fd0ced936d8c70245eac189f64f7fdc91b9389
Alyx Holms <aholms@specterops.io>
2023-09-21 14:52:27 -0600

    feat: add skip to TestSizeOfProperties as it is too flakey

--- packages/go/dawgs/graph/properties_test.go
+++ packages/go/dawgs/graph/properties_test.go
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 package graph_test
@@ -20,8 +20,8 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/specterops/bloodhound/dawgs/graph"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewProperties(t *testing.T) {
@@ -64,6 +64,7 @@ func TestNewProperties(t *testing.T) {
 }
 
 func TestSizeOfProperties(t *testing.T) {
+	t.Skip("Skipping SizeOf due to flake")
 	properties := graph.NewProperties()
 
 	require.Equal(t, 24, int(properties.SizeOf()))
