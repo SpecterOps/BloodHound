ee475cf6d0efd83866279b24d287737fe595a85e
Irshad Ajmal Ahmed <iahmed@specterops.io>
2023-10-13 16:15:34 -0500

    get audit logs endpoints fixes and doc updates (#149)
    * get audit logs endpoints fixes and doc updates
    
    * refactor: rename audit logs db method
    
    * rename GetAuditLogs

--- cmd/api/src/api/v2/audit_integration_test.go
+++ cmd/api/src/api/v2/audit_integration_test.go
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 //go:build serial_integration
@@ -27,7 +27,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_GetAuditLogs(t *testing.T) {
+func Test_ListAuditLogs(t *testing.T) {
 	testCtx := integration.NewContext(t, integration.StartBHServer)
 
 	t.Run("Test Getting Latest Audit Logs", func(t *testing.T) {
@@ -56,7 +56,7 @@ func Test_GetAuditLogs(t *testing.T) {
 		testCtx.DeleteAssetGroup(newAssetGroup.ID)
 
 		// Expect one audit log entry from the deletion
-		auditLogs := testCtx.GetAuditLogsBetween(deletionTimestamp, time.Now(), 0, 1000)
+		auditLogs := testCtx.ListAuditLogs(deletionTimestamp, time.Now(), 0, 1000)
 		require.Equal(t, 1, len(auditLogs), "Expected only 1 audit log entry but saw %d", len(auditLogs))
 
 		testCtx.AssetAuditLog(auditLogs[0], "DeleteAssetGroup", map[string]any{
