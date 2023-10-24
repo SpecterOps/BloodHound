f34dc6328522fa2bc7a14d22e56696fe6b6526b8
Alyx Holms <aholms@specterops.io>
2023-10-23 23:43:14 -0600

    chore: add missing license headers

--- cmd/api/src/database/migration/test_migrations/v0.1.1.sql
+++ cmd/api/src/database/migration/test_migrations/v0.1.1.sql
@@ -1,3 +1,19 @@
+-- Copyright 2023 Specter Ops, Inc.
+--
+-- Licensed under the Apache License, Version 2.0
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- SPDX-License-Identifier: Apache-2.0
+
 -- Name should now be unique, so rename duplicates and enable constraint
 -- FIRST, REMOVE DUPLICATE RECORDS WITH BOTH NAME AND FOO MATCHING
 DELETE FROM migration_test mt1 USING migration_test mt2
