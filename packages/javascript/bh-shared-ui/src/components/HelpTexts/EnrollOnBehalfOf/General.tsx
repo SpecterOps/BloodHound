5b95dbd2eae46da4651d3d462846acd01eedf0c3
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2023-11-02 17:30:43 +0100

    chore: update EnrollOnBehalfOf help text

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/EnrollOnBehalfOf/General.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/EnrollOnBehalfOf/General.tsx
@@ -28,10 +28,11 @@ const General: FC<EdgeInfoProps> = ({ sourceName, sourceType, targetName }) => {
             </Typography>
             <Typography variant='body2'>
                 The certificate template {sourceName} is configured to be used as an enrollment agent. The certificate
-                template {targetName} is configured to allow enrollment by enrollment agents. This enables a principal
-                with a certificate of certificate template {sourceName} to enroll on behalf of other principals for
-                certificate template {targetName} as long as enrollment agent restrictions configured on the enterprise CA
-                permit it.
+                template {targetName} is configured to allow enrollment by enrollment agents. Both certificate templates
+                is published by an enterprise CA which is trusted for NT authentication and chain up to a root CA for the
+                domain. This enables a principal with a certificate of certificate template {sourceName} to enroll on
+                behalf of other principals for certificate template {targetName} as long as enrollment agent restrictions
+                configured on the enterprise CA permit it.
             </Typography>
         </>
     );
