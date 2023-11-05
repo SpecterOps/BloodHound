05f99b09ece92e7d9bc6e368870120924a42cf90
Wesley Robert Maffly-Kipp <wmaffly-kipp@specterops.io>
2023-11-05 12:22:23 -0600

    run license check

--- packages/javascript/bh-shared-ui/src/components/AssetGroupEdit/AssetGroupAutocomplete.tsx
+++ packages/javascript/bh-shared-ui/src/components/AssetGroupEdit/AssetGroupAutocomplete.tsx
@@ -1,3 +1,19 @@
+// Copyright 2023 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 import { Autocomplete, AutocompleteRenderInputParams, TextField } from '@mui/material';
 import { FC, HTMLAttributes, ReactNode, SyntheticEvent, useState } from 'react';
 import AutocompleteOption from './AutocompleteOption';
@@ -29,8 +45,6 @@ const AssetGroupAutocomplete: FC<{
     const searchResultsWithActions = data?.map((result) => {
         const resultInChangelog = changelog.find((member) => member.objectid === result.objectid);
         const matchedSelector = assetGroup.Selectors.find((selector) => selector.selector === result.objectid);
-
-        console.log(assetGroup.Selectors, result.objectid);
 
         let action = ChangelogAction.ADD;
 
