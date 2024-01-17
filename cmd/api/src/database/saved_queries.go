a2fca90db14233a7b20c7feb3a4aa660ca27bc0f
mistahj67 <26472282+mistahj67@users.noreply.github.com>
2024-01-17 14:38:19 -0700

    Bed 3859 - Fix inaccurate count strategy in paginated SQL queries (#317)
    * fix: counts when filtering AuditLogs
    
    * fix: counts when filtering SavedQueries

--- cmd/api/src/database/saved_queries.go
+++ cmd/api/src/database/saved_queries.go
@@ -19,30 +19,33 @@ package database
 import (
 	"github.com/gofrs/uuid"
 	"github.com/specterops/bloodhound/src/model"
+	"gorm.io/gorm"
 )
 
 func (s *BloodhoundDB) ListSavedQueries(userID uuid.UUID, order string, filter model.SQLFilter, skip, limit int) (model.SavedQueries, int, error) {
 	var (
 		queries model.SavedQueries
+		result  *gorm.DB
 		count   int64
+		cursor  = s.Scope(Paginate(skip, limit)).Where("user_id = ?", userID)
 	)
-
-	cursor := s.Scope(Paginate(skip, limit)).Where("user_id = ?", userID)
 
 	if filter.SQLString != "" {
 		cursor = cursor.Where(filter.SQLString, filter.Params)
+		result = s.db.Model(&queries).Where("user_id = ?", userID).Where(filter.SQLString, filter.Params).Count(&count)
+	} else {
+		result = s.db.Model(&queries).Where("user_id = ?", userID).Count(&count)
+	}
+
+	if result.Error != nil {
+		return queries, 0, result.Error
 	}
 
 	if order != "" {
 		cursor = cursor.Order(order)
 	}
-
-	result := s.db.Where("user_id = ?", userID).Find(&queries).Count(&count)
-	if result.Error != nil {
-		return queries, 0, result.Error
-	}
-
 	result = cursor.Find(&queries)
+
 	return queries, int(count), CheckError(result)
 }
 
