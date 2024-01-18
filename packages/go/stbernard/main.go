8e56d9df5657959bf95108df4098bf1cf44a9b53
Alyx Holms <aholms@specterops.io>
2024-01-17 17:17:16 -0700

    feat: expermintal analysis command added to support Go and JS analysis

--- packages/go/stbernard/main.go
+++ packages/go/stbernard/main.go
@@ -19,13 +19,14 @@ package main
 import (
 	"errors"
 	"log"
+	"strings"
 
 	"github.com/specterops/bloodhound/packages/go/stbernard/command"
 )
 
 func main() {
 	if cmd, err := command.ParseCLI(); err != nil {
-		if errors.Is(err, command.NoCmdErr) {
+		if errors.Is(err, command.ErrNoCmd) {
 			log.Fatal("No command specified")
 		} else {
 			log.Fatalf("Error while parsing command: %v", err)
@@ -33,6 +34,6 @@ func main() {
 	} else if err := cmd.Run(); err != nil {
 		log.Fatalf("Failed to run command %s: %v", cmd.Name(), err)
 	} else {
-		log.Printf("%s completed successfully", cmd.Name())
+		log.Printf("%s completed successfully", strings.ToUpper(cmd.Name()))
 	}
 }
