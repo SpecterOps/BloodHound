cd37bf6655ed28459aa0868524cb6b55fc9dab67
Ben Waples <bwaples@specterops.io>
2024-01-18 15:41:29 -0800

    CanAbuseUPNCertMapping finding panel (#316)
    * initial help text
    
    * remove hard coded test
    
    * organize

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/index.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/index.tsx
@@ -64,6 +64,7 @@ import AdminTo from './AdminTo/AdminTo';
 import AllExtendedRights from './AllExtendedRights/AllExtendedRights';
 import AllowedToAct from './AllowedToAct/AllowedToAct';
 import AllowedToDelegate from './AllowedToDelegate/AllowedToDelegate';
+import CanAbuseUPNCertMapping from './CanAbuseUPNCertMapping/CanAbuseUPNCertMapping';
 import CanPSRemote from './CanPSRemote/CanPSRemote';
 import CanRDP from './CanRDP/CanRDP';
 import Contains from './Contains/Contains';
@@ -146,6 +147,7 @@ const EdgeInfoComponents = {
     ReadGMSAPassword: ReadGMSAPassword,
     HasSIDHistory: HasSIDHistory,
     TrustedBy: TrustedBy,
+    CanAbuseUPNCertMapping: CanAbuseUPNCertMapping,
     CanPSRemote: CanPSRemote,
     AZAddMembers: AZAddMembers,
     AZAddSecret: AZAddSecret,
