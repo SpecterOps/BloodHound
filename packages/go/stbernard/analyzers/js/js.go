77a8b26fe82c55e84a272dcf305d1b1d0db673e8
Alyx Holms <aholms@specterops.io>
2024-01-23 10:01:02 -0700

    fix: copyright dates

--- packages/go/stbernard/analyzers/js/js.go
+++ packages/go/stbernard/analyzers/js/js.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Specter Ops, Inc.
+// Copyright 2024 Specter Ops, Inc.
 //
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
