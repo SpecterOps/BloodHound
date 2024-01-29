9b15040ef3905a136a67d4f659d4ebea943286c8
Mistah J <26472282+mistahj67@users.noreply.github.com>
2024-01-29 13:06:38 -0700

    update & abstract role tests to other roles

--- cmd/api/src/auth/role_test.go
+++ cmd/api/src/auth/role_test.go
@@ -21,11 +21,13 @@ package auth_test
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gofrs/uuid"
 	"github.com/specterops/bloodhound/lab"
 	"github.com/specterops/bloodhound/src/api"
 	v2 "github.com/specterops/bloodhound/src/api/v2"
 	"github.com/specterops/bloodhound/src/auth"
+	"github.com/specterops/bloodhound/src/model"
 	"github.com/specterops/bloodhound/src/model/appcfg"
 	"github.com/specterops/bloodhound/src/test/lab/fixtures"
 	"github.com/stretchr/testify/require"
@@ -34,24 +36,48 @@ import (
 	"time"
 )
 
-func TestRole_PowerUser(t *testing.T) {
+func testCondition(role auth.RoleTemplate, permission model.Permission) string {
+	if role.Permissions.Has(permission) {
+		return "SHOULD"
+	}
+	return "SHOULD NOT"
+}
+
+func requireForbidden(t *testing.T, err error) {
+	var errByte []byte
+	errByte, err = json.Marshal(err)
+	require.Nil(t, err)
+
+	errWrapper := api.ErrorWrapper{}
+	err = json.Unmarshal(errByte, &errWrapper)
+	require.Nilf(t, err, "Failed to unmarshal error %v", string(errByte))
+	require.Equal(t, errWrapper.HTTPStatus, http.StatusForbidden)
+}
+
+func testRoleAccess(t *testing.T, roleName string) {
+	role, ok := auth.Roles()[roleName]
+	require.Truef(t, ok, "invalid role name")
 
 	harness := lab.NewHarness()
 	lab.Pack(harness, fixtures.BHAdminApiClientFixture)
-	powerUserClientFixture := fixtures.NewUserApiClientFixture(fixtures.BHAdminApiClientFixture, auth.RolePowerUser)
-	lab.Pack(harness, powerUserClientFixture)
+	userClientFixture := fixtures.NewUserApiClientFixture(fixtures.BHAdminApiClientFixture, role.Name)
+	lab.Pack(harness, userClientFixture)
 
 	lab.NewSpec(t, harness).Run(
-		lab.TestCase("SHOULD be able to access AppReadApplicationConfiguration endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access AppReadApplicationConfiguration endpoints", testCondition(role, auth.Permissions().AppReadApplicationConfiguration)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.GetAppConfigs()
-			require.Nil(t, err)
+			_, err := userClient.GetAppConfigs()
+			if role.Permissions.Has(auth.Permissions().AppReadApplicationConfiguration) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access AppWriteApplicationConfiguration endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access AppWriteApplicationConfiguration endpoints", testCondition(role, auth.Permissions().AppWriteApplicationConfiguration)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
 			updatedPasswordExpirationWindowParameter := v2.AppConfigUpdateRequest{
@@ -60,90 +86,147 @@ func TestRole_PowerUser(t *testing.T) {
 					"duration": "P30D",
 				},
 			}
-			_, err := powerUserClient.PutAppConfig(updatedPasswordExpirationWindowParameter)
-			require.Nil(t, err)
+			_, err := userClient.PutAppConfig(updatedPasswordExpirationWindowParameter)
+			if role.Permissions.Has(auth.Permissions().AppWriteApplicationConfiguration) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access AuthCreateToken endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access own AuthCreateToken endpoints", testCondition(role, auth.Permissions().AuthCreateToken)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			randomUserUuid, err := uuid.NewV4()
-			require.Nil(t, err)
+			user, err := userClient.GetSelf()
+			require.Nilf(t, err, "failed looking up user details")
 
-			_, err = powerUserClient.ListUserTokens(randomUserUuid)
-			require.Nil(t, err)
+			_, err = userClient.ListUserTokens(user.ID)
+			if role.Permissions.Has(auth.Permissions().AuthCreateToken) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD NOT be able to access AuthManageProviders endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access AuthManageProviders endpoints", testCondition(role, auth.Permissions().AuthManageProviders)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListSAMLIdentityProviders()
-			var errByte []byte
-			errByte, err = json.Marshal(err)
-			require.Nil(t, err)
-
-			errWrapper := api.ErrorWrapper{}
-			err = json.Unmarshal(errByte, &errWrapper)
-			require.Nil(t, err)
-			require.Equal(t, errWrapper.HTTPStatus, http.StatusForbidden)
+			_, err := userClient.ListSAMLIdentityProviders()
+			if role.Permissions.Has(auth.Permissions().AuthManageProviders) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access AuthManageSelf endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access AuthManageSelf endpoints", testCondition(role, auth.Permissions().AuthManageSelf)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListPermissions()
-			require.Nil(t, err)
+			_, err := userClient.ListPermissions()
+			if role.Permissions.Has(auth.Permissions().AuthManageSelf) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD NOT be able to access AuthManageUsers endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access AuthManageUsers endpoints", testCondition(role, auth.Permissions().AuthManageUsers)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListAuditLogs(time.Now(), time.Now(), 0, 0)
-			require.NotNil(t, err)
-
-			var errByte []byte
-			errByte, err = json.Marshal(err)
-			require.Nil(t, err)
-
-			errWrapper := api.ErrorWrapper{}
-			err = json.Unmarshal(errByte, &errWrapper)
-			require.Nil(t, err)
-			require.Equal(t, errWrapper.HTTPStatus, http.StatusForbidden)
+			_, err := userClient.ListAuditLogs(time.Now(), time.Now(), 0, 0)
+			if role.Permissions.Has(auth.Permissions().AuthManageUsers) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access GraphDbWrite endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access GraphDBWrite endpoints", testCondition(role, auth.Permissions().GraphDBWrite)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.CreateFileUploadTask()
-			require.Nil(t, err)
+			_, err := userClient.CreateFileUploadTask()
+			if role.Permissions.Has(auth.Permissions().GraphDBWrite) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access GraphDbRead endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access GraphDBRead endpoints", testCondition(role, auth.Permissions().GraphDBRead)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListAssetGroups()
-			require.Nil(t, err)
+			_, err := userClient.ListAssetGroups()
+			if role.Permissions.Has(auth.Permissions().GraphDBRead) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access SavedQueriesRead endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access SavedQueriesRead endpoints", testCondition(role, auth.Permissions().SavedQueriesRead)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListPermissions()
-			require.Nil(t, err)
+			_, err := userClient.ListSavedQueries()
+			if role.Permissions.Has(auth.Permissions().SavedQueriesRead) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
 		}),
 
-		lab.TestCase("SHOULD be able to access SavedQueriesWrite endpoints", func(assert *require.Assertions, harness *lab.Harness) {
-			powerUserClient, ok := lab.Unpack(harness, powerUserClientFixture)
+		lab.TestCase(fmt.Sprintf("%s be able to access SavedQueriesWrite endpoints", testCondition(role, auth.Permissions().SavedQueriesWrite)), func(assert *require.Assertions, harness *lab.Harness) {
+			userClient, ok := lab.Unpack(harness, userClientFixture)
 			assert.True(ok)
 
-			_, err := powerUserClient.ListPermissions()
+			_, err := userClient.CreateSavedQuery()
+			if role.Permissions.Has(auth.Permissions().SavedQueriesWrite) {
+				require.Nil(t, err)
+			} else {
+				requireForbidden(t, err)
+			}
+		}),
+	)
+}
+
+func TestRole_ReadOnly(t *testing.T) {
+	testRoleAccess(t, auth.RoleReadOnly)
+}
+
+func TestRole_UploadOnly(t *testing.T) {
+	testRoleAccess(t, auth.RoleUploadOnly)
+}
+
+func TestRole_User(t *testing.T) {
+	testRoleAccess(t, auth.RoleUser)
+}
+
+func TestRole_PowerUser(t *testing.T) {
+	testRoleAccess(t, auth.RolePowerUser)
+}
+
+func TestRole_Administrator(t *testing.T) {
+	testRoleAccess(t, auth.RoleAdministrator)
+}
+
+func TestRole_Administrator_ListOtherUserTokens(t *testing.T) {
+	harness := lab.NewHarness()
+	lab.Pack(harness, fixtures.BHAdminApiClientFixture)
+	lab.NewSpec(t, harness).Run(
+		lab.TestCase("Should be able to access AuthCreateToken endpoints for other users", func(assert *require.Assertions, harness *lab.Harness) {
+			adminClient, ok := lab.Unpack(harness, fixtures.BHAdminApiClientFixture)
+			assert.True(ok)
+
+			randoUser, err := uuid.NewV4()
+			require.Nilf(t, err, "failed to create rando user")
+
+			_, err = adminClient.ListUserTokens(randoUser)
 			require.Nil(t, err)
 		}),
 	)
