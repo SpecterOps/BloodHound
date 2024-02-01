13f416002dcef5e5f0917b1c0b5be5e69c8537fa
James Barnett <jbarnett@specterops.io>
2024-02-01 10:22:57 -0500

    chore: change source column to TEXT
    It was previously sized to only account for max 1 IPv6 address, but
    our design changes mean it could now be any length

--- cmd/api/src/database/migration/migrations/v5.6.0.sql
+++ cmd/api/src/database/migration/migrations/v5.6.0.sql
@@ -3,4 +3,5 @@ DROP CONSTRAINT IF EXISTS audit_logs_status_check,
 ADD CONSTRAINT status_check
 CHECK (status IN ('intent', 'success', 'failure')),
 ALTER COLUMN status SET DEFAULT 'intent',
+ALTER COLUMN source TYPE TEXT,
 ADD COLUMN IF NOT EXISTS commit_id TEXT;
