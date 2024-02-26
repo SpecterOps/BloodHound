c4f88ec9aab79bc3898d301fa94a6e9b3e7211a3
Brandon Shearin <bshearin@specterops.io>
2024-02-26 14:17:19 -0500

    restructure func

--- cmd/api/src/api/v2/database_wipe.go
+++ cmd/api/src/api/v2/database_wipe.go
@@ -42,9 +42,9 @@ func (s Resources) HandleDatabaseWipe(response http.ResponseWriter, request *htt
 
 	var (
 		payload DatabaseWipe
-		nodeIDs []graph.ID
 		// use this struct to flag any fields that failed to delete
-		errors          DatabaseWipe
+		errors DatabaseWipe
+		// deleting collected graph data OR high value selectors starts analsyis
 		kickoffAnalysis bool
 	)
 
@@ -57,7 +57,7 @@ func (s Resources) HandleDatabaseWipe(response http.ResponseWriter, request *htt
 		return
 	}
 
-	// return `BadRequest` if request specifies high value selectors without an asset group ID
+	// return `BadRequest` if request specifies `high value selectors` without an `asset group ID`
 	if payload.DeleteHighValueSelectors && payload.AssetGroupId == 0 {
 		api.WriteErrorResponse(
 			request.Context(),
@@ -108,49 +108,12 @@ func (s Resources) HandleDatabaseWipe(response http.ResponseWriter, request *htt
 
 	// delete graph
 	if payload.DeleteCollectedGraphData {
-
-		if err := s.Graph.ReadTransaction(request.Context(), func(tx graph.Transaction) error {
-			fetchedNodeIDs, err := ops.FetchNodeIDs(tx.Nodes())
-
-			nodeIDs = append(nodeIDs, fetchedNodeIDs...)
-			return err
-		}); err != nil {
-			errors.DeleteCollectedGraphData = true
-			log.Errorf("%s: %s", "error fetching all nodes", err.Error())
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, false, "collected graph data")
-
-		} else if err := s.Graph.BatchOperation(request.Context(), func(batch graph.Batch) error {
-			for _, nodeId := range nodeIDs {
-				// deleting a node also deletes all of its edges due to a sql trigger
-				if err := batch.DeleteNode(nodeId); err != nil {
-					return err
-				}
-			}
-			return nil
-		}); err != nil {
-			errors.DeleteCollectedGraphData = true
-			log.Errorf("%s: %s", "error deleting all nodes", err.Error())
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, false, "collected graph data")
-
-		} else {
-			// if successful, handle audit log and kick off analysis
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, true, "collected graph data")
-			kickoffAnalysis = true
-		}
-
+		s.deleteCollectedGraphData(request.Context(), &errors, auditEntry, &kickoffAnalysis)
 	}
 
 	// delete custom high value selectors
 	if payload.DeleteHighValueSelectors {
-		if err := s.DB.DeleteAssetGroupSelectorsForAssetGroup(request.Context(), payload.AssetGroupId); err != nil {
-			errors.DeleteHighValueSelectors = true
-			log.Errorf("%s %d: %s", "there was an error deleting asset group with id = ", payload.AssetGroupId, err.Error())
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, false, "high value selectors")
-		} else {
-			// if succesful, handle audit log and kick off analysis
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, true, "high value selectors")
-			kickoffAnalysis = true
-		}
+		s.deleteHighValueSelectors(request.Context(), &errors, payload.AssetGroupId, auditEntry, &kickoffAnalysis)
 	}
 
 	// if deleting `nodes` or deleting `asset group selectors` is successful, kickoff an analysis
@@ -160,26 +123,12 @@ func (s Resources) HandleDatabaseWipe(response http.ResponseWriter, request *htt
 
 	// delete file ingest history
 	if payload.DeleteFileIngestHistory {
-		if err := s.DB.DeleteAllFileUploads(); err != nil {
-			errors.DeleteFileIngestHistory = true
-			log.Errorf("%s: %s", "there was an error deleting file ingest history", err.Error())
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, false, "file ingest history")
-
-		} else {
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, true, "file ingest history")
-		}
+		s.deleteFileIngestHistory(request.Context(), &errors, auditEntry)
 	}
 
 	// delete data quality history
 	if payload.DeleteDataQualityHistory {
-		if err := s.DB.DeleteAllDataQuality(); err != nil {
-			errors.DeleteDataQualityHistory = true
-			log.Errorf("%s: %s", "there was an error deleting data quality history", err.Error())
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, false, "data quality history")
-
-		} else {
-			s.handleAuditLogForDatabaseWipe(request.Context(), auditEntry, true, "data quality history")
-		}
+		s.deleteDataQualityHistory(request.Context(), &errors, auditEntry)
 	}
 
 	// return a user friendly error message indicating what operations failed
@@ -194,6 +143,71 @@ func (s Resources) HandleDatabaseWipe(response http.ResponseWriter, request *htt
 		response.WriteHeader(http.StatusNoContent)
 	}
 
+}
+
+func (s Resources) deleteCollectedGraphData(ctx context.Context, errors *DatabaseWipe, auditEntry *model.AuditEntry, kickoffAnalysis *bool) {
+	var nodeIDs []graph.ID
+
+	if err := s.Graph.ReadTransaction(ctx, func(tx graph.Transaction) error {
+		fetchedNodeIDs, err := ops.FetchNodeIDs(tx.Nodes())
+
+		nodeIDs = append(nodeIDs, fetchedNodeIDs...)
+		return err
+	}); err != nil {
+		errors.DeleteCollectedGraphData = true
+		log.Errorf("%s: %s", "error fetching all nodes", err.Error())
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, false, "collected graph data")
+
+	} else if err := s.Graph.BatchOperation(ctx, func(batch graph.Batch) error {
+		for _, nodeId := range nodeIDs {
+			// deleting a node also deletes all of its edges due to a sql trigger
+			if err := batch.DeleteNode(nodeId); err != nil {
+				return err
+			}
+		}
+		return nil
+	}); err != nil {
+		errors.DeleteCollectedGraphData = true
+		log.Errorf("%s: %s", "error deleting all nodes", err.Error())
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, false, "collected graph data")
+
+	} else {
+		// if successful, handle audit log and kick off analysis
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, true, "collected graph data")
+		*kickoffAnalysis = true
+	}
+}
+
+func (s Resources) deleteHighValueSelectors(ctx context.Context, errors *DatabaseWipe, assetGroupId int, auditEntry *model.AuditEntry, kickoffAnalysis *bool) {
+	if err := s.DB.DeleteAssetGroupSelectorsForAssetGroup(ctx, assetGroupId); err != nil {
+		errors.DeleteHighValueSelectors = true
+		log.Errorf("%s %d: %s", "there was an error deleting asset group with id = ", assetGroupId, err.Error())
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, false, "high value selectors")
+	} else {
+		// if succesful, handle audit log and kick off analysis
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, true, "high value selectors")
+		*kickoffAnalysis = true
+	}
+}
+
+func (s Resources) deleteFileIngestHistory(ctx context.Context, errors *DatabaseWipe, auditEntry *model.AuditEntry) {
+	if err := s.DB.DeleteAllFileUploads(); err != nil {
+		errors.DeleteFileIngestHistory = true
+		log.Errorf("%s: %s", "there was an error deleting file ingest history", err.Error())
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, false, "file ingest history")
+	} else {
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, true, "file ingest history")
+	}
+}
+
+func (s Resources) deleteDataQualityHistory(ctx context.Context, errors *DatabaseWipe, auditEntry *model.AuditEntry) {
+	if err := s.DB.DeleteAllDataQuality(); err != nil {
+		errors.DeleteDataQualityHistory = true
+		log.Errorf("%s: %s", "there was an error deleting data quality history", err.Error())
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, false, "data quality history")
+	} else {
+		s.handleAuditLogForDatabaseWipe(ctx, auditEntry, true, "data quality history")
+	}
 }
 
 func (s Resources) handleAuditLogForDatabaseWipe(ctx context.Context, auditEntry *model.AuditEntry, success bool, msg string) error {
