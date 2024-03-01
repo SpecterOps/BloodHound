b3fdfae336452f7cf1a4d25466a525dcd98b536f
Brandon Shearin <bshearin@specterops.io>
2024-03-01 11:25:19 -0500

    template 2 integration test

--- cmd/api/src/analysis/ad/adcs_integration_test.go
+++ cmd/api/src/analysis/ad/adcs_integration_test.go
@@ -729,6 +729,87 @@ func TestADCSESC4(t *testing.T) {
 		})
 	})
 
+	testContext.DatabaseTestWithSetup(func(harness *integration.HarnessDetails) error {
+		harness.ESC4Template2.Setup(testContext)
+		return nil
+	}, func(harness integration.HarnessDetails, db graph.Database) {
+		operation := analysis.NewPostRelationshipOperation(context.Background(), db, "ADCS Post Process Test - ESC4 template 1")
+
+		groupExpansions, err := ad2.ExpandAllRDPLocalGroups(context.Background(), db)
+		require.Nil(t, err)
+
+		enterpriseCertAuthorities, err := ad2.FetchNodesByKind(context.Background(), db, ad.EnterpriseCA)
+		require.Nil(t, err)
+
+		certTemplates, err := ad2.FetchNodesByKind(context.Background(), db, ad.CertTemplate)
+		require.Nil(t, err)
+
+		domains, err := ad2.FetchNodesByKind(context.Background(), db, ad.Domain)
+		require.Nil(t, err)
+
+		cache := ad2.NewADCSCache()
+		cache.BuildCache(context.Background(), db, enterpriseCertAuthorities, certTemplates)
+
+		for _, domain := range domains {
+			innerDomain := domain
+
+			for _, enterpriseCA := range enterpriseCertAuthorities {
+				if cache.DoesCAChainProperlyToDomain(enterpriseCA, innerDomain) {
+					innerEnterpriseCA := enterpriseCA
+
+					operation.Operation.SubmitReader(func(ctx context.Context, tx graph.Transaction, outC chan<- analysis.CreatePostRelationshipJob) error {
+						if err := ad2.PostADCSESC4(ctx, tx, outC, groupExpansions, innerEnterpriseCA, innerDomain, cache); err != nil {
+							t.Logf("failed post processing for %s: %v", ad.ADCSESC4.String(), err)
+						} else {
+							return nil
+						}
+
+						return nil
+					})
+				}
+			}
+
+		}
+
+		operation.Done()
+
+		db.ReadTransaction(context.Background(), func(tx graph.Transaction) error {
+			if results, err := ops.FetchStartNodes(tx.Relationships().Filterf(func() graph.Criteria {
+				return query.Kind(query.Relationship(), ad.ADCSESC4)
+			})); err != nil {
+				t.Fatalf("error fetching esc4 edges in integration test; %v", err)
+			} else {
+				require.Equal(t, 18, len(results))
+
+				require.True(t, results.Contains(harness.ESC4Template2.Group11))
+				require.True(t, results.Contains(harness.ESC4Template2.Group12))
+
+				require.True(t, results.Contains(harness.ESC4Template2.Group21))
+				require.True(t, results.Contains(harness.ESC4Template2.Group22))
+				require.True(t, results.Contains(harness.ESC4Template2.Group25))
+
+				require.True(t, results.Contains(harness.ESC4Template2.Group31))
+				require.True(t, results.Contains(harness.ESC4Template2.Group32))
+				require.True(t, results.Contains(harness.ESC4Template2.Group33))
+				require.True(t, results.Contains(harness.ESC4Template2.Group35))
+
+				require.True(t, results.Contains(harness.ESC4Template2.Group41))
+				require.True(t, results.Contains(harness.ESC4Template2.Group42))
+				require.True(t, results.Contains(harness.ESC4Template2.Group44))
+				require.True(t, results.Contains(harness.ESC4Template2.Group45))
+
+				require.True(t, results.Contains(harness.ESC4Template2.Group51))
+				require.True(t, results.Contains(harness.ESC4Template2.Group52))
+				require.True(t, results.Contains(harness.ESC4Template2.Group53))
+				require.True(t, results.Contains(harness.ESC4Template2.Group54))
+				require.True(t, results.Contains(harness.ESC4Template2.Group55))
+
+			}
+
+			return nil
+		})
+	})
+
 }
 
 func TestADCSESC9a(t *testing.T) {
