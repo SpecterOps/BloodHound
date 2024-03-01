e2c1259c33a6caeaf5410e768d501aed5d37fedf
Brandon Shearin <bshearin@specterops.io>
2024-03-01 10:58:36 -0500

    checkout lab files from main

--- cmd/api/src/test/lab/fixtures/domain.go
+++ cmd/api/src/test/lab/fixtures/domain.go
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-//
+// 
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-//
+// 
 //     http://www.apache.org/licenses/LICENSE-2.0
-//
+// 
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+// 
 // SPDX-License-Identifier: Apache-2.0
 
 package fixtures
@@ -19,15 +19,14 @@ package fixtures
 import (
 	"log"
 
+	"github.com/specterops/bloodhound/lab"
 	"github.com/gofrs/uuid"
 	"github.com/specterops/bloodhound/dawgs/graph"
 	"github.com/specterops/bloodhound/graphschema/ad"
 	"github.com/specterops/bloodhound/graphschema/common"
-	"github.com/specterops/bloodhound/lab"
-	"github.com/specterops/bloodhound/src/api/v2/apiclient"
 )
 
-func NewDomainFixture(sid uuid.UUID, name string, collected, blocksInheritance bool, apiClient *lab.Fixture[apiclient.Client]) *lab.Fixture[*graph.Node] {
+func NewDomainFixture(sid uuid.UUID, name string, collected, blocksInheritance bool) *lab.Fixture[*graph.Node] {
 	fixture := lab.NewFixture(func(harness *lab.Harness) (*graph.Node, error) {
 		if node, err := CreateNode(harness, graph.PropertyMap{
 			common.Name:          name,
@@ -52,15 +51,11 @@ func NewDomainFixture(sid uuid.UUID, name string, collected, blocksInheritance b
 		log.Fatalln(err)
 	}
 
-	if err := lab.SetDependency(fixture, apiClient); err != nil {
-		log.Fatalln(err)
-	}
-
 	return fixture
 }
 
 var (
 	BasicDomainSID     = uuid.Must(uuid.NewV4())
 	BasicDomainName    = "TestDomain"
-	BasicDomainFixture = NewDomainFixture(BasicDomainSID, BasicDomainName, true, false, BHAdminApiClientFixture)
+	BasicDomainFixture = NewDomainFixture(BasicDomainSID, BasicDomainName, true, false)
 )
