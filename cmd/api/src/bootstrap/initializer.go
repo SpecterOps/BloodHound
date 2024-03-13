480b88999fcbe060b106510df6a5066857a393cb
Mistah J <26472282+mistahj67@users.noreply.github.com>
2024-03-13 12:38:35 -0700

    fix: use ctx for graph db close

--- cmd/api/src/bootstrap/initializer.go
+++ cmd/api/src/bootstrap/initializer.go
@@ -65,7 +65,7 @@ func (s Initializer[DBType, GraphType]) Launch(parentCtx context.Context, handle
 	} else {
 		// Ensure that the database instances are closed once we're ready to exit regardless of p
 		defer databaseConnections.RDMS.Close(ctx)
-		defer databaseConnections.Graph.Close(context.Background())
+		defer databaseConnections.Graph.Close(ctx)
 
 		daemonManager.Start(daemonInstances...)
 	}
