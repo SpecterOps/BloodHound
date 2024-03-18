c32893ab5139ebf1d858e73d2ebdc43dbafe06c3
Alyx Holms <aholms@specterops.io>
2024-03-18 10:13:07 -0600

    chore: document environment and fix up command implementation

--- packages/go/stbernard/environment/environment.go
+++ packages/go/stbernard/environment/environment.go
@@ -5,8 +5,10 @@ import (
 	"strings"
 )
 
+// Environment is a string map representation of env vars
 type Environment map[string]string
 
+// NewEnvironment pulls os.Environ and converts to an Environment
 func NewEnvironment() Environment {
 	var (
 		envVars = os.Environ()
@@ -21,12 +23,14 @@ func NewEnvironment() Environment {
 	return envMap
 }
 
+// SetIfEmpty sets a value only if the key currently has no value
 func (s Environment) SetIfEmpty(key string, value string) {
 	if _, ok := s[key]; !ok {
 		s[key] = value
 	}
 }
 
+// Slice converts the Environment to a slice of strings in the form `KEY=VALUE` to send to external libraries
 func (s Environment) Slice() []string {
 	var envSlice = make([]string, 0, len(s))
 	for key, val := range s {
