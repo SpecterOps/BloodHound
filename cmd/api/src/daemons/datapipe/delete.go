8c3fa90c04478de9de9518ed4fcef841dbb978e3
Rohan Vazarkar <rvazarkar@users.noreply.github.com>
2024-03-26 12:21:22 -0400

    fix: performantly delete graph data (#524)

--- cmd/api/src/daemons/datapipe/delete.go
+++ cmd/api/src/daemons/datapipe/delete.go
@@ -5,31 +5,73 @@ import (
 	"fmt"
 	"github.com/specterops/bloodhound/dawgs/graph"
 	"github.com/specterops/bloodhound/dawgs/ops"
+	"github.com/specterops/bloodhound/dawgs/util/channels"
 )
 
 func DeleteCollectedGraphData(ctx context.Context, graphDB graph.Database) error {
-	var nodeIDs []graph.ID
+	operation := ops.StartNewOperation[graph.ID](ops.OperationContext{
+		Parent:     ctx,
+		DB:         graphDB,
+		NumReaders: 1,
+		NumWriters: 1,
+	})
 
-	if err := graphDB.ReadTransaction(ctx,
-		func(tx graph.Transaction) error {
-			fetchedNodeIDs, err := ops.FetchNodeIDs(tx.Nodes())
-
-			nodeIDs = append(nodeIDs, fetchedNodeIDs...)
-			return err
-		},
-	); err != nil {
-		return fmt.Errorf("error fetching all nodes: %w", err)
-	} else if err := graphDB.BatchOperation(ctx, func(batch graph.Batch) error {
-		for _, nodeId := range nodeIDs {
-			// deleting a node also deletes all of its edges due to a sql trigger
-			if err := batch.DeleteNode(nodeId); err != nil {
-				return err
+	operation.SubmitWriter(func(ctx context.Context, batch graph.Batch, inC <-chan graph.ID) error {
+		for {
+			if nextID, hasNextID := channels.Receive(ctx, inC); hasNextID {
+				if err := batch.DeleteRelationship(nextID); err != nil {
+					return err
+				}
+			} else {
+				break
 			}
 		}
+
 		return nil
-	}); err != nil {
-		return fmt.Errorf("error deleting all nodes: %w", err)
-	} else {
-		return nil
+	})
+
+	operation.SubmitReader(func(ctx context.Context, tx graph.Transaction, outC chan<- graph.ID) error {
+		return tx.Relationships().FetchIDs(func(cursor graph.Cursor[graph.ID]) error {
+			channels.PipeAll(ctx, cursor.Chan(), outC)
+			return cursor.Error()
+		})
+	})
+
+	if err := operation.Done(); err != nil {
+		return fmt.Errorf("error deleting graph relationships: %w", err)
 	}
+
+	operation = ops.StartNewOperation[graph.ID](ops.OperationContext{
+		Parent:     ctx,
+		DB:         graphDB,
+		NumReaders: 1,
+		NumWriters: 1,
+	})
+
+	operation.SubmitWriter(func(ctx context.Context, batch graph.Batch, inC <-chan graph.ID) error {
+		for {
+			if nextID, hasNextID := channels.Receive(ctx, inC); hasNextID {
+				if err := batch.DeleteNode(nextID); err != nil {
+					return err
+				}
+			} else {
+				break
+			}
+		}
+
+		return nil
+	})
+
+	operation.SubmitReader(func(ctx context.Context, tx graph.Transaction, outC chan<- graph.ID) error {
+		return tx.Nodes().FetchIDs(func(cursor graph.Cursor[graph.ID]) error {
+			channels.PipeAll(ctx, cursor.Chan(), outC)
+			return cursor.Error()
+		})
+	})
+
+	if err := operation.Done(); err != nil {
+		return fmt.Errorf("error deleting graph nodes: %w", err)
+	}
+
+	return nil
 }
