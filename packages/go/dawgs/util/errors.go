cbd8b28b6298a021eabf4024029a2fe1e445b2e6
James Barnett <jbarnett@specterops.io>
2024-03-27 12:19:25 -0400

    fix: IsNeoTimeoutError should handle graph.Error properly

--- packages/go/dawgs/util/errors.go
+++ packages/go/dawgs/util/errors.go
@@ -1,26 +1,28 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 package util
 
 import (
 	"errors"
-	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"strings"
 	"sync"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/specterops/bloodhound/dawgs/graph"
 )
 
 type ErrorCollector interface {
@@ -58,9 +60,14 @@ func (s *errorCollector) Combined() error {
 }
 
 func IsNeoTimeoutError(err error) bool {
-	if castError, ok := err.(*neo4j.Neo4jError); !ok {
+	switch e := err.(type) {
+	case *neo4j.Neo4jError:
+		return strings.Contains(e.Code, "TransactionTimedOut")
+	case graph.Error:
+		return strings.Contains(e.Error(), "Neo.ClientError.Transaction.TransactionTimedOut")
+	case *graph.Error:
+		return strings.Contains(e.Error(), "Neo.ClientError.Transaction.TransactionTimedOut")
+	default:
 		return false
-	} else {
-		return strings.Contains(castError.Code, "TransactionTimedOut")
 	}
 }
