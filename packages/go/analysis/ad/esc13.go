98d67b1154e20c393cce61d7124d5dcd64589edc
rvazarkar <rvazarkar@specterops.io>
2024-04-04 15:40:45 -0400

    faet: esc13 post initial

--- packages/go/analysis/ad/esc13.go
+++ packages/go/analysis/ad/esc13.go
@@ -23,14 +23,45 @@ import (
 	"github.com/specterops/bloodhound/dawgs/graph"
 	"github.com/specterops/bloodhound/dawgs/ops"
 	"github.com/specterops/bloodhound/dawgs/query"
+	"github.com/specterops/bloodhound/dawgs/util/channels"
 	"github.com/specterops/bloodhound/graphschema/ad"
+	"github.com/specterops/bloodhound/log"
 )
 
-func PostADCSESC13(ctx context.Context, tx graph.Transaction, outC chan<- analysis.CreatePostRelationshipJob, groupExpansions impact.PathAggregator, eca, domain *graph.Node, cache ADCSCache) error {
+func PostADCSESC13(ctx context.Context, tx graph.Transaction, outC chan<- analysis.CreatePostRelationshipJob, groupExpansions impact.PathAggregator, eca *graph.Node, cache ADCSCache) error {
 	if publishedCertTemplates, ok := cache.PublishedTemplateCache[eca.ID]; !ok {
 		return nil
 	} else {
+		for _, template := range publishedCertTemplates {
+			if isValid, err := isCertTemplateValidForESC13(template); err != nil {
+				log.Errorf("error checking esc13 cert template: %v", err)
+			} else if !isValid {
+				continue
+			} else if groupNodes, err := getCertTemplateGroupLinks(template, tx); err != nil {
+				log.Errorf("error getting cert template group links: %v", err)
+			} else if len(groupNodes) == 0 {
+				continue
+			} else {
+				controlBitmap := CalculateCrossProductNodeSets(groupExpansions, cache.CertTemplateEnrollers[template.ID], cache.EnterpriseCAEnrollers[eca.ID])
+				if filtered, err := filterUserDNSResults(tx, controlBitmap, template); err != nil {
+					log.Warnf("error filtering users from victims for esc13: %v", err)
+					continue
+				} else {
+					for _, groupID := range groupNodes.IDs() {
+						filtered.Each(func(value uint32) bool {
+							channels.Submit(ctx, outC, analysis.CreatePostRelationshipJob{
+								FromID: graph.ID(value),
+								ToID:   groupID,
+								Kind:   ad.ADCSESC13,
+							})
+							return true
+						})
+					}
+				}
+			}
+		}
 
+		return nil
 	}
 }
 
@@ -54,12 +85,29 @@ func isCertTemplateValidForESC13(ct *graph.Node) (bool, error) {
 	}
 }
 
-func certTemplateHasGroupLink(ct *graph.Node, tx graph.Transaction) (bool, error) {
+func getCertTemplateGroupLinks(ct *graph.Node, tx graph.Transaction) (graph.NodeSet, error) {
 	if policyNodes, err := ops.FetchEndNodes(tx.Relationships().Filterf(func() graph.Criteria {
 		return query.And(
 			query.InIDs(query.Start(), ct.ID),
 			query.KindIn(query.Relationship(), ad.ExtendedByPolicy),
-			query.KindIn(query.End(), ad.Issua)
+			query.KindIn(query.End(), ad.IssuancePolicy),
+		)
+	})); err != nil {
+		return graph.NodeSet{}, err
+	} else if len(policyNodes) == 0 {
+		return graph.NodeSet{}, nil
+	} else {
+		policyNodeIDs := policyNodes.IDs()
+		if groupNodes, err := ops.FetchEndNodes(tx.Relationships().Filterf(func() graph.Criteria {
+			return query.And(
+				query.InIDs(query.Start(), policyNodeIDs...),
+				query.KindIn(query.Relationship(), ad.OIDGroupLink),
+				query.KindIn(query.End(), ad.Group),
 			)
-	}))
+		})); err != nil {
+			return graph.NodeSet{}, err
+		} else {
+			return groupNodes, nil
+		}
+	}
 }
