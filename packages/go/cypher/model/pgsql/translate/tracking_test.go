de586baac89b9467578daeeece183e86d6d2ad23
Brandon Shearin <bshearin@specterops.io>
2024-04-25 10:58:10 -0600

    tracked identifier tests

--- packages/go/cypher/model/pgsql/translate/tracking_test.go
+++ packages/go/cypher/model/pgsql/translate/tracking_test.go
@@ -37,6 +37,7 @@ func TestIdentifierGenerator(t *testing.T) {
 	require.Equal(t, pgsql.Identifier("e1"), identifier)
 }
 
+// todo: wtf is constraint tracker
 func TestConstraintTracker(t *testing.T) {
 
 	tracker := translate.NewConstraintTracker()
@@ -63,4 +64,69 @@ func TestConstraintTracker(t *testing.T) {
 	result, expr := tracker.Consume(deps)
 
 	fmt.Println(result, expr)
+}
+
+func TestTrackedIdentifier(t *testing.T) {
+
+	var (
+		trackedNodeIdent = translate.TrackedIdentifier{
+			Identifier: pgsql.Identifier("whatsup"),
+			DataType:   pgsql.NodeComposite,
+		}
+
+		expectedNodeComposite = pgsql.CompositeValue{
+			Values: []pgsql.Expression{
+				pgsql.CompoundIdentifier{trackedNodeIdent.Identifier, pgsql.ColumnID},
+				pgsql.CompoundIdentifier{trackedNodeIdent.Identifier, pgsql.ColumnKindIDs},
+				pgsql.CompoundIdentifier{trackedNodeIdent.Identifier, pgsql.ColumnProperties},
+			},
+			DataType: pgsql.NodeComposite,
+		}
+
+		trackedEdgeIdent = translate.TrackedIdentifier{
+			Identifier: pgsql.Identifier("whatsup"),
+			DataType:   pgsql.EdgeComposite,
+		}
+
+		expectedEdgeComposite = pgsql.CompositeValue{
+			Values: []pgsql.Expression{
+				pgsql.CompoundIdentifier{trackedEdgeIdent.Identifier, pgsql.ColumnID},
+				pgsql.CompoundIdentifier{trackedEdgeIdent.Identifier, pgsql.ColumnStartID},
+				pgsql.CompoundIdentifier{trackedEdgeIdent.Identifier, pgsql.ColumnEndID},
+				pgsql.CompoundIdentifier{trackedEdgeIdent.Identifier, pgsql.ColumnKindID},
+				pgsql.CompoundIdentifier{trackedEdgeIdent.Identifier, pgsql.ColumnProperties},
+			},
+			DataType: pgsql.EdgeComposite,
+		}
+	)
+
+	// >>> build FromClause
+	fromClauses, err := trackedNodeIdent.BuildFromClauses()
+	require.Nil(t, err)
+	require.Equal(t,
+		[]pgsql.FromClause{{
+			Relation: pgsql.TableReference{
+				Name: pgsql.CompoundIdentifier{
+					trackedNodeIdent.Identifier,
+				},
+			},
+		}},
+		fromClauses,
+	)
+
+	// >>> build CompositeValue
+	// then result of this func is typically used in a top-level select like: `select (<trackedIdent>.id, <trackedIdent>.kind_ids, <trackedIdent>.properties)::nodecomposite as <alias>` where <trackedIdent> is likely to be of the form n0, n1, ... nX`
+	compositeNodeValue, err := trackedNodeIdent.BuildCompositeValue()
+	require.Nil(t, err)
+	require.Equal(t, expectedNodeComposite, compositeNodeValue)
+
+	compositeEdgeValue, err := trackedEdgeIdent.BuildCompositeValue()
+	require.Nil(t, err)
+	require.Equal(t, expectedEdgeComposite, compositeEdgeValue)
+
+	// >>> build Projection
+	proj, err := trackedNodeIdent.BuildProjection()
+	require.Nil(t, err)
+	require.Equal(t, expectedNodeComposite, proj)
+
 }
