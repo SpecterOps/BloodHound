5af7379690032a27d4c4093c90b3a2fbbc168cdc
John Hopper <jhopper@specterops.io>
2024-05-08 16:04:03 -0700

    BED-4377 feat: add parameter parsing support to cypher frontend

--- packages/go/cypher/frontend/expression.go
+++ packages/go/cypher/frontend/expression.go
@@ -1,17 +1,17 @@
 // Copyright 2023 Specter Ops, Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 // SPDX-License-Identifier: Apache-2.0
 
 package frontend
@@ -53,14 +53,8 @@ func (s *PropertiesVisitor) EnterOC_Parameter(ctx *parser.OC_ParameterContext) {
 }
 
 func (s *PropertiesVisitor) ExitOC_Parameter(ctx *parser.OC_ParameterContext) {
-	if symbolicName := s.ctx.Exit().(*SymbolicNameOrReservedWordVisitor).Name; symbolicName == "" {
-		s.Properties.Parameter = &model.Parameter{
-			Symbol: strings.TrimPrefix(ctx.GetText(), "$"),
-		}
-	} else {
-		s.Properties.Parameter = &model.Parameter{
-			Symbol: symbolicName,
-		}
+	s.Properties.Parameter = &model.Parameter{
+		Symbol: extractParameterSymbol(s.ctx, ctx),
 	}
 }
 
