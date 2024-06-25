425bce7923437ceaad4c73e371e34b2709db4d6a
Stephen Hinck <shinck@specterops.io>
2024-06-25 12:33:13 -0700

    data quality docs

--- cmd/ui/src/views/QA/QA.tsx
+++ cmd/ui/src/views/QA/QA.tsx
@@ -14,7 +14,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { Alert, AlertTitle, Box, Grid, Link } from '@mui/material';
+import { Alert, AlertTitle, Box, Grid, Link, Typography } from '@mui/material';
 import {
     ActiveDirectoryPlatformInfo,
     AzurePlatformInfo,
@@ -102,6 +102,11 @@ const QualityAssurance: React.FC = () => {
                     }}
                 />
             }>
+            <Typography variant='body1'>
+                <p>
+                Understand the data collected within BloodHound broken down by environment and principal type.
+                </p>
+            </Typography>
             {dataError && (
                 <Box paddingBottom={2}>
                     <Alert severity='warning'>
