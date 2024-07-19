361632695c1185f7f9ce4ea081e67881a7180288
Ben Waples <bwaples@specterops.io>
2024-07-19 16:57:35 -0700

    chore: update comment

--- cmd/ui/src/views/EarlyAccessFeatures/EarlyAccessFeatures.tsx
+++ cmd/ui/src/views/EarlyAccessFeatures/EarlyAccessFeatures.tsx
@@ -159,7 +159,7 @@ const EarlyAccessFeatures: React.FC = () => {
                                     <EarlyAccessFeatureToggle
                                         flag={flag}
                                         onClick={(flagId) => {
-                                            // TODO: Consider adding flag keys to cue files so we don't use random strings. Consider adding more flexibility/composability to side effects for toggling feature flags on and off
+                                            // TODO: Consider adding more flexibility/composability to side effects for toggling feature flags on and off
                                             if (flag.key === 'dark_mode') {
                                                 dispatch(setDarkMode(false));
                                             }
