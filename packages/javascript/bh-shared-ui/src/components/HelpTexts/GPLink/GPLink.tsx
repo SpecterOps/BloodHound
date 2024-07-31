a5b5f78d7282f3cebec2dad7850970353a588316
jknudsen <jknudsen@specterops.io>
2024-07-31 15:10:11 +0200

    docs: gpo abuse info

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/GPLink/GPLink.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/GPLink/GPLink.tsx
@@ -15,13 +15,15 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import General from './General';
-import Abuse from './Abuse';
+import WindowsAbuse from './WindowsAbuse';
+import LinuxAbuse from './LinuxAbuse';
 import Opsec from './Opsec';
 import References from './References';
 
 const GPLink = {
     general: General,
-    abuse: Abuse,
+    windowsAbuse: WindowsAbuse,
+    linuxAbuse: LinuxAbuse,
     opsec: Opsec,
     references: References,
 };
