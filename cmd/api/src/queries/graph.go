64d66b182531e7a6810397aebcc88633d6a09547
Stephanie Lamb <slamb@specterops.io>
2024-09-19 11:36:36 -0500

    updated file to use errors package instead of bloodhounf/errors

--- cmd/api/src/queries/graph.go
+++ cmd/api/src/queries/graph.go
@@ -21,7 +21,7 @@ package queries
 import (
 	"bytes"
 	"context"
-	e "errors"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -47,7 +47,6 @@ import (
 	"github.com/specterops/bloodhound/dawgs/graph"
 	"github.com/specterops/bloodhound/dawgs/ops"
 	"github.com/specterops/bloodhound/dawgs/query"
-	"github.com/specterops/bloodhound/errors"
 	"github.com/specterops/bloodhound/graphschema/ad"
 	"github.com/specterops/bloodhound/graphschema/azure"
 	"github.com/specterops/bloodhound/graphschema/common"
@@ -84,7 +83,7 @@ type EntityQueryParameters struct {
 
 func GetEntityObjectIDFromRequestPath(request *http.Request) (string, error) {
 	if id, hasID := mux.Vars(request)["object_id"]; !hasID {
-		return "", errors.Error("no object ID found in request")
+		return "", errors.New("no object ID found in request")
 	} else {
 		return id, nil
 	}
@@ -637,7 +636,7 @@ func (s *GraphQuery) GetEntityCountResults(ctx context.Context, node *graph.Node
 		go func(delegateKey string, delegate any) {
 			defer waitGroup.Done()
 
-			if result, err := runEntityQuery(ctx, s.Graph, delegate, node, 0, 0); e.Is(err, graph.ErrContextTimedOut) {
+			if result, err := runEntityQuery(ctx, s.Graph, delegate, node, 0, 0); errors.Is(err, graph.ErrContextTimedOut) {
 				log.Warnf("Running entity query for key %s: %v", delegateKey, err)
 			} else if err != nil {
 				log.Errorf("Error running entity query for key %s: %v", delegateKey, err)
