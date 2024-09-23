991981aac46ba230e3c16619a7226d32e12394f0
Michael Lipka <mvlipka@gmail.com>
2024-09-23 15:43:21 -0500

    BED-4851 added migration

--- cmd/api/src/database/migration/migrations/v6.0.0.sql
+++ cmd/api/src/database/migration/migrations/v6.0.0.sql
@@ -54,3 +54,17 @@ $$
     when undefined_table then null;
   end
 $$;
+
+-- OIDC Provider
+CREATE TABLE IF NOT EXISTS oidc_providers
+(
+  id         BIGSERIAL PRIMARY KEY,
+  name       TEXT NOT NULL,
+  client_id  TEXT NOT NULL,
+  login_url  TEXT NOT NULL,
+
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+
+  UNIQUE (name)
+)
