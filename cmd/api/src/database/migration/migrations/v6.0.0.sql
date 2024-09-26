88d018662690a6bb978c00d26484cf821b9746d5
Michael Lipka <mvlipka@gmail.com>
2024-09-26 12:12:25 -0500

    BED-4851 changed migration to 6.1.0

--- cmd/api/src/database/migration/migrations/v6.0.0.sql
+++ cmd/api/src/database/migration/migrations/v6.0.0.sql
@@ -78,16 +78,3 @@ $$
   end
 $$;
 
--- OIDC Provider
-CREATE TABLE IF NOT EXISTS oidc_providers
-(
-  id         BIGSERIAL PRIMARY KEY,
-  name       TEXT NOT NULL,
-  client_id  TEXT NOT NULL,
-  issuer     TEXT NOT NULL,
-
-  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
-
-  UNIQUE (name)
-)
