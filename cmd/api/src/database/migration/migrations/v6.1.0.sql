9faae07582eafefb39246f094b4fb35d7b5cb047
Rohan Vazarkar <rvazarkar@specterops.io>
2024-09-30 15:52:44 -0400

    chore: move migration to correct file

--- cmd/api/src/database/migration/migrations/v6.1.0.sql
+++ cmd/api/src/database/migration/migrations/v6.1.0.sql
@@ -11,3 +11,8 @@ CREATE TABLE IF NOT EXISTS oidc_providers
 
   UNIQUE (name)
 )
+
+
+-- Add Scheduled Analysis Configs
+  INSERT INTO parameters (key, name, description, value, created_at, updated_at)
+  VALUES ('analysis.scheduled', 'Scheduled Analysis', 'This configuration parameter allows setting a schedule for analysis. When enabled, analysis will only run when the scheduled time arrives','{"enabled": false, "rrule": "", "last_run": "2006-01-02T15:04:05Z"}',current_timestamp,current_timestamp) ON CONFLICT DO NOTHING;
