f638badf67e488c98b26ed73b74f9cb3cdced051
Mistah J <26472282+mistahj67@users.noreply.github.com>
2024-10-11 09:25:31 -0700

    feat: add callback handler for OIDC

--- cmd/api/src/api/v2/auth/sso.go
+++ cmd/api/src/api/v2/auth/sso.go
@@ -177,3 +177,26 @@ func (s ManagementResource) SSOLoginHandler(response http.ResponseWriter, reques
 		}
 	}
 }
+
+func (s ManagementResource) SSOCallbackHandler(response http.ResponseWriter, request *http.Request) {
+	ssoProviderSlug := mux.Vars(request)[api.URIPathVariableSSOProviderSlug]
+	log.Debugf("HERE I AM IN CALLBACK - provider %s", ssoProviderSlug)
+
+	if ssoProvider, err := s.db.GetSSOProviderBySlug(request.Context(), ssoProviderSlug); err != nil {
+		api.HandleDatabaseError(request, response, err)
+	} else {
+		switch ssoProvider.Type {
+		case model.SessionAuthProviderSAML:
+			//todo handle saml callback
+			api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusNotFound, api.ErrorResponseDetailsResourceNotFound, request), response)
+		case model.SessionAuthProviderOIDC:
+			if oidcProvider, err := s.db.GetOIDCProviderBySSOProviderID(request.Context(), ssoProvider.ID); err != nil {
+				api.HandleDatabaseError(request, response, err)
+			} else {
+				s.OIDCCallbackHandler(response, request, ssoProvider, oidcProvider)
+			}
+		default:
+			api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusNotFound, api.ErrorResponseDetailsResourceNotFound, request), response)
+		}
+	}
+}
