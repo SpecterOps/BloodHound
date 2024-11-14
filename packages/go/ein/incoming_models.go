ec69260d6a8a5a8c0deefea961cd9ab9547e78a4
Marc André Tanner <mat@brain-dump.org>
2024-11-14 14:14:14 +0100

    feat: parse GPO changes for local group membership
    While collected by SharpHound, the GPOChanges node of OUs was not taken
    into account to generate local group membership relations.
    
    For every affected computer, new ADLocalGroup nodes with corresponding
    LocalToComputer and MemberOfLocalGroup edges are generated.
    
    This should fix issue #280.

--- packages/go/ein/incoming_models.go
+++ packages/go/ein/incoming_models.go
@@ -310,8 +310,17 @@ type Computer struct {
 	NTLMRegistryData        RegistryDataAPIResult
 }
 
+type GPOChanges struct {
+	LocalAdmins        []TypedPrincipal
+	RemoteDesktopUsers []TypedPrincipal
+	DcomUsers          []TypedPrincipal
+	PSRemoteUsers      []TypedPrincipal
+	AffectedComputers  []TypedPrincipal
+}
+
 type OU struct {
 	IngestBase
 	ChildObjects []TypedPrincipal
 	Links        []GPLink
+	GPOChanges   GPOChanges
 }
