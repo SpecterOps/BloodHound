579acacc6abccd9ccdb0e32f8e7588d43b3e625b
ykaiboussiSO <ykaiboussi@specterops.io>
2024-12-20 12:19:09 -0600

    update comment

--- cmd/e2e/src/helpers/hashpassword.ts
+++ cmd/e2e/src/helpers/hashpassword.ts
@@ -4,7 +4,7 @@ import { hash, argon2id } from 'argon2';
  * @param password a plain-text password
  *
  * Returns a string suitable for use as the 'digest' of an auth_secrets record in the postgres db
- * Matches the same algorithm used by the BHE API when creating or updating auth_secrets
+ * Matches the same algorithm used by the BH API when creating or updating auth_secrets
  */
 const hashPassword = async (password: string) => {
     const hashedPassword = await hash(password, {
