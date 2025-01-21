0fe603f59f661612501350bb27ab4a238a822642
Francisco Quintero Abbo <fquintero@specterops.io>
2025-01-21 14:50:05 -0500

    chore: added useApiVersion hook

--- packages/javascript/bh-shared-ui/src/components/index.ts
+++ packages/javascript/bh-shared-ui/src/components/index.ts
@@ -14,8 +14,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-export { default as ApiVersion } from './ApiVersion';
-
 export * from './AppNotifications';
 export { default as AppNotifications } from './AppNotifications';
 
--- packages/javascript/bh-shared-ui/src/hooks/index.ts
+++ packages/javascript/bh-shared-ui/src/hooks/index.ts
@@ -22,6 +22,8 @@ export { default as useDebouncedValue } from './useDebouncedValue';
 
 export { default as useToggle } from './useToggle';
 
+export { default as useApiVersion } from './useApiVersion';
+
 export * from './useConfiguration';
 
 export * from './useDataQualityStats';
