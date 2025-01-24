67a10ccbe7d27afacd6bead933e52d060cad52f8
Francisco Quintero Abbo <fquintero@specterops.io>
2025-01-24 13:55:45 -0500

    wip: structure for main nav tests

--- packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.test.tsx
+++ packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.test.tsx
@@ -14,6 +14,28 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 describe('MainNav', () => {
-    it.todo('test', () => {});
+    it.todo('should render a logo and two lists', () => {});
+    it.todo('should render a navigation list item', () => {});
+    it.todo('should render action list item that handles a function', () => {});
+    it.todo('should render a version number when collapsed', () => {});
+    it.todo('should render a label and version number when expanded', () => {});
+    it.todo('should only render an icon in list item when collapsed', () => {});
+    it.todo('should render an icon and label in list item when expanded', () => {});
 });
