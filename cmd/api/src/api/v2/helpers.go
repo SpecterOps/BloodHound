d1cf61b91694c25c8847e7439491385931b157bb
Stephanie Lamb <slamb@specterops.io>
2025-01-28 14:37:50 -0600

    readded limit check but included -1 for all responses from db

--- cmd/api/src/api/v2/helpers.go
+++ cmd/api/src/api/v2/helpers.go
@@ -74,6 +74,8 @@ func ParseLimitQueryParameter(params url.Values, defaultValue int) (int, error) 
 		return defaultValue, nil
 	} else if limit, err := strconv.Atoi(param); err != nil {
 		return 0, fmt.Errorf("error converting limit value %v to int: %v", param, err)
+	} else if limit < -1 {
+		return 0, fmt.Errorf(utils.ErrorInvalidLimit, limit)
 	} else {
 		return limit, nil
 	}
