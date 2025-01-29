13c6be5e55eb405af6a8477c19d4ea1f8c2a74bd
Elijah Brown <ebrown@specterops.io>
2025-01-29 14:15:55 -0700

    Clean up

--- cmd/api/src/daemons/datapipe/convertors.go
+++ cmd/api/src/daemons/datapipe/convertors.go
@@ -17,7 +17,6 @@
 package datapipe
 
 import (
-	"log/slog"
 	"strings"
 
 	"github.com/specterops/bloodhound/ein"
@@ -37,13 +36,9 @@ func convertComputerData(computer ein.Computer, converted *ConvertedData) {
 
 	converted.RelProps = append(converted.RelProps, ein.ParseComputerMiscData(computer)...)
 	for _, localGroup := range computer.LocalGroups {
-		if !localGroup.Collected || len(localGroup.Results) == 0 {
-			if strings.HasPrefix(strings.ToLower(localGroup.Name), "direct access users") {
-				slog.Info("Saving Direct Access Users group", "localGroup.Name", localGroup.Name)
-
-			} else {
-				continue
-			}
+		// throw away groups with 0 members unless the group name begins with "DIRECT ACCESS USERS"
+		if !localGroup.Collected || (len(localGroup.Results) == 0 && !strings.HasPrefix(strings.ToUpper(localGroup.Name), "DIRECT ACCESS USERS")) {
+			continue
 		}
 
 		parsedLocalGroupData := ein.ConvertLocalGroup(localGroup, computer)
