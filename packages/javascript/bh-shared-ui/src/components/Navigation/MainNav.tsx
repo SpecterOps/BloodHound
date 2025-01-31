a4bf55da5e14bad6452a074d3bcbced0c85bd374
Francisco Quintero Abbo <fquintero@specterops.io>
2025-01-31 15:36:34 -0500

    fix: reduced width of version number when expanded

--- packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.tsx
+++ packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.tsx
@@ -103,7 +103,7 @@ const MainNavVersionNumber: FC = () => {
         <div className='relative w-full flex min-h-10 h-10' data-testid='main-nav-version-number'>
             <div
                 className={
-                    'w-9 break-all group-hover:w-full group-hover:whitespace-nowrap flex absolute top-3 left-3 duration-300 ease-in-out text-xs font-medium text-neutral-dark-0 dark:text-neutral-light-1 group-hover:left-16'
+                    'w-9 break-all group-hover:w-auto group-hover:overflow-x-hidden group-hover:whitespace-nowrap flex absolute top-3 left-3 duration-300 ease-in-out text-xs font-medium text-neutral-dark-0 dark:text-neutral-light-1 group-hover:left-16'
                 }>
                 <span className={'opacity-0 hidden duration-300 ease-in-out group-hover:opacity-100 group-hover:block'}>
                     BloodHound:&nbsp;
