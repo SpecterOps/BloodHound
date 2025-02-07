702e4cb4f55e12fde90dd5403ab8b3e7f1b6a0c6
Mistah J <26472282+mistahj67@users.noreply.github.com>
2025-02-07 11:27:21 -0700

    chore: nits

--- cmd/api/src/database/auth_test.go
+++ cmd/api/src/database/auth_test.go
@@ -537,8 +537,8 @@ func TestDatabase_CreateUserSession(t *testing.T) {
 		assert.Equal(t, user, newUserSession.User)
 	}
 	user, err := dbInst.GetUser(testCtx, user.ID)
-	assert.Nil(t, err)
-	assert.True(t, !user.LastLogin.IsZero(), "User last login date was not set")
+	assert.NoError(t, err)
+	assert.False(t, user.LastLogin.IsZero(), "User last login date was not set")
 }
 
 func TestDatabase_SetUserSessionFlag(t *testing.T) {
