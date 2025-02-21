9b940bc39c7f9a0fdcca13873d6e0f80522cfa0b
ALCooper12 <acooper@specterops.io>
2025-02-21 14:11:25 -0800

    Added the ulule/limiter library(module) to replace tollbooth for implementing our current rate limiting middleware logic

--- cmd/api/src/api/middleware/rate_limit.go
+++ cmd/api/src/api/middleware/rate_limit.go
@@ -17,66 +17,44 @@
 package middleware
 
 import (
+	"log/slog"
 	"net/http"
+	"strings"
 	"time"
 
-	"github.com/specterops/bloodhound/headers"
-
-	"github.com/didip/tollbooth/v6"
-	"github.com/didip/tollbooth/v6/limiter"
+	//"github.com/didip/tollbooth/v6"
+	//"github.com/didip/tollbooth/v6/limiter"
 	"github.com/gorilla/mux"
-	"github.com/specterops/bloodhound/src/api"
+	"github.com/ulule/limiter/v3"
+	"github.com/ulule/limiter/v3/drivers/middleware/stdlib"
+	"github.com/ulule/limiter/v3/drivers/store/memory"
 )
 
 // DefaultRateLimit is the default number of allowed requests per second
 const DefaultRateLimit = 55
 
-// RateLimitHandler returns a http.Handler that limits the rate of requests
-// for a given handler
-//
-// Usage:
-//
-//	limiter := tollbooth.NewLimiter(1, nil).
-//		SetMethods([]string{"GET", "POST"}).
-//		...configure tollbooth Limiter ...
-//
-//	router.Handle("/teapot", RateLimitHandler(limiter, handler))
-func RateLimitHandler(limiter *limiter.Limiter, handler http.Handler) http.Handler {
-	return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
-		if err := tollbooth.LimitByRequest(limiter, response, request); err != nil {
-			// In case SetOnLimitReached was called
-			limiter.ExecOnLimitReached(response, request)
-
-			api.WriteErrorResponse(request.Context(), &api.ErrorWrapper{
-				HTTPStatus: err.StatusCode,
-				Timestamp:  time.Now(),
-				RequestID:  request.Header.Get(headers.RequestID.String()),
-				Errors: []api.ErrorDetails{
-					{
-						Context: "middleware",
-						Message: err.Error(),
-					},
-				},
-			}, response)
-		} else {
-			handler.ServeHTTP(response, request)
-		}
-	})
-}
-
 // RateLimitMiddleware is a convenience function for creating rate limiting middleware
-//
-// Usage:
-//
-//	limiter := tollbooth.NewLimiter(1, nil).
-//		SetMethods([]string{"GET", "POST"}).
-//		...configure tollbooth Limiter ...
 //
 //	router.Use(RateLimitMiddleware(limiter))
 func RateLimitMiddleware(limiter *limiter.Limiter) mux.MiddlewareFunc {
-	return func(next http.Handler) http.Handler {
-		return RateLimitHandler(limiter, next)
-	}
+	ipGetter := stdlib.WithKeyGetter(
+		func(r *http.Request) string {
+			if xff := r.Header.Get("X-Forwarded-For"); xff == "" {
+				slog.DebugContext(r.Context(), "No data found in X-Forwarded-For header")
+				return r.RemoteAddr
+			} else {
+				ips := strings.Split(xff, ",")
+				rightMostIP := strings.TrimSpace(ips[len(ips)-1])
+
+				return rightMostIP
+			}
+		},
+	)
+
+	middleware := stdlib.NewMiddleware(limiter, ipGetter)
+
+	return middleware.Handler
+
 }
 
 // DefaultRateLimitMiddleware is a convenience function for creating the default rate limiting middleware
@@ -86,6 +64,14 @@ func RateLimitMiddleware(limiter *limiter.Limiter) mux.MiddlewareFunc {
 //
 //	router.Use(DefaultRateLimitMiddleware())
 func DefaultRateLimitMiddleware() mux.MiddlewareFunc {
-	limiter := tollbooth.NewLimiter(DefaultRateLimit, nil)
-	return RateLimitMiddleware(limiter)
+	rate := limiter.Rate{
+		Period: 1 * time.Second,
+		Limit:  DefaultRateLimit,
+	}
+
+	store := memory.NewStore()
+
+	instance := limiter.New(store, rate, limiter.WithTrustForwardHeader(false))
+
+	return RateLimitMiddleware(instance)
 }
