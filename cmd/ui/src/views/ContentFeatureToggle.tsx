e54f678a200bdf5813126530669a879c3415270e
Ben Waples <bwaples@specterops.io>
2025-02-25 08:54:24 -0800

    fix: stop content feature flag running on login

--- cmd/ui/src/views/ContentFeatureToggle.tsx
+++ cmd/ui/src/views/ContentFeatureToggle.tsx
@@ -1,10 +1,18 @@
 import { useFeatureFlag } from 'bh-shared-ui';
 import React from 'react';
+import { fullyAuthenticatedSelector } from 'src/ducks/auth/authSlice';
+import { useAppSelector } from 'src/store';
 import Content from './Content';
 import ContentV2 from './ContentV2';
 
 const ContentFeatureToggle: React.FC = () => {
-    const { data: flag } = useFeatureFlag('back_button_support');
+    const authState = useAppSelector((state) => state.auth);
+    const fullyAuthenticated = useAppSelector(fullyAuthenticatedSelector);
+
+    const { data: flag } = useFeatureFlag('enable_back_button', {
+        // block this feature flag check from running on login page
+        enabled: !!authState.isInitialized && fullyAuthenticated,
+    });
 
     return flag?.enabled ? <ContentV2 /> : <Content />;
 };
