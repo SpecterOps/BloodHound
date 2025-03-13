ed25b1aa9e14872e39dac0293bf1dd3f422e7e85
Eli K Miller <emiller@specterops.io>
2025-03-12 23:17:35 -0500

    chore: update back_button_support flag description

--- cmd/api/src/database/migration/migrations/v7.2.0.sql
+++ cmd/api/src/database/migration/migrations/v7.2.0.sql
@@ -1,2 +1,5 @@
 -- Set `back_button_support` feature flag as user updatable
 UPDATE feature_flags SET user_updatable = true WHERE key = 'back_button_support';
+
+-- Specify the `back_button_support` feature flag is currently only for BHCE users
+UPDATE feature_flags SET description = 'Enable users to quickly navigate between views in a wider range of scenarios by utilizing the browser navigation buttons. Currently for BloodHound Community Edition users only.' WHERE key = 'back_button_support';
