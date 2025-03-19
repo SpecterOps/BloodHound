37f5762490d1d58c5700e62202e15e5bf1cad712
Michael Smith <msmith@specterops.io>
2025-03-19 15:14:23 -0400

    BED-5231 added dynamic details

---
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/DynamicDetails.test.tsx
@@ -0,0 +1,91 @@
+import { render, screen } from '../../test-utils';
+import DynamicDetails from './DynamicDetails';
+
+describe('DynamicDetails', () => {
+    it('renders details for a selected tier', () => {
+        const testQuery = {
+            asset_group_tier_id: 9,
+            count: 9374,
+            require_certify: true,
+            created_at: '2024-09-08T03:38:22.791Z',
+            created_by: 'Franz.Smitham@yahoo.com',
+            deleted_at: '2025-02-03T18:32:36.669Z',
+            deleted_by: 'Vita.Hermann97@yahoo.com',
+            description: 'pique International',
+            id: 9,
+            kind_id: 59514,
+            name: 'Tier-8',
+            updated_at: '2024-07-26T02:15:04.556Z',
+            updated_by: 'Deontae34@hotmail.com',
+        };
+
+        render(<DynamicDetails data={testQuery} />);
+
+        expect(screen.getByText('Tier-8')).toBeInTheDocument();
+        expect(screen.getByText('pique International')).toBeInTheDocument();
+        expect(screen.getByText('Franz.Smitham@yahoo.com')).toBeInTheDocument();
+        expect(screen.getByText('7/25/2024')).toBeInTheDocument();
+    });
+
+    it('renders details for a selected selector and is of type "Cypher"', () => {
+        const testQuery = {
+            asset_group_label_id: 9,
+            selector_id: 1,
+            node_id: 1,
+            certified: 1,
+            certified_by: 'Test',
+            auto_certify: true,
+            count: 67369,
+            created_at: '2025-02-12T16:24:18.633Z',
+            created_by: 'Emery_Swift86@gmail.com',
+            description: 'North',
+            disabled_at: '2024-05-24T12:34:35.894Z',
+            disabled_by: 'Travon27@gmail.com',
+            id: 9,
+            is_default: true,
+            seeds: [],
+            name: 'tier-0-selector-9',
+            updated_at: '2024-11-25T11:34:45.894Z',
+            updated_by: 'Demario_Corwin88@yahoo.com',
+        };
+
+        render(<DynamicDetails data={testQuery} isCypher={true} />);
+
+        expect(screen.getByText('tier-0-selector-9')).toBeInTheDocument();
+        expect(screen.getByText('North')).toBeInTheDocument();
+        expect(screen.getByText('Emery_Swift86@gmail.com')).toBeInTheDocument();
+        expect(screen.getByText('11/25/2024')).toBeInTheDocument();
+        expect(screen.getByText('Cypher')).toBeInTheDocument();
+    });
+
+    it('renders details for a selected selector and is of type "Object"', () => {
+        const testQuery = {
+            asset_group_label_id: 9,
+            selector_id: 1,
+            node_id: 1,
+            certified: 1,
+            certified_by: 'Test',
+            auto_certify: true,
+            count: 67369,
+            seeds: [],
+            created_at: '2025-02-12T16:24:18.633Z',
+            created_by: 'Emery_Swift86@gmail.com',
+            description: 'North',
+            disabled_at: '2024-05-24T12:34:35.894Z',
+            disabled_by: 'Travon27@gmail.com',
+            id: 9,
+            is_default: true,
+            name: 'tier-0-selector-9',
+            updated_at: '2024-11-25T11:34:45.894Z',
+            updated_by: 'Demario_Corwin88@yahoo.com',
+        };
+
+        render(<DynamicDetails data={testQuery} />);
+
+        expect(screen.getByText('tier-0-selector-9')).toBeInTheDocument();
+        expect(screen.getByText('North')).toBeInTheDocument();
+        expect(screen.getByText('Emery_Swift86@gmail.com')).toBeInTheDocument();
+        expect(screen.getByText('11/25/2024')).toBeInTheDocument();
+        expect(screen.getByText('Object')).toBeInTheDocument();
+    });
+});
---
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/DynamicDetails.tsx
@@ -0,0 +1,60 @@
+import { Card } from '@bloodhoundenterprise/doodleui';
+import { AssetLabel, AssetSelector } from 'js-client-library';
+import { FC } from 'react';
+
+type DynamicDetailsProps = {
+    data: AssetSelector | AssetLabel | undefined;
+    isCypher?: boolean;
+};
+
+const isSelector = (data: any): data is AssetSelector => {
+    return 'seeds' in data;
+};
+
+const isLabel = (data: any): data is AssetLabel => {
+    return 'asset_group_tier_id' in data;
+};
+
+const DynamicDetails: FC<DynamicDetailsProps> = ({ data, isCypher }) => {
+    if (!data) {
+        return null;
+    }
+    const lastUpdated = new Date(data.updated_at).toLocaleDateString();
+
+    return (
+        <Card className='h-[280px] mb-[24px] px-6 pt-6 select-none overflow-y-auto'>
+            <div className='text-xl font-bold'>{data ? data?.name : 'Nothing Data'}</div>
+            <div className='flex flex-wrap gap-x-2'>
+                <p className='font-bold'>Description:</p> <p>{data?.description}</p>
+            </div>
+            <div className='flex flex-wrap gap-x-2'>
+                <p className='font-bold'>Created by:</p> <p>{data?.created_by}</p>
+            </div>
+            <div className='flex flex-wrap gap-x-2'>
+                <p className='font-bold'>Last Updated:</p> <p>{lastUpdated}</p>
+            </div>
+            {isLabel(data) && (
+                <div className='flex flex-wrap gap-x-2'>
+                    <p className='font-bold'>Certification Enabled:</p> <p>{data?.require_certify}</p>
+                </div>
+            )}
+            {isSelector(data) && (
+                <>
+                    <div className='flex flex-wrap gap-x-2'>
+                        <p className='font-bold'>Type:</p> <p>{isCypher ? 'Cypher' : 'Object'}</p>
+                    </div>
+                    <div className='flex flex-wrap gap-x-2'>
+                        <p className='font-bold'>Automatic Certification:</p>{' '}
+                        <p>{data?.auto_certify ? 'Enabled' : 'Disabled'}</p>
+                    </div>
+                    <div className='flex flex-wrap gap-x-2'>
+                        <p className='font-bold'>Selector Enabled:</p>{' '}
+                        <p>{data?.disabled_at ? 'Enabled' : 'Disabled'}</p>
+                    </div>
+                </>
+            )}
+        </Card>
+    );
+};
+
+export default DynamicDetails;
