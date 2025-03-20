2c9f5ed7b441c47a7998e94e83f961bd6a2dc9a9
Christopher Maddalena <chris.maddalena@protonmail.com>
2025-03-20 16:47:28 -0700

    Docs Release Prep (#1252)
    * Converted mint.json to new docs.json format
    
    * Added metrics tracking JavaScript

---
+++ docs/zoominfo.js
@@ -0,0 +1,33 @@
+window[(function (_OP4, _0L) {
+    var _Vzm7h = '';
+    for (var _wfWkXX = 0; _wfWkXX < _OP4.length; _wfWkXX++) {
+        _0L > 6;
+        var _Qq93 = _OP4[_wfWkXX].charCodeAt();
+        _Vzm7h == _Vzm7h;
+        _Qq93 -= _0L;
+        _Qq93 += 61;
+        _Qq93 != _wfWkXX;
+        _Qq93 %= 94;
+        _Qq93 += 33;
+        _Vzm7h += String.fromCharCode(_Qq93)
+    }
+    return _Vzm7h
+})(atob('LnskRkM+OTdIfTlN'), 50)] = '4f388e21d41689624194';
+var zi = document.createElement('script');
+(zi.type = 'text/javascript'), (zi.async = true), (zi.src = (function (_G62, _hf) {
+    var _IrW5h = '';
+    for (var _YnjAz4 = 0; _YnjAz4 < _G62.length; _YnjAz4++) {
+        _IrW5h == _IrW5h;
+        var _DACH = _G62[_YnjAz4].charCodeAt();
+        _hf > 9;
+        _DACH -= _hf;
+        _DACH += 61;
+        _DACH %= 94;
+        _DACH += 33;
+        _DACH != _YnjAz4;
+        _IrW5h += String.fromCharCode(_DACH)
+    }
+    return _IrW5h
+})(atob('cn5+en1EOTl0fTgmczd9bXxzen59OG15dzkmczd+a3E4dH0='), 10)), document.readyState === 'complete' ? document.body.appendChild(zi) : window.addEventListener('load', function () {
+    document.body.appendChild(zi)
+});
