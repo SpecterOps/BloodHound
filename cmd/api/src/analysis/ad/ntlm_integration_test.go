04224729d885e09844a1f3c5c162f296e36ee440
Rohan Vazarkar <rvazarkar@users.noreply.github.com>
2025-03-20 15:32:02 -0400

    chore: fix an SCA error (#1251)

--- cmd/api/src/analysis/ad/ntlm_integration_test.go
+++ cmd/api/src/analysis/ad/ntlm_integration_test.go
@@ -427,7 +427,7 @@ func TestPostCoerceAndRelayNTLMToLDAP(t *testing.T) {
 						continue
 					} else if protectedUsersForDomain.Contains(innerComputer.ID.Uint64()) && !ldapSigningForDomain.IsValidFunctionalLevel {
 						continue
-					} else if webClientRunning, err := innerComputer.Properties.Get(ad.WebClientRunning.String()).Bool(); webClientRunning {
+					} else if webClientRunning, _ := innerComputer.Properties.Get(ad.WebClientRunning.String()).Bool(); webClientRunning {
 						continue
 					} else if err = ad2.PostCoerceAndRelayNTLMToLDAP(outC, innerComputer, authenticatedUserID, ldapSigningCache); err != nil {
 						t.Logf("failed post processing for %s: %v", ad.CoerceAndRelayNTLMToLDAP.String(), err)
