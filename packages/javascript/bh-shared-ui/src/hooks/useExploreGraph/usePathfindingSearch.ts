c0c590e199499c82838cb95c109791409c1368ce
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2025-03-20 11:09:14 -0400

    BED-5581: Remove extra browser history entries from pathfinding swap (#1239)
    * moved pathfinding swap functionality up into hook and added a test
    
    * remove extra dev logging

--- packages/javascript/bh-shared-ui/src/hooks/useExploreGraph/usePathfindingSearch.ts
+++ packages/javascript/bh-shared-ui/src/hooks/useExploreGraph/usePathfindingSearch.ts
@@ -108,6 +108,16 @@ export const usePathfindingSearch = () => {
         }
     };
 
+    const handleSwapPathfindingInputs = () => {
+        if (sourceSelectedItem && destinationSelectedItem) {
+            setExploreParams({
+                searchType: 'pathfinding',
+                primarySearch: destinationSelectedItem.objectid,
+                secondarySearch: sourceSelectedItem.objectid,
+            });
+        }
+    };
+
     // Handle changes internal to the search form that should not trigger a graph query. Each param should sync independently
     const handleSourceNodeEdited = (edit: string) => {
         setSourceSelectedItem(undefined);
@@ -128,5 +138,6 @@ export const usePathfindingSearch = () => {
         handleSourceNodeSelected,
         handleDestinationNodeEdited,
         handleDestinationNodeSelected,
+        handleSwapPathfindingInputs,
     };
 };
