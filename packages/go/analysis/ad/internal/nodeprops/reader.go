bd81d811a7f15a3aec47a93d1062fafa8dff638b
Kareem Hepburn <khepburn@specterops.io>
2025-03-25 17:06:12 -0500

    refactor(nodeprops): Simplify error handling in domain ID and name reader

--- packages/go/analysis/ad/internal/nodeprops/reader.go
+++ packages/go/analysis/ad/internal/nodeprops/reader.go
@@ -13,34 +13,34 @@ func ReadDomainIDandNameAsString(nodeToRead *graph.Node) (string, string, error)
 	var domainSIDStr, domainNameStr string
 	var err error
 
-	returnFunc := func(errMsg string) (string, string, error) {
-		return domainSIDStr, domainNameStr, fmt.Errorf("failed to read domain SID and name: %s", errMsg)
+	errMessageFunc := func(errMsg string) error {
+		return fmt.Errorf("failed to read domain SID and name: %s", errMsg)
 	}
 
 	if nodeToRead == nil {
-		return returnFunc("given nodeToRead is nil")
+		return "", "", errMessageFunc("given nodeToRead is nil")
 	}
 
 	if domainSID := nodeToRead.Properties.Get(ad.DomainSID.String()); domainSID.IsNil() {
-		return returnFunc("read domain SID property value is nil")
+		return "", "", errMessageFunc("read domain SID property value is nil")
 	} else {
 		if domainSIDStr, err = domainSID.String(); err != nil {
-			return returnFunc(err.Error())
+			return "", "", errMessageFunc(err.Error())
 		} else {
 			if len(strings.TrimSpace(domainSIDStr)) == 0 {
-				return returnFunc("read domain SID is empty or blank")
+				return "", "", errMessageFunc("read domain SID is empty or blank")
 			}
 		}
 	}
 
 	if domainName := nodeToRead.Properties.Get(common.Name.String()); domainName.IsNil() {
-		return returnFunc("read domain name property value is nil")
+		return "", "", errMessageFunc("read domain name property value is nil")
 	} else {
 		if domainNameStr, err = domainName.String(); err != nil {
-			return returnFunc(err.Error())
+			return "", "", errMessageFunc(err.Error())
 		} else {
 			if len(strings.TrimSpace(domainNameStr)) == 0 {
-				return returnFunc("read domain name is empty or blank")
+				return "", "", errMessageFunc("read domain name is empty or blank")
 			}
 		}
 	}
