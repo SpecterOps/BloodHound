37c8cbdc024f8b11d121e29a9ec75d918d7f05c2
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2025-04-03 11:44:28 -0700

    BED-5499: Move pathfinding search component to `bh-shared-ui` (#1306)
    * moved over pathfinding components to shared-ui
    
    * fixed tests and added handling for empty edge filter list
    
    * added comment regarding filter state refactor
    
    * removed unnecessary test changes
    
    * tailwind styles

--- packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/EdgeFilteringDialog.test.tsx
+++ packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/EdgeFilteringDialog.test.tsx
@@ -15,10 +15,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import userEvent from '@testing-library/user-event';
-import { AllEdgeTypes, EdgeCheckboxType, getInitialPathFilters } from 'bh-shared-ui';
 import { useState } from 'react';
-import { act } from 'react-dom/test-utils';
-import { render, screen } from 'src/test-utils';
+import { AllEdgeTypes, EdgeCheckboxType } from '../../../edgeTypes';
+import { getInitialPathFilters } from '../../../hooks';
+import { act, render, screen } from '../../../test-utils';
 import EdgeFilteringDialog from './EdgeFilteringDialog';
 
 const INITIAL_FILTERS = getInitialPathFilters();
