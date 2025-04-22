e6a224e5a001775ac1e6898cbaaa55ea2aaba019
Stephanie Lamb <slamb@specterops.io>
2025-04-22 14:55:59 -0500

    reformatting sql query

--- cmd/api/src/database/assetgrouptags.go
+++ cmd/api/src/database/assetgrouptags.go
@@ -363,10 +363,11 @@ func (s *BloodhoundDB) GetSelectorsByMemberId(ctx context.Context, memberId int,
 	var selectors model.AssetGroupTagSelectors
 
 	return selectors, CheckError(s.db.WithContext(ctx).Raw(`
-		SELECT s.* from asset_group_tag_selectors s JOIN 
-		asset_group_tag_selector_nodes n ON s.id = n.selector_id JOIN 
-		asset_group_tags t ON s.asset_group_tag_id = t.id WHERE 
-		t.id = ? AND n.node_id = ?`, assetGroupTagId, memberId).Find(&selectors))
+		SELECT s.* from asset_group_tag_selectors s
+		JOIN asset_group_tag_selector_nodes n ON s.id = n.selector_id
+		JOIN asset_group_tags t ON s.asset_group_tag_id = t.id
+		WHERE t.id = ? AND n.node_id = ?
+		`, assetGroupTagId, memberId).Find(&selectors))
 }
 
 func (s *BloodhoundDB) GetAssetGroupTagForSelection(ctx context.Context) ([]model.AssetGroupTag, error) {
