75fecd67653d31c4170719d6069c089d650fed47
Mistah J <26472282+mistahj67@users.noreply.github.com>
2025-04-25 12:59:05 -0700

    fix: test

--- packages/go/dawgs/drivers/pg/node_test.go
+++ packages/go/dawgs/drivers/pg/node_test.go
@@ -59,7 +59,7 @@ func TestNodeQuery(t *testing.T) {
 		}
 	)
 
-	mockTx.EXPECT().Raw("-- match (n) where n.prop = $ return n limit 1\nwith s0 as (select (n0.id, n0.kind_ids, n0.properties)::nodecomposite as n0 from node n0 where (n0.properties ->> 'prop' = @pi0::text)) select s0.n0 as n from s0 limit 1;", gomock.Any()).Return(mockResult)
+	mockTx.EXPECT().Raw("-- match (n) where n.prop = $ return n limit 1\nwith s0 as (select (n0.id, n0.kind_ids, n0.properties)::nodecomposite as n0 from node n0 where ((n0.properties ->> 'prop') = @pi0::text)) select s0.n0 as n from s0 limit 1;", gomock.Any()).Return(mockResult)
 
 	mockResult.EXPECT().Error().Return(nil)
 	mockResult.EXPECT().Next().Return(true)
@@ -85,7 +85,7 @@ func TestNodeQueryOrderByNodeIdWithLimit(t *testing.T) {
 		}
 	)
 
-	mockTx.EXPECT().Raw("-- match (n) where n.prop = $ return n order by id(n) desc limit 2\nwith s0 as (select (n0.id, n0.kind_ids, n0.properties)::nodecomposite as n0 from node n0 where (n0.properties ->> 'prop' = @pi0::text)) select s0.n0 as n from s0 order by (s0.n0).id desc limit 2;", gomock.Any()).Return(mockResult)
+	mockTx.EXPECT().Raw("-- match (n) where n.prop = $ return n order by id(n) desc limit 2\nwith s0 as (select (n0.id, n0.kind_ids, n0.properties)::nodecomposite as n0 from node n0 where ((n0.properties ->> 'prop') = @pi0::text)) select s0.n0 as n from s0 order by (s0.n0).id desc limit 2;", gomock.Any()).Return(mockResult)
 
 	mockResult.EXPECT().Error().Return(nil)
 	mockResult.EXPECT().Next().AnyTimes()
