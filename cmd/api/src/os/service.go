33885d75420e5e48ab85751129d2389ac5478cd4
Katherine Powderly <kpowderly@specterops.io>
2025-04-30 10:48:41 -0500

    comment

--- cmd/api/src/os/service.go
+++ cmd/api/src/os/service.go
@@ -3,6 +3,9 @@ package os
 import "os"
 
 //go:generate go run go.uber.org/mock/mockgen -destination=./mocks/mock.go -package=mocks . Service
+
+// Serves as a lightweight wrapper around the os package which allows for
+// path management to be abstracted.
 type Service interface {
 	CreateTemporaryDirectory(dir, pattern string) (*os.File, error)
 }
