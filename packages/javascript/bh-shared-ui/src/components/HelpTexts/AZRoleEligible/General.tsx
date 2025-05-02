e8d1b8bbbfc346549975b632cae0a52fa218fbef
Rohan Vazarkar <rvazarkar@specterops.io>
2025-05-02 16:54:23 -0400

    feat: add edge panels for AZRoleEligible for https://specterops.atlassian.net/browse/BED-5608

---
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/AZRoleEligible/General.tsx
@@ -0,0 +1,30 @@
+// Copyright 2023 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Typography } from '@mui/material';
+import { FC } from 'react';
+
+const General: FC = () => {
+    return (
+        <Typography variant='body2'>
+            The Entra user or group is eligible for a role assignment.
+
+            If the user is compromised, an attacker could activate the role, or use a current activation to escalate privileges in the tenant.
+        </Typography>
+    );
+};
+
+export default General;
---
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/AZRoleEligible/References.tsx
@@ -0,0 +1,63 @@
+// Copyright 2023 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import { Box, Link } from '@mui/material';
+import { FC } from 'react';
+
+const References: FC = () => {
+    return (
+        <Box sx={{ overflowX: 'auto' }}>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/rest/api/authorization/role-eligibility-schedule-instances/get?view=rest-authorization-2020-10-01&tabs=HTTP'>
+                Role Eligibility Schedule Instances - Get - REST API (Azure Authorization)
+            </Link>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/graph/api/rbacapplication-list-roleeligibilityscheduleinstances?view=graph-rest-1.0&tabs=http'>
+                List roleEligibilityScheduleInstances - Microsoft Graph v1.0
+            </Link>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/graph/api/policyroot-list-rolemanagementpolicies?view=graph-rest-1.0&tabs=http'>
+                List roleManagementPolicies - Microsoft Graph v1.0
+            </Link>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/graph/api/policyroot-list-rolemanagementpolicyassignments?view=graph-rest-1.0&tabs=http'>
+                List roleManagementPolicyAssignments - Microsoft Graph v1.0
+            </Link>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/graph/api/unifiedrolemanagementpolicyassignment-get?view=graph-rest-1.0&tabs=http'>
+                Get unifiedRoleManagementPolicyAssignment - Microsoft Graph v1.0
+            </Link>
+            <Link
+                target='_blank'
+                rel='noopener'
+                href='https://learn.microsoft.com/en-us/entra/id-governance/privileged-identity-management/pim-apis'>
+                API concepts in Privileged Identity management - Microsoft Entra ID Governance
+            </Link>
+        </Box>
+    );
+};
+
+export default References;
