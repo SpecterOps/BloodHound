a59f75436190a8ae1f54b708ed9609d28d920e77
anemeth <anemeth@specterops.io>
2025-05-20 15:04:33 -0700

    Add new config to migrations

--- cmd/api/src/database/migration/migrations/v7.5.0.sql
+++ cmd/api/src/database/migration/migrations/v7.5.0.sql
@@ -1,3 +1,10 @@
 -- is_generic column not actually needed.
 ALTER TABLE ingest_tasks
 DROP COLUMN IF EXISTS is_generic;
+
+INSERT INTO parameters (key, name, description, value, created_at, updated_at) VALUES (
+    'analysis.restrict_outbound_ntlm_default_value', 
+    'Restrict Outbound NTLM Default Value', 
+    'When enabled, any computer''s Restrict Outbound NTLM registry value is treated as Restricting if the registry doesn''t exist on that computer for NTLM edge processing. When disabled, treat the missing registry as Not Restricting.', '{ "enabled": false }',
+    current_timestamp, current_timestamp
+);
