869afaefb03553e7548af861f0c742fcec0fada8
Wesley Robert Maffly-Kipp <wmaffly-kipp@specterops.io>
2025-05-20 10:44:58 -0700

    removed entityinfo redux logic as well

--- cmd/ui/src/ducks/index.ts
+++ cmd/ui/src/ducks/index.ts
@@ -16,6 +16,5 @@
 
 export { default as assetgroups } from './assetgroups';
 export { default as auth } from './auth/authSlice';
-export { default as entityinfo } from './entityinfo';
 export { default as global } from './global';
 export { default as tierzero } from './tierzero';
