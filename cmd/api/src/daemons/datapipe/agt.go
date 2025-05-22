6688adfc632ddcac456c7c227294ec1d761764ac
Mistah J <26472282+mistahj67@users.noreply.github.com>
2025-05-21 17:55:24 -1000

    refactor: cleanup selector engine recursion

--- cmd/api/src/daemons/datapipe/agt.go
+++ cmd/api/src/daemons/datapipe/agt.go
@@ -22,7 +22,6 @@ import (
 	"log/slog"
 	"slices"
 	"sync"
-	"sync/atomic"
 
 	"github.com/specterops/bloodhound/analysis"
 	"github.com/specterops/bloodhound/bhlog/measure"
@@ -115,7 +114,7 @@ func FetchNodesFromSeeds(ctx context.Context, graphDb graph.Database, seeds []mo
 	}
 
 	if expansionMethod == model.AssetGroupExpansionMethodAll || expansionMethod == model.AssetGroupExpansionMethodChildren {
-		collected := fetchAllChildNodes(ctx, graphDb, seedNodes, result, limit)
+		collected := fetchExpandedNodes(ctx, graphDb, fetchChildNodes, seedNodes, result, limit)
 		if result.LimitReached(limit) {
 			return result
 		}
@@ -127,14 +126,14 @@ func FetchNodesFromSeeds(ctx context.Context, graphDb graph.Database, seeds []mo
 	}
 
 	if expansionMethod == model.AssetGroupExpansionMethodAll || expansionMethod == model.AssetGroupExpansionMethodParents {
-		fetchParentNodes(ctx, graphDb, seedNodes, result, limit)
+		_ = fetchExpandedNodes(ctx, graphDb, fetchParentNodes, seedNodes, result, limit)
 	}
 
 	return result
 }
 
 // fetchChildNodes - fetches all children for a single node and submits any found to supplied collector ch
-func fetchChildNodes(ctx context.Context, tx traversal.Traversal, node *graph.Node, ch chan<- *nodeWithSource) error {
+func fetchChildNodes(ctx context.Context, tx traversal.Traversal, node *graph.Node, ch chan<- *nodeWithSource) {
 	var pattern traversal.PatternContinuation
 
 	switch {
@@ -149,6 +148,9 @@ func fetchChildNodes(ctx context.Context, tx traversal.Traversal, node *graph.No
 		// MATCH (n:OU)-[:Contains*..]->(m:Base) RETURN m
 		pattern = traversal.NewPattern().OutboundWithDepth(0, 0, query.And(
 			query.KindIn(query.Relationship(), ad.Contains),
+			query.KindIn(query.End(), ad.Entity),
+		)).InboundWithDepth(0, 0, query.And(
+			query.KindIn(query.Relationship(), ad.MemberOf),
 			query.KindIn(query.End(), ad.Entity),
 		))
 	case node.Kinds.ContainsOneOf(azure.Group):
@@ -170,111 +172,38 @@ func fetchChildNodes(ctx context.Context, tx traversal.Traversal, node *graph.No
 		pattern = traversal.NewPattern().InboundWithDepth(0, 1, query.And(
 			query.KindIn(query.Relationship(), azure.HasRole),
 			query.KindIn(query.End(), azure.Entity),
+		)).InboundWithDepth(0, 0, query.And(
+			query.KindIn(query.Relationship(), azure.MemberOf),
+			query.KindIn(query.End(), azure.Entity),
 		))
 	default:
 		// Skip any that do not need expanding
-		return nil
+		return
 	}
 
 	if err := tx.BreadthFirst(ctx, traversal.Plan{
 		Root: node,
 		Driver: pattern.Do(func(path *graph.PathSegment) error {
-			if path.Trunk != nil {
-				channels.Submit(ctx, ch, &nodeWithSource{Source: model.AssetGroupSelectorNodeSourceChild, Node: path.Trunk.Node})
-			}
-
-			channels.Submit(ctx, ch, &nodeWithSource{Source: model.AssetGroupSelectorNodeSourceChild, Node: path.Node})
-
+			path.WalkReverse(func(nextSegment *graph.PathSegment) bool {
+				return channels.Submit(ctx, ch, &nodeWithSource{Source: model.AssetGroupSelectorNodeSourceChild, Node: nextSegment.Node})
+			})
 			return nil
 		})}); err != nil {
+		slog.ErrorContext(ctx, "AGT: error fetching child nodes", "node", node.ID, "err", err)
 
-		return err
 	}
-
-	return nil
 }
 
-// fetchAllChildNodes - concurrently fetches all seeds + their children until no additional children are found
-func fetchAllChildNodes(ctx context.Context, db graph.Database, seedNodes nodeWithSourceSet, result nodeWithSourceSet, limit int) []*nodeWithSource {
-	var (
-		wg              = sync.WaitGroup{}
-		queueLen        = &atomic.Int64{}
-		chCtx, doneFunc = context.WithCancel(ctx)
-
-		sendCh, getCh = channels.BufferedPipe[*nodeWithSource](chCtx)
-		collectorCh   = make(chan *nodeWithSource)
-
-		traversalInst = traversal.New(db, analysis.MaximumDatabaseParallelWorkers)
-		collected     []*nodeWithSource
-	)
-	defer doneFunc()
-	// Close the send channel to the buffered pipe
-	defer close(sendCh)
-
-	// Spin out some workers, at least 1 per seed node
-	for range len(seedNodes) {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				// Block here until we receive a node to fetch child nodes
-				if node, ok := channels.Receive(chCtx, getCh); !ok {
-					return
-				} else {
-					// Fetch child nodes for this node and send any collected to the collector
-					if err := fetchChildNodes(chCtx, traversalInst, node.Node, collectorCh); err != nil {
-						slog.ErrorContext(ctx, "AGT: error fetching child nodes", "node", node.ID, "err", err)
-					}
-
-					// Fire to collector to signal job done to synchronously decr queue
-					if !channels.Submit(chCtx, collectorCh, nil) {
-						break
-					}
-				}
-			}
-		}()
-	}
-
-	// Spin out a collector
-	go func() {
-		for {
-			// Synchronously fill the collected child nodes and queue up more child checks
-			// Keep track of queue length and trigger finish when queue is empty
-			if nodeWithSrc, ok := channels.Receive(chCtx, collectorCh); !ok {
-				break
-			} else {
-				if nodeWithSrc != nil && result.AddIfNotExists(nodeWithSrc) {
-					// As new child nodes are found, fill collection and queue up a child check for that child as well
-					collected = append(collected, nodeWithSrc)
-					if result.LimitReached(limit) {
-						doneFunc()
-						break
-					}
-					queueLen.Add(1)
-					if !channels.Submit(chCtx, sendCh, nodeWithSrc) {
-						break
-					}
-				} else if nodeWithSrc == nil {
-					// Decr and check if we need to stop
-					queueLen.Add(-1)
-					if queueLen.Load() == 0 {
-						// Once queue hits 0, we fire ctx cancel to exit all workers
-						doneFunc()
-						break
-					}
-				}
-			}
+func fetchParentNodes(ctx context.Context, tx traversal.Traversal, node *graph.Node, ch chan<- *nodeWithSource) {
+	if node.Kinds.ContainsOneOf(ad.Entity) {
+		if err := fetchADParentNodes(ctx, tx, node, ch); err != nil {
+			slog.ErrorContext(ctx, "AGT: error fetching active directory parent nodes", "node", node.ID, "err", err)
 		}
-	}()
-
-	// Start off with seed nodes
-	for _, node := range seedNodes {
-		queueLen.Add(1)
-		channels.Submit(chCtx, sendCh, node)
+	} else if node.Kinds.ContainsOneOf(azure.Entity) {
+		if err := fetchAzureParentNodes(ctx, tx, node, ch); err != nil {
+			slog.ErrorContext(ctx, "AGT: error fetching azure parent nodes", "node", node.ID, "err", err)
+		}
 	}
-
-	wg.Wait() // Wait for workers to process all nodes
-	return collected
 }
 
 // fetchADParentNodes -  fetches all parents for a single active directory node and submits any found to supplied collector ch
@@ -362,30 +291,22 @@ func fetchAzureParentNodes(ctx context.Context, tx traversal.Traversal, node *gr
 	return nil
 }
 
-// fetchParentNodes - concurrently fetches all parents for seed nodes (which may also contain their children)
-func fetchParentNodes(ctx context.Context, db graph.Database, seedNodes nodeWithSourceSet, result nodeWithSourceSet, limit int) {
-	// Expand to parent nodes as needed
+// fetchExpandedNodes - concurrently fetches expanded nodes based off seed nodes
+func fetchExpandedNodes(ctx context.Context, db graph.Database, fetchNodes func(ctx context.Context, tx traversal.Traversal, node *graph.Node, ch chan<- *nodeWithSource), seedNodes nodeWithSourceSet, result nodeWithSourceSet, limit int) []*nodeWithSource {
 	var (
 		wg                      = sync.WaitGroup{}
 		ch                      = make(chan *nodeWithSource)
 		ctxWithCancel, doneFunc = context.WithCancel(ctx)
 		traversalInst           = traversal.New(db, analysis.MaximumDatabaseParallelWorkers)
+		collected               []*nodeWithSource
 	)
 	defer doneFunc()
-	// Spin out a job per node -> may be just seeds or seeds + children here
+	// Spin out a job per seed node
 	for _, node := range seedNodes {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			if node.Kinds.ContainsOneOf(ad.Entity) {
-				if err := fetchADParentNodes(ctxWithCancel, traversalInst, node.Node, ch); err != nil {
-					slog.ErrorContext(ctx, "AGT: error fetching active directory parent nodes", "node", node.ID, "err", err)
-				}
-			} else if node.Kinds.ContainsOneOf(azure.Entity) {
-				if err := fetchAzureParentNodes(ctxWithCancel, traversalInst, node.Node, ch); err != nil {
-					slog.ErrorContext(ctx, "AGT: error fetching azure parent nodes", "node", node.ID, "err", err)
-				}
-			}
+			fetchNodes(ctxWithCancel, traversalInst, node.Node, ch)
 		}()
 	}
 
@@ -395,13 +316,16 @@ func fetchParentNodes(ctx context.Context, db graph.Database, seedNodes nodeWith
 		close(ch)
 	}()
 
-	// This will block and collect all parent nodes until channel is closed
+	// This will block and collect all expanded nodes until channel is closed
 	for nodeWithSrc := range ch {
-		if result.AddIfNotExists(nodeWithSrc) && result.LimitReached(limit) {
-			doneFunc()
+		if result.AddIfNotExists(nodeWithSrc) {
+			collected = append(collected, nodeWithSrc)
+			if result.LimitReached(limit) {
+				doneFunc()
+			}
 		}
 	}
-
+	return collected
 }
 
 // fetchOldSelectedNodes - fetches the currently selected nodes and assembles a map lookup for minimal memory footprint
