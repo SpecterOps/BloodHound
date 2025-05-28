482597b8e2c0174bafbfe6f6eda29e1ece1795bb
Alex Nemeth <80649445+definitelynotagoblin@users.noreply.github.com>
2025-05-28 14:06:18 -0700

    Update cmd/api/src/database/migration/migrations/v7.5.0.sql
    Co-authored-by: mistahj67 <26472282+mistahj67@users.noreply.github.com>

--- cmd/api/src/database/migration/migrations/v7.5.0.sql
+++ cmd/api/src/database/migration/migrations/v7.5.0.sql
@@ -18,10 +18,4 @@
 ALTER TABLE ingest_tasks
 DROP COLUMN IF EXISTS is_generic;
 
-INSERT INTO parameters (key, name, description, value, created_at, updated_at)
-SELECT
-    'analysis.restrict_outbound_ntlm_default_value', 
-    'Restrict Outbound NTLM Default Value', 
-    'When enabled, any computer''s Restrict Outbound NTLM registry value is treated as Restricting if the registry doesn''t exist on that computer for NTLM edge processing. When disabled, treat the missing registry as Not Restricting.', '{ "enabled": false }',
-    current_timestamp, current_timestamp
-WHERE NOT EXISTS (SELECT 1 FROM parameters WHERE key = 'analysis.restrict_outbound_ntlm_default_value');
+INSERT INTO parameters (key, name, description, value, created_at, updated_at) VALUES ( 'analysis.restrict_outbound_ntlm_default_value','Restrict Outbound NTLM Default Value','When enabled, any computer''s Restrict Outbound NTLM registry value is treated as Restricting if the registry doesn''t exist on that computer for NTLM edge processing. When disabled, treat the missing registry as Not Restricting.', '{ "enabled": false }',current_timestamp, current_timestamp) ON CONFLICT DO NOTHING;
