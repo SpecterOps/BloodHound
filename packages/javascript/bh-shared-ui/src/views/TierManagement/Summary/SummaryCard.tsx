ecdbfb72501219a5455f78942ed6025862b113ca
Michael Smith <msmith@specterops.io>
2025-05-29 15:57:41 -0700

    PR feedback

--- packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryCard.tsx
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryCard.tsx
@@ -1,54 +1,53 @@
-import { Card } from '@bloodhoundenterprise/doodleui';
+import { Button, Card } from '@bloodhoundenterprise/doodleui';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { faSquarePlus } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FC } from 'react';
 import LargeRightArrow from '../../../components/AppIcon/Icons/LargeRightArrow';
-import Plus from '../../../components/AppIcon/Icons/Plus';
 import { ROUTE_TIER_MANAGEMENT_DETAILS } from '../../../routes';
 import { useAppNavigate } from '../../../utils';
+import { abbreviatedNumber } from '../../../utils/abbreviatedNumber';
 
 type SummaryCardProps = {
     title: string;
+    type: number;
     selectorCount: number | undefined;
     memberCount: number | undefined;
     id: number;
 };
 
-const SummaryCard: FC<SummaryCardProps> = ({ title, selectorCount, memberCount, id }) => {
+const SummaryCard: FC<SummaryCardProps> = ({ title, type, selectorCount, memberCount, id }) => {
     const navigate = useAppNavigate();
     return (
         <Card className='w-full flex px-6 py-4 rounded-xl'>
-            <div className=' text-xl flex-1 flex items-center justify-center font-bold'>{title}</div>
+            <div className='text-xl flex-1 flex items-center justify-center font-bold truncate min-w-0'>
+                <div className='text-xl font-bold truncate min-w-0'>{title}</div>
+            </div>
             <LargeRightArrow className='w-8 h-16' />
             <div className='flex-1 flex flex-col items-center justify-center'>
                 <p className='text-l font-semibold'>Selectors</p>
-                <p className='text-2xl font-thin'>{selectorCount}</p>
+                <p className='text-2xl font-thin'>{abbreviatedNumber(selectorCount ?? 0)}</p>
             </div>
             <LargeRightArrow className='w-8 h-16' />
             <div className='flex-1 flex flex-col items-center justify-center'>
                 <p className='text-l font-semibold'>Members</p>
-                <p className='text-2xl font-thin'>{memberCount}</p>
+                <p className='text-2xl font-thin'>{abbreviatedNumber(memberCount ?? 0)}</p>
             </div>
 
             <div className='flex-1 flex flex-col items-center justify-center border-l border-black dark:border-white text-sm'>
-                <div
+                <Button
+                    variant={'text'}
                     onClick={(e) => {
                         // Prevent event bubbling for the view details action
                         e.stopPropagation();
-                        navigate(`/tier-management/${ROUTE_TIER_MANAGEMENT_DETAILS}/tier/${id}`);
+                        navigate(
+                            `/tier-management/${ROUTE_TIER_MANAGEMENT_DETAILS}/${type === 1 ? 'tier' : 'label'}/${id}`
+                        );
                     }}
-                    className=' flex items-center space-x-2 hover:underline cursor-pointer'
-                    role='button'
-                    tabIndex={0}
-                    aria-label={`View details for ${title}`}
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter' || e.key === ' ') {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            navigate(`/tier-management/${ROUTE_TIER_MANAGEMENT_DETAILS}/tier/${id}`);
-                        }
-                    }}>
-                    <Plus />
+                    className=' flex items-center space-x-2 hover:underline'>
+                    <FontAwesomeIcon icon={faSquarePlus as IconProp} />
                     <p>View Details</p>
-                </div>
+                </Button>
             </div>
         </Card>
     );
