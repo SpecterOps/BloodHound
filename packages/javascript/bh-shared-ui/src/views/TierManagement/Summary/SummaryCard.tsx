03e214bc24889f9c3520422582f614f433a0623f
Michael Smith <msmith@specterops.io>
2025-05-29 10:40:26 -0700

    BED-5230 added summary card

---
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryCard.tsx
@@ -0,0 +1,47 @@
+import { Card } from '@bloodhoundenterprise/doodleui';
+import { FC } from 'react';
+import LargeRightArrow from '../../../components/AppIcon/Icons/LargeRightArrow';
+import Plus from '../../../components/AppIcon/Icons/Plus';
+import { ROUTE_TIER_MANAGEMENT_DETAILS } from '../../../routes';
+import { useAppNavigate } from '../../../utils';
+
+type SummaryCardProps = {
+    title: string;
+    selectorCount: number | undefined;
+    memberCount: number | undefined;
+    id: number;
+};
+
+const SummaryCard: FC<SummaryCardProps> = ({ title, selectorCount, memberCount, id }) => {
+    const navigate = useAppNavigate();
+    return (
+        <Card className='w-full flex px-6 py-4 rounded-xl'>
+            <div className=' text-xl flex-1 flex items-center justify-center font-bold'>{title}</div>
+            <LargeRightArrow className='w-8 h-16' />
+            <div className='flex-1 flex flex-col items-center justify-center'>
+                <p className='text-l font-semibold'>Selectors</p>
+                <p className='text-2xl font-thin'>{selectorCount}</p>
+            </div>
+            <LargeRightArrow className='w-8 h-16' />
+            <div className='flex-1 flex flex-col items-center justify-center'>
+                <p className='text-l font-semibold'>Members</p>
+                <p className='text-2xl font-thin'>{memberCount}</p>
+            </div>
+
+            <div className='flex-1 flex flex-col items-center justify-center border-l border-black dark:border-white text-sm'>
+                <div
+                    onClick={(e) => {
+                        //prevent event bubbling since card is clickable as well as this link
+                        e.stopPropagation();
+                        navigate(`/tier-management/${ROUTE_TIER_MANAGEMENT_DETAILS}/tier/${id}`);
+                    }}
+                    className=' flex items-center space-x-2 hover:underline cursor-pointer'>
+                    <Plus />
+                    <p>View Details</p>
+                </div>
+            </div>
+        </Card>
+    );
+};
+
+export default SummaryCard;
