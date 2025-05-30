6e3b026d55b854c41cf3eccd67331e8057cb59d2
Alyx Holms <aholms@specterops.io>
2025-05-30 12:01:21 -0700

    chore: simplify code and optimize concurrency strategy

--- packages/go/stbernard/command/license/internal/cmd.go
+++ packages/go/stbernard/command/license/internal/cmd.go
@@ -1,20 +1,19 @@
-//  Copyright 2025 Specter Ops, Inc.
-//
-//  Licensed under the Apache License, Version 2.0
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-//  SPDX-License-Identifier: Apache-2.0
-//
-
+// Copyright Specter Ops, Inc.
+// 
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+//     
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-License-Identifier: Apache-2.0
+// 
 package license
 
 import (
@@ -22,7 +21,9 @@ import (
 	"fmt"
 	"io/fs"
 	"log/slog"
+	"os"
 	"path/filepath"
+	"runtime"
 	"slices"
 	"sync"
 	"time"
@@ -31,32 +32,65 @@ import (
 	"github.com/specterops/bloodhound/packages/go/stbernard/workspace"
 )
 
-// capacity of paths slice
-// is set to 512 as a reasonable default for most projects, but can be adjusted
-// based on the expected number of files in the workspace.
-// This helps to avoid frequent reallocations and improves performance.
-// If the number of files exceeds this capacity, the slice will grow dynamically.
-var paths = make([]string, 0, 512)
-
 func Run(env environment.Environment) error {
+	var (
+		ignoreDir            = []string{".git", ".vscode", ".devcontainer", "node_modules", "dist", ".beagle", ".yarn", "sha256"}
+		ignorePaths          = []string{"tools/docker-compose/configs/pgadmin/pgpass", "justfile", "cmd/api/src/api/static/assets", "packages/python/beagle/beagle/semver", "cmd/api/src/cmd/testidp/samlidp"}
+		disallowedExtensions = []string{".zip", ".example", ".git", ".gitignore", ".png", ".mdx", ".iml", ".g4", ".sum", ".bazel", ".bzl", ".typed", ".md", ".json", ".template", "sha256", ".pyc", ".gif", ".tiff", ".lock", ".txt", ".png", ".jpg", ".jpeg", ".ico", ".gz", ".tar", ".woff2", ".header", ".pro", ".cert", ".crt", ".key", ".example", ".svg", ".sha256"}
+		now                  = time.Now()
+
+		// Concurrency primitives
+		errs       []error
+		wg         = &sync.WaitGroup{}
+		errsMu     = &sync.Mutex{}
+		numWorkers = runtime.NumCPU()
+		pathChan   = make(chan string, numWorkers)
+	)
 
 	wrkPaths, err := workspace.FindPaths(env)
 	if err != nil {
 		return fmt.Errorf("failed to find workspace paths: %w", err)
 	}
 
-	if err := checkRootFiles(wrkPaths.Root); err != nil {
-		return fmt.Errorf("failed to check root files: %w", err)
+	licensePath := filepath.Join(wrkPaths.Root, "LICENSE")
+
+	// Make sure root LICENSE FILE exists
+	if _, err := os.Stat(licensePath); errors.Is(err, os.ErrNotExist) {
+		if err := os.WriteFile(licensePath, []byte(licenseContent), 0644); err != nil {
+			return fmt.Errorf("failed to write root license file: %w", err)
+		}
+	} else if err != nil {
+		return fmt.Errorf("failed to check for root license file: %w", err)
 	}
 
-	ignoreDir := []string{".git", ".vscode", ".devcontainer", "node_modules", "dist", ".beagle", ".yarn", "sha256"}
-	ignorePaths := []string{"tools/docker-compose/configs/pgadmin/pgpass", "justfile", "cmd/api/src/api/static/assets", "packages/python/beagle/beagle/semver", "cmd/api/src/cmd/testidp/samlidp"}
-	disallowedExtensions := []string{".zip", ".example", ".git", ".gitignore", ".png", ".mdx", ".iml", ".g4", ".sum", ".bazel", ".bzl", ".typed", ".md", ".json", ".template", "sha256", ".pyc", ".gif", ".tiff", ".lock", ".txt", ".png", ".jpg", ".jpeg", ".ico", ".gz", ".tar", ".woff2", ".header", ".pro", ".cert", ".crt", ".key", ".example", ".svg", ".sha256"}
+	for range numWorkers {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 
-	now := time.Now()
+			for path := range pathChan {
+				result, err := isHeaderPresent(path)
+				if err != nil {
+					errsMu.Lock()
+					errs = append(errs, err)
+					errsMu.Unlock()
+					return
+				}
+
+				if !result {
+					if err := processFile(path); err != nil {
+						errsMu.Lock()
+						errs = append(errs, err)
+						errsMu.Unlock()
+					}
+				}
+			}
+		}()
+	}
+
 	err = filepath.Walk(wrkPaths.Root, func(path string, info fs.FileInfo, err error) error {
 		// ignore directories and paths that are in the ignore list
-		scanPath := shouldProcessPath(ignorePaths, wrkPaths.Root, path)
+		scanPath := shouldProcessPath(ignorePaths, path)
 		if info.IsDir() && (slices.Contains(ignoreDir, info.Name()) || !scanPath) {
 			return filepath.SkipDir
 		}
@@ -64,7 +98,7 @@ func Run(env environment.Environment) error {
 		// ignore files that are in the ignore list
 		ext := filepath.Ext(path)
 		if !info.IsDir() && !slices.Contains(disallowedExtensions, ext) {
-			paths = append(paths, path)
+			pathChan <- path
 		}
 		return nil
 	})
@@ -72,54 +106,18 @@ func Run(env environment.Environment) error {
 		return fmt.Errorf("error walking the path: %w", err)
 	}
 
-	// append all the errors from the goroutines
-	var (
-		errs   []error
-		errsMu sync.Mutex
-	)
-	var wg sync.WaitGroup
-
-	// Use a worker pool pattern with limited concurrency
-	const maxWorkers = 10
-	sem := make(chan struct{}, maxWorkers)
-
-	for _, path := range paths {
-		wg.Add(1)
-
-		ext := filepath.Ext(path)
-		go func(filePath, fileExt string) {
-			defer wg.Done()
-			sem <- struct{}{}        // acquire semaphore
-			defer func() { <-sem }() // release semaphore
-
-			result, err := isHeaderPresent(filePath)
-			if err != nil {
-				errsMu.Lock()
-				errs = append(errs, err)
-				errsMu.Unlock()
-				return
-			}
-
-			if !result {
-				if err := processFile(filePath, fileExt); err != nil {
-					errsMu.Lock()
-					errs = append(errs, err)
-					errsMu.Unlock()
-				}
-			}
-		}(path, ext)
-	}
-
-	// block main untill all the goroutines in done state
+	// close path channel to signal we're done sending values
+	close(pathChan)
+	// block main until all the goroutines in done state
 	wg.Wait()
 	diff := time.Since(now)
 
-	slog.Info(fmt.Sprintf("running scans on bhce took %v", diff))
+	slog.Info("running scans on bhce", slog.Duration("execution_time", diff))
 	return errors.Join(errs...)
 }
 
-func processFile(path, ext string) error {
-	switch ext {
+func processFile(path string) error {
+	switch filepath.Ext(path) {
 	case ".go", ".work", ".mod", ".ts", ".tsx", ".js", ".cjs", ".jsx", ".cue", ".scss":
 		h := generateLicenseHeader("//")
 		return writeFile(path, h)
--- packages/go/stbernard/command/license/internal/validations.go
+++ packages/go/stbernard/command/license/internal/validations.go
@@ -1,20 +1,19 @@
-//  Copyright 2025 Specter Ops, Inc.
-//
-//  Licensed under the Apache License, Version 2.0
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-//
-//  SPDX-License-Identifier: Apache-2.0
-//
-
+// Copyright Specter Ops, Inc.
+// 
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+//     
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// 
+// SPDX-License-Identifier: Apache-2.0
+// 
 package license
 
 import (
@@ -23,16 +22,7 @@ import (
 	"strings"
 )
 
-func dirCheck(path string) bool {
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-
-	return fileInfo.IsDir()
-}
-
-func shouldProcessPath(ignorPaths []string, wd, path string) bool {
+func shouldProcessPath(ignorPaths []string, path string) bool {
 	for _, ig := range ignorPaths {
 		if strings.Contains(path, ig) {
 			return false // Don't process ignored paths
