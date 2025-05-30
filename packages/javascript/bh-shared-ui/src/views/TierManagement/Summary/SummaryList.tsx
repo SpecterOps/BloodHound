d78f0fbe71953a0fcebd2b3a4ee6f320b2cf7bf7
Michael Smith <msmith@specterops.io>
2025-05-30 13:26:35 -0700

    BED-5229 added summary tier list

---
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryList.tsx
@@ -0,0 +1,69 @@
+import { AssetGroupTagsListItem } from 'js-client-library';
+import { FC } from 'react';
+import { UseQueryResult } from 'react-query';
+import DownArrow from '../../../components/AppIcon/Icons/DownArrow';
+import { cn } from '../../../utils';
+import { itemSkeletons } from '../Details/utils';
+import SummaryCard from './SummaryCard';
+
+type SummaryListProps = {
+    onSelect: (id: number) => void;
+    listQuery: UseQueryResult<AssetGroupTagsListItem[]>;
+    selected: string;
+    title: 'Tiers' | 'Labels';
+};
+
+const SummaryList: FC<SummaryListProps> = ({ onSelect, listQuery, selected, title }) => {
+    return (
+        <div className='flex flex-col w-full h-full space-y-4'>
+            <div className='flex flex-col flex-1 space-y-4'>
+                <ul>
+                    {listQuery.isLoading ? (
+                        itemSkeletons.map((skeleton, index) => {
+                            return skeleton(title, index);
+                        })
+                    ) : listQuery.isError ? (
+                        <li className='border-y border-neutral-light-3 dark:border-neutral-dark-3 relative h-10 pl-2'>
+                            <span className='text-base'>There was an error fetching this data</span>
+                        </li>
+                    ) : listQuery.isSuccess ? (
+                        listQuery.data
+                            ?.sort((a, b) => {
+                                return b.name.localeCompare(a.name);
+                            })
+                            .map((listItem) => {
+                                return (
+                                    <>
+                                        <li
+                                            key={listItem.id}
+                                            onClick={() => {
+                                                onSelect(listItem.id);
+                                            }}
+                                            className={cn('relative mb-4 cursor-pointer', {
+                                                'border rounded-xl border-[#33318F] dark:border-[#ffffff]':
+                                                    selected === listItem.id.toString(),
+                                            })}>
+                                            <SummaryCard
+                                                title={listItem.name}
+                                                type={listItem.type}
+                                                id={listItem.id}
+                                                selectorCount={listItem.counts?.selectors}
+                                                memberCount={listItem.counts?.members}
+                                            />
+                                        </li>
+                                        {listItem.type === 1 ? (
+                                            <div className='flex justify-center mt-2 last:hidden'>
+                                                <DownArrow className='w-8 h-6' />
+                                            </div>
+                                        ) : null}
+                                    </>
+                                );
+                            })
+                    ) : null}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default SummaryList;
