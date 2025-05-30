90775859e8179ed720774fa25cfcd98e45b50a52
Michael Smith <msmith@specterops.io>
2025-05-30 09:38:15 -0700

    typo

--- packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryCard.tsx
+++ packages/javascript/bh-shared-ui/src/views/TierManagement/Summary/SummaryCard.tsx
@@ -21,7 +21,7 @@ const SummaryCard: FC<SummaryCardProps> = ({ title, type, selectorCount, memberC
     return (
         <Card className='w-full flex px-6 py-4 rounded-xl'>
             <div className='flex-1 flex items-center justify-center truncate min-w-0'>
-                <div className='text-2xl font-bold truncate min-w-0'>JHFHGFGHFHGFJGFJHFGHJJHFGJGF</div>
+                <div className='text-2xl font-bold truncate min-w-0'>{title}</div>
             </div>
             <LargeRightArrow className='w-8 h-16' />
             <div className='flex-1 flex flex-col items-center justify-center'>
