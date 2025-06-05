6f27646f1a7188a0b202ecf02bdc794465a008c5
Katherine Powderly <kpowderly@specterops.io>
2025-06-05 11:01:04 -0500

    Test: BED-5641 Auth Contract Tests (#1526)
    * added contract tests for auth, login, and oidc handlers
    
    * finish saml tests
    
    * added contract tests for all bhce v2 auth resources
    
    * just prepare-for-codereview
    
    * remove unused field
    
    * restructured tests, increased coverage
    
    * test: GetPermission
    
    * test: GetRole & GetUser
    
    * test: GetSelf
    
    * working on CreateAuthToken
    
    * test: CreateAuthToken
    
    * test: DeleteAuthToken IP
    
    * test: DeleteAuthToken
    
    * test: EnrollMFA
    
    * test: logout
    
    * test: partial testing for OIDCLoginHandler & OIDCCallbackHandler
    
    * test: partial testing for SAMLLoginRedirect, SAMLCallbackRedirect, ListSAMLSignOnEndpoints & ListSAMLProviders
    
    * test: GetSAMLProvider
    
    * test: CreateSAMLProviderRequest & UpdateSAMLProviderRequest
    
    * added some todo's to start tackling
    
    * test: finished EnrollMFA and cleaned up test structure in auth_test.go
    
    * testing cleanup
    
    * more test cleanup
    
    * added a tls service to mock the certification parsing but it didn't solve all my issues with the SAML tests. Still looking to see if there's any clean way to add some coverage but not likely from what I can tell, end of the day commit
    
    * some cleanup
    
    * missed a fundamental piece of testing the http header so now having to go back in and update test cases
    
    * missed a fundamental piece of testing the http header so now having to go back in and update test cases
    
    * test: GetSAMLProvider
    
    * cleaned up a couple more tests
    
    * resolved failing test OIDC
    
    * just prepare for code review
    
    * cleaning up old tests
    
    * cleaning up old tests
    
    * cleaned up old tests
    
    * clean up
    
    * modified tests to properly use mux router so we can test the responses http header
    
    * wrapped up mux router work, need to finish final abstraction
    
    * added one more test
    
    * just prepare
    
    * consistency across tests
    
    * addressing code rabbit comments
    
    * addressing code rabbit nits
    
    * final couple endpoints
    
    * pushing up changes for sso success
    
    * updated to account for modified service
    
    * added some more SAMLLoginHandler tests
    
    * refactor and test fix for GetCollectorManifest
    
    * refactor and test fix for DownloadCollectorChecksumByVersion
    
    * updated SSOLoginHandler test & OIDCCallbackHandler
    
    * just prepare
    
    * removed random rebased files from cache
    
    * removed random rebased files from cache
    
    * added more tests for SAMLCallbackHandler
    
    * addressed nit
    
    ---------
    
    Co-authored-by: Justin Wu <jwu@specterops.io>

--- cmd/api/src/api/v2/auth/oidc.go
+++ cmd/api/src/api/v2/auth/oidc.go
@@ -165,7 +165,7 @@ func (s ManagementResource) OIDCLoginHandler(response http.ResponseWriter, reque
 		slog.WarnContext(request.Context(), fmt.Sprintf("[OIDC] Failed to generate state: %v", err))
 		// Technical issues scenario
 		api.RedirectToLoginURL(response, request, "We're having trouble connecting. Please check your internet and try again.")
-	} else if provider, err := oidc.NewProvider(request.Context(), ssoProvider.OIDCProvider.Issuer); err != nil {
+	} else if provider, err := s.OIDC.NewProvider(request.Context(), ssoProvider.OIDCProvider.Issuer); err != nil {
 		slog.WarnContext(request.Context(), fmt.Sprintf("[OIDC] Failed to create OIDC provider: %v", err))
 		// SSO misconfiguration or technical issue
 		// Treat this as a misconfiguration scenario
@@ -228,7 +228,7 @@ func (s ManagementResource) OIDCCallbackHandler(response http.ResponseWriter, re
 		// State mismatch
 		slog.WarnContext(request.Context(), "[OIDC] state does not match")
 		api.RedirectToLoginURL(response, request, "Invalid: `state` do not match")
-	} else if provider, err := oidc.NewProvider(request.Context(), ssoProvider.OIDCProvider.Issuer); err != nil {
+	} else if provider, err := s.OIDC.NewProvider(request.Context(), ssoProvider.OIDCProvider.Issuer); err != nil {
 		// SSO misconfiguration scenario
 		slog.WarnContext(request.Context(), fmt.Sprintf("[OIDC] Failed to create OIDC provider: %v", err))
 		api.RedirectToLoginURL(response, request, "Your SSO connection failed due to misconfiguration, please contact your Administrator")
