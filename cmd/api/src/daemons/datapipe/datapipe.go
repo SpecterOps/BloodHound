d1d414f8bfa5a1df582c647ce5b98823057b628d
kpom <kpomeroy@specterops.io>
2025-06-09 16:03:39 -0500

    refactor (datapipe): Make an extendable interface for the pipeline, and allow context to be passed intead of static

--- cmd/api/src/daemons/datapipe/datapipe.go
+++ cmd/api/src/daemons/datapipe/datapipe.go
@@ -22,12 +22,8 @@ import (
 	"log/slog"
 	"time"
 
-	"github.com/specterops/bloodhound/cache"
-	"github.com/specterops/bloodhound/dawgs/graph"
-	"github.com/specterops/bloodhound/src/config"
-	"github.com/specterops/bloodhound/src/database"
 	"github.com/specterops/bloodhound/src/model"
-	"github.com/specterops/bloodhound/src/services/upload"
+	"pkg.specterops.io/bloodhoundad/bhe/database"
 )
 
 const (
@@ -35,21 +31,30 @@ const (
 )
 
 type Daemon struct {
-	db           database.Database
 	tickInterval time.Duration
 	pipeline     Pipeline
-	ctx          context.Context
+	db           database.Database
 }
 
 func (s *Daemon) Name() string {
 	return "Data Pipe Daemon"
 }
 
-func NewDaemon(ctx context.Context, cfg config.Configuration, db database.Database, graphdb graph.Database, cache cache.Cache, tickInterval time.Duration, ingestSchema upload.IngestSchema) *Daemon {
+func NewDaemon(pipeline Pipeline, tickInterval time.Duration, db database.Database) *Daemon {
 	return &Daemon{
+		db:           db,
 		tickInterval: tickInterval,
-		pipeline:     *NewPipeline(ctx, cfg, db, graphdb, cache, ingestSchema),
+		pipeline:     pipeline,
 	}
+}
+
+// PipelineInterface defines methods that operate on instance state.
+// These methods are not static and require a fully initialized pipeline.
+type Pipeline interface {
+	PruneData(context.Context)
+	DeleteData(context.Context)
+	IngestTasks(context.Context)
+	Analyze(context.Context)
 }
 
 func (s *Daemon) Start(ctx context.Context) {
@@ -61,26 +66,26 @@ func (s *Daemon) Start(ctx context.Context) {
 	defer datapipeLoopTimer.Stop()
 	defer pruningTicker.Stop()
 
-	s.WithDatapipeStatus(model.DatapipeStatusPurging, s.pipeline.clearOrphanedData)
+	s.WithDatapipeStatus(ctx, model.DatapipeStatusPurging, s.pipeline.PruneData)
 
 	for {
 		select {
 		case <-pruningTicker.C:
 
-			s.WithDatapipeStatus(model.DatapipeStatusPurging, s.pipeline.clearOrphanedData)
+			s.WithDatapipeStatus(ctx, model.DatapipeStatusPurging, s.pipeline.PruneData)
 
 		case <-datapipeLoopTimer.C:
-			if s.db.HasCollectedGraphDataDeletionRequest(s.ctx) {
-				s.WithDatapipeStatus(model.DatapipeStatusPurging, s.pipeline.deleteData)
+			if s.db.HasCollectedGraphDataDeletionRequest(ctx) {
+				s.WithDatapipeStatus(ctx, model.DatapipeStatusPurging, s.pipeline.DeleteData)
 			}
 
-			s.WithDatapipeStatus(model.DatapipeStatusIngesting, s.pipeline.IngestTasks)
+			s.WithDatapipeStatus(ctx, model.DatapipeStatusIngesting, s.pipeline.IngestTasks)
 
-			s.WithDatapipeStatus(model.DatapipeStatusAnalyzing, s.pipeline.analyze)
+			s.WithDatapipeStatus(ctx, model.DatapipeStatusAnalyzing, s.pipeline.Analyze)
 
 			datapipeLoopTimer.Reset(s.tickInterval)
 
-		case <-s.ctx.Done():
+		case <-ctx.Done():
 			return
 		}
 	}
@@ -92,17 +97,17 @@ func (s *Daemon) Stop(ctx context.Context) error {
 
 // Any function can be wrapped with a datapipe lock, giving it the status. If everything locks
 // the datapipe through this same wrapper, it should always defer the idle status after.
-func (s *Daemon) WithDatapipeStatus(status model.DatapipeStatus, action func()) {
+func (s *Daemon) WithDatapipeStatus(ctx context.Context, status model.DatapipeStatus, action func(context.Context)) {
 
-	if err := s.db.SetDatapipeStatus(s.ctx, status, false); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Error setting datapipe status: %v", err))
+	if err := s.db.SetDatapipeStatus(ctx, status, false); err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("Error setting datapipe status: %v", err))
 		return
 	}
 	defer func() {
-		if err := s.db.SetDatapipeStatus(s.ctx, model.DatapipeStatusIdle, false); err != nil {
-			slog.ErrorContext(s.ctx, "failed to reset datapipe status", "error", err)
+		if err := s.db.SetDatapipeStatus(ctx, model.DatapipeStatusIdle, false); err != nil {
+			slog.ErrorContext(ctx, "failed to reset datapipe status", "error", err)
 		}
 	}()
 
-	action()
+	action(ctx)
 }
