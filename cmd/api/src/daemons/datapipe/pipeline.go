86063d9115d95bea8c1c21d3d7248fd321f9e0d4
kpom <kpomeroy@specterops.io>
2025-06-09 16:03:39 -0500

    refactor (datapipe): Make an extendable interface for the pipeline, and allow context to be passed intead of static

--- cmd/api/src/daemons/datapipe/pipeline.go
+++ cmd/api/src/daemons/datapipe/pipeline.go
@@ -33,25 +33,23 @@ import (
 	"github.com/specterops/bloodhound/src/services/upload"
 )
 
-type Pipeline struct {
+type BHCEPipeline struct {
 	db                  database.Database
 	graphdb             graph.Database
 	cache               cache.Cache
 	cfg                 config.Configuration
-	ctx                 context.Context
 	orphanedFileSweeper *OrphanFileSweeper
 	ingestSchema        upload.IngestSchema
 	jobService          job.JobService
 	graphifyService     graphify.GraphifyService
 }
 
-func NewPipeline(ctx context.Context, cfg config.Configuration, db database.Database, graphDB graph.Database, cache cache.Cache, ingestSchema upload.IngestSchema) *Pipeline {
-	return &Pipeline{
+func NewPipeline(ctx context.Context, cfg config.Configuration, db database.Database, graphDB graph.Database, cache cache.Cache, ingestSchema upload.IngestSchema) *BHCEPipeline {
+	return &BHCEPipeline{
 		db:                  db,
 		graphdb:             graphDB,
 		cache:               cache,
 		cfg:                 cfg,
-		ctx:                 ctx,
 		orphanedFileSweeper: NewOrphanFileSweeper(NewOSFileOperations(), cfg.TempDirectory()),
 		ingestSchema:        ingestSchema,
 		jobService:          job.NewJobService(ctx, db),
@@ -61,30 +59,30 @@ func NewPipeline(ctx context.Context, cfg config.Configuration, db database.Data
 
 // This handles the deletion of data if the customer requests it. I'm not sure why the defers
 // are stacked the way the are, but this should keep all current functionality.
-func (s *Pipeline) deleteData() {
+func (s *BHCEPipeline) DeleteData(ctx context.Context) {
 
 	defer func() {
-		_ = s.db.DeleteAnalysisRequest(s.ctx)
-		_ = s.db.RequestAnalysis(s.ctx, "datapie")
+		_ = s.db.DeleteAnalysisRequest(ctx)
+		_ = s.db.RequestAnalysis(ctx, "datapie")
 		measure.Measure(slog.LevelInfo, "Purge Graph Data Completed")()
 	}()
 
 	slog.Info("Begin Purge Graph Data")
 
-	if err := s.db.CancelAllIngestJobs(s.ctx); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Error cancelling jobs during data deletion: %v", err))
-	} else if err := s.db.DeleteAllIngestTasks(s.ctx); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Error deleting ingest tasks during data deletion: %v", err))
-	} else if err := DeleteCollectedGraphData(s.ctx, s.graphdb); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Error deleting graph data: %v", err))
+	if err := s.db.CancelAllIngestJobs(ctx); err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("Error cancelling jobs during data deletion: %v", err))
+	} else if err := s.db.DeleteAllIngestTasks(ctx); err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("Error deleting ingest tasks during data deletion: %v", err))
+	} else if err := DeleteCollectedGraphData(ctx, s.graphdb); err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("Error deleting graph data: %v", err))
 	}
 }
 
 // This is called on Daemon start. We get a list of all filenames we know/expect and delete any
 // other files. Would love to move this out of datapipe entirely eventually and into... somewhere
-func (s *Pipeline) clearOrphanedData() {
-	if ingestTasks, err := s.db.GetAllIngestTasks(s.ctx); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Failed fetching available ingest tasks: %v", err))
+func (s *BHCEPipeline) PruneData(ctx context.Context) {
+	if ingestTasks, err := s.db.GetAllIngestTasks(ctx); err != nil {
+		slog.ErrorContext(ctx, fmt.Sprintf("Failed fetching available ingest tasks: %v", err))
 	} else {
 		expectedFiles := make([]string, len(ingestTasks))
 
@@ -92,15 +90,15 @@ func (s *Pipeline) clearOrphanedData() {
 			expectedFiles[idx] = ingestTask.FileName
 		}
 
-		go s.orphanedFileSweeper.Clear(s.ctx, expectedFiles)
+		go s.orphanedFileSweeper.Clear(ctx, expectedFiles)
 	}
 }
 
 // This is currently public to support as a first class testing seam, but with some refactoring may be split away from the
 // Daemon object enough to be self standing and pulled to an internal package namespace
-func (s *Pipeline) IngestTasks() {
+func (s *BHCEPipeline) IngestTasks(ctx context.Context) {
 	// Ingest all available ingest tasks
-	s.graphifyService.ProcessTasks(updateJobFunc(s.ctx, s.db))
+	s.graphifyService.ProcessTasks(updateJobFunc(ctx, s.db))
 
 	// Manage time-out state progression for ingest jobs
 	s.jobService.ProcessStaleIngestJobs()
@@ -126,17 +124,17 @@ func updateJobFunc(ctx context.Context, db database.Database) graphify.UpdateJob
 	}
 }
 
-func (s *Pipeline) analyze() {
+func (s *BHCEPipeline) Analyze(ctx context.Context) {
 
 	// If there are completed ingest jobs or if analysis was user-requested, perform analysis.
 	if hasJobsWaitingForAnalysis, err := s.jobService.HasIngestJobsWaitingForAnalysis(); err != nil {
-		slog.ErrorContext(s.ctx, fmt.Sprintf("Failed looking up jobs waiting for analysis: %v", err))
-	} else if hasJobsWaitingForAnalysis || s.db.HasAnalysisRequest(s.ctx) {
+		slog.ErrorContext(ctx, fmt.Sprintf("Failed looking up jobs waiting for analysis: %v", err))
+	} else if hasJobsWaitingForAnalysis || s.db.HasAnalysisRequest(ctx) {
 
 		// Ensure that the user-requested analysis switch is deleted. This is done at the beginning of the
 		// function so that any re-analysis requests are caught while analysis is in-progress.
-		if err := s.db.DeleteAnalysisRequest(s.ctx); err != nil {
-			slog.ErrorContext(s.ctx, fmt.Sprintf("Error deleting analysis request: %v", err))
+		if err := s.db.DeleteAnalysisRequest(ctx); err != nil {
+			slog.ErrorContext(ctx, fmt.Sprintf("Error deleting analysis request: %v", err))
 			return
 		}
 
@@ -146,7 +144,7 @@ func (s *Pipeline) analyze() {
 
 		defer measure.LogAndMeasure(slog.LevelInfo, "Graph Analysis")()
 
-		if err := RunAnalysisOperations(s.ctx, s.db, s.graphdb, s.cfg); err != nil {
+		if err := RunAnalysisOperations(ctx, s.db, s.graphdb, s.cfg); err != nil {
 			if errors.Is(err, ErrAnalysisFailed) {
 				s.jobService.FailAnalyzedIngestJobs()
 
@@ -156,8 +154,8 @@ func (s *Pipeline) analyze() {
 		} else {
 			s.jobService.CompleteAnalyzedIngestJobs()
 
-			if _, err := s.db.GetFlagByKey(s.ctx, appcfg.FeatureEntityPanelCaching); err != nil {
-				slog.ErrorContext(s.ctx, fmt.Sprintf("Error retrieving entity panel caching flag: %v", err))
+			if _, err := s.db.GetFlagByKey(ctx, appcfg.FeatureEntityPanelCaching); err != nil {
+				slog.ErrorContext(ctx, fmt.Sprintf("Error retrieving entity panel caching flag: %v", err))
 			} else {
 				if err := s.cache.Reset(); err != nil {
 					slog.Error(fmt.Sprintf("Error while resetting the cache: %v", err))
