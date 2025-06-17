ca4ee8bfe19aafbabdd776626ee579fccff113c9
jknudsen <jknudsen@specterops.io>
2025-06-17 15:24:32 +0200

    make ManageCA and ManageCertificate traversable

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/ManageCA/LinuxAbuse.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/ManageCA/LinuxAbuse.tsx
@@ -19,11 +19,54 @@ import { FC } from 'react';
 
 const Abuse: FC = () => {
     return (
-        <Typography variant='body2'>
-            This relationship alone is not enough to perform a privilege escalation or impersonation primitive. This
-            relationship may contribute to other relationships and attributes, from which an escalation opportunity may
-            emerge.
-        </Typography>
+        <>
+            <Typography variant='body2'>
+                CA administrators can perform the following actions that may enable and ADCS escalation:
+                <ol style={{ listStyleType: 'decimal', paddingLeft: '1.5em' }}>
+                    <li>Grant CA officer (ManageCertificates) on enterprise CA</li>
+                    <li>Publish certificate templates</li>
+                    <li>Grant enroll on enterprise CA</li>
+                    <li>Enable configurations like the ESC6 flag (EDITF_ATTRIBUTESUBJECTALTNAME2)</li>
+                </ol>
+            </Typography>
+            <Typography variant='body2'>
+                An attacker can identify an ADCS escalation where one or more requirements are not met and enable the
+                abuse by performing one or more of the actions above.
+            </Typography>
+            <Typography variant='body2'>
+                The combination of the ManageCA and ManageCertificates permissions allow the attacker to approve
+                certificate requests that were denied becuase lack of enrollment rights on the certificate template or
+                on the enterprise CA. A common action is therefore to grant this permission, enroll in an ESC1 template
+                that the attacker does not have enrollment rights on, issue the certificate of the denied request. This
+                attack fail if role separation is enabled on the CA, as it enforces that users cannot have both ManageCA
+                and ManageCertificates. However, this setting is very rare.
+            </Typography>
+            <Typography variant='body1'>Grant CA officer (ManageCertificates)</Typography>
+            <Typography variant='body2'>Certipy allow you to grant the CA officer role:</Typography>
+            <Typography component={'pre'}>
+                {"certipy ca -ca 'corp-DC-CA' -add-officer john -username john@corp.local -password Passw0rd"}
+            </Typography>
+            <Typography variant='body1'>Publish certificate template</Typography>
+            <Typography variant='body2'>Certificate templates can be published to the CA using Certipy:</Typography>
+            <Typography component={'pre'}>
+                {"certipy ca -ca 'corp-DC-CA' -enable-template TemplateCN -username john@corp.local -password Passw0rd"}
+            </Typography>
+            <Typography variant='body1'>Approve certificate request (pending or denied)</Typography>
+            <Typography variant='body2'>
+                Certificate requsts can be approved using Certipy (requires ManageCertificates):
+            </Typography>
+            <Typography component={'pre'}>
+                {"certipy ca -ca 'corp-DC-CA' -issue-request 785 -username john@corp.local -password Passw0rd"}
+            </Typography>
+            <Typography variant='body2'>Download the certificate with this command:</Typography>
+            <Typography component={'pre'}>
+                {
+                    'certipy req -username john@corp.local -password Passw0rd -ca corp-DC-CA -target ca.corp.local -retrieve 785'
+                }
+            </Typography>
+            <Typography variant='body1'>Enable ESC6 flag (requires restart of CA service)</Typography>
+            <Typography variant='body2'>See Windows abuse.</Typography>
+        </>
     );
 };
 
