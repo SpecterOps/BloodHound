1bc488ed1f59382ef21810cc1515568f92ef3320
Alyx Holms <aholms@specterops.io>
2025-06-17 12:43:41 -0700

    wip: first working ingest test TODO: burn it all with fire

--- cmd/api/src/services/graphify/tasks_integration_test.go
+++ cmd/api/src/services/graphify/tasks_integration_test.go
@@ -1,18 +1,3 @@
-// Copyright 2025 Specter Ops, Inc.
-//
-// Licensed under the Apache License, Version 2.0
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// SPDX-License-Identifier: Apache-2.0
 //go:build integration
 
 package graphify_test
@@ -20,74 +5,101 @@ package graphify_test
 import (
 	"context"
 	"os"
-	"path"
+	"os/exec"
 	"testing"
 	"time"
 
-	"github.com/specterops/bloodhound/cmd/api/src/model"
+	"github.com/peterldowns/pgtestdb"
+	"github.com/specterops/dawgs"
+	"github.com/specterops/dawgs/drivers/pg"
+	"github.com/specterops/bloodhound/packages/go/graphschema"
 	"github.com/specterops/bloodhound/packages/go/lab/generic"
+	"github.com/specterops/bloodhound/cmd/api/src/auth"
+	"github.com/specterops/bloodhound/cmd/api/src/config"
+	"github.com/specterops/bloodhound/cmd/api/src/database"
+	"github.com/specterops/bloodhound/cmd/api/src/model"
+	"github.com/specterops/bloodhound/cmd/api/src/services/graphify"
+	"github.com/specterops/bloodhound/cmd/api/src/services/upload"
 	"github.com/stretchr/testify/require"
 )
 
-func TestVersion6IngestJSON(t *testing.T) {
+func TestVersion6Ingest(t *testing.T) {
+	// TODO: Create setup function (should return a valid DB and GraphDB handle at minimum)
 	t.Parallel()
 	var (
 		ctx = context.Background()
-
-		fixturesPath = path.Join("fixtures", t.Name(), "v6ingest")
-
-		testSuite = setupIntegrationTestSuite(t, fixturesPath)
+		// TODO: Read in configuration from integration testing file, no hard coded DB connections
+		conf = pgtestdb.Config{
+			DriverName:                "pgx",
+			User:                      "bloodhound",
+			Password:                  "bloodhoundcommunityedition",
+			Host:                      "localhost",
+			Port:                      "65432",
+			Options:                   "sslmode=disable",
+			ForceTerminateConnections: true,
+		}
+		connConf = pgtestdb.Custom(t, conf, pgtestdb.NoopMigrator{})
 
 		files = []string{
-			path.Join(testSuite.WorkDir, "computers.json"),
-			path.Join(testSuite.WorkDir, "containers.json"),
-			path.Join(testSuite.WorkDir, "domains.json"),
-			path.Join(testSuite.WorkDir, "gpos.json"),
-			path.Join(testSuite.WorkDir, "groups.json"),
-			path.Join(testSuite.WorkDir, "ous.json"),
-			path.Join(testSuite.WorkDir, "sessions.json"),
-			path.Join(testSuite.WorkDir, "users.json"),
+			"fixtures/tmp/computers.json",
+			"fixtures/tmp/containers.json",
+			"fixtures/tmp/domains.json",
+			"fixtures/tmp/gpos.json",
+			"fixtures/tmp/groups.json",
+			"fixtures/tmp/ous.json",
+			"fixtures/tmp/sessions.json",
+			"fixtures/tmp/users.json",
 		}
 	)
 
-	defer teardownIntegrationTestSuite(t, &testSuite)
+	//#region Setup for dbs
+	pool, err := pg.NewPool(connConf.URL())
+	require.NoError(t, err)
+
+	gormDB, err := database.OpenDatabase(connConf.URL())
+	require.NoError(t, err)
+
+	db := database.NewBloodhoundDB(gormDB, auth.NewIdentityResolver())
+
+	// TODO: make sure to migrate the DB so we have a good blank slate
+
+	graphDB, err := dawgs.Open(ctx, pg.DriverName, dawgs.Config{
+		GraphQueryMemoryLimit: 1024 * 1024 * 1024 * 2,
+		ConnectionString:      connConf.URL(),
+		Pool:                  pool,
+	})
+	require.NoError(t, err)
+
+	err = graphDB.AssertSchema(ctx, graphschema.DefaultGraphSchema())
+	require.NoError(t, err)
+
+	ingestSchema, err := upload.LoadIngestSchema()
+	require.NoError(t, err)
+	//#endregion
+
+	service := graphify.NewGraphifyService(ctx, db, graphDB, config.Configuration{}, ingestSchema)
+
+	//#region hacks until refactor for embedfs
+	cmd := exec.Command("rm", "-r", "fixtures/tmp")
+	err = cmd.Run()
+	require.NoError(t, err, "%s", cmd.Err)
+
+	cmd = exec.Command("cp", "-r", "fixtures/v6ingest/", "fixtures/tmp/")
+	err = cmd.Run()
+	require.NoError(t, err, "%s", cmd.Err)
+	//#endregion
 
 	for _, file := range files {
-		total, failed, err := testSuite.GraphifyService.ProcessIngestFile(ctx, model.IngestTask{FileName: file, FileType: model.FileTypeJson}, time.Now())
+		total, failed, err := service.ProcessIngestFile(ctx, model.IngestTask{FileName: file, FileType: model.FileTypeJson}, time.Now())
 		require.NoError(t, err)
 		require.Zero(t, failed)
 		require.Equal(t, 1, total)
 	}
 
-	expected, err := generic.LoadGraphFromFile(os.DirFS(path.Join("fixtures", t.Name())), "v6expected.json")
+	// TODO: use an embedded filesystem instead of bodging dirfs here
+	expected, err := generic.LoadGraphFromFile(os.DirFS("fixtures"), "v6expected.json")
 	require.NoError(t, err)
-	generic.AssertDatabaseGraph(t, ctx, testSuite.GraphDB, &expected)
-}
-
-func TestVersion6IngestZIP(t *testing.T) {
-	t.Parallel()
-	var (
-		ctx = context.Background()
-
-		fixturesPath = path.Join("fixtures", t.Name(), "v6ingest")
-
-		testSuite = setupIntegrationTestSuite(t, fixturesPath)
-
-		files = []string{
-			path.Join(testSuite.WorkDir, "archive.zip"),
-		}
-	)
-
-	defer teardownIntegrationTestSuite(t, &testSuite)
-
-	for _, file := range files {
-		total, failed, err := testSuite.GraphifyService.ProcessIngestFile(ctx, model.IngestTask{FileName: file, FileType: model.FileTypeZip}, time.Now())
-		require.NoError(t, err)
-		require.Zero(t, failed)
-		require.Equal(t, 8, total)
-	}
-
-	expected, err := generic.LoadGraphFromFile(os.DirFS(path.Join("fixtures", t.Name())), "v6expected.json")
-	require.NoError(t, err)
-	generic.AssertDatabaseGraph(t, ctx, testSuite.GraphDB, &expected)
+	// TODO: decide if AssertDatabaseGraph should get a DB handle or transaction (should we worry about wrapping transactions
+	// at this level or let the function do it)
+	generic.AssertDatabaseGraph(t, ctx, graphDB, &expected)
 }
