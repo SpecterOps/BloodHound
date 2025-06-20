b9b46afb467f590571b4693b6e182e007c3797f1
jknudsen <jknudsen@specterops.io>
2025-06-20 10:16:37 +0200

    Add WriteSPN info under WritePublicInformation

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/WritePublicInformation/LinuxAbuse.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/WritePublicInformation/LinuxAbuse.tsx
@@ -26,6 +26,14 @@ const LinuxAbuse: FC<EdgeInfoProps> = () => {
                 The write access to the AltSecurityIdentities may enable an ADCS ESC14 Scenario A attack.
             </Typography>
             <Typography variant='body2'>
+                Alternatively, the write access to the SPN enable a targeted Kerberoasting attack against user accounts
+                with a weak password. See the{' '}
+                <Link target='_blank' rel='noopener' href='https://bloodhound.specterops.io/resources/edges/write-spn'>
+                    WriteSPN
+                </Link>{' '}
+                edge for more details.
+            </Typography>
+            <Typography variant='body2'>
                 An attacker can add an explicit certificate mapping in the AltSecurityIdentities of the target referring
                 to a certificate in the attacker's possession, and then use this certificate to authenticate as the
                 target.
