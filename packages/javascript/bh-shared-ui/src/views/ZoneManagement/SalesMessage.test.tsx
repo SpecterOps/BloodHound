096614860abdedee4a95119bf33872e6f6a775a5
Leah Miller <lmiller@specterops.io>
2025-06-26 13:49:05 -0700

    failing tests

--- packages/javascript/bh-shared-ui/src/views/ZoneManagement/SalesMessage.test.tsx
+++ packages/javascript/bh-shared-ui/src/views/ZoneManagement/SalesMessage.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import SalesMessage from './SalesMessage';
 
-// const mockUseGetConfiguration = vi.fn();
-// vi.mock('../../hooks', async () => {
-//     const actual = await vi.importActual('../../hooks');
-//     return {
-//         ...actual,
-//         useGetConfiguration: () => mockUseGetConfiguration,
-//     };
-// });
+const mockUseGetConfiguration = vi.fn();
+vi.mock('../../hooks', async () => {
+    const actual = await vi.importActual('../../hooks');
+    return {
+        ...actual,
+        useGetConfiguration: () => mockUseGetConfiguration,
+    };
+});
 
 const mockParseTieringConfiguration = vi.fn();
 vi.mock('js-client-library', async () => {
@@ -20,12 +20,18 @@ vi.mock('js-client-library', async () => {
 });
 
 describe('SalesMessage', async () => {
-
-
     it('renders sales message when multi_tier_analysis_enabled is false', () => {
-        // (mockUseGetConfiguration).mockReturnValue({
-        //     data: {},
-        // });
+        (mockUseGetConfiguration).mockReturnValue({
+            data: [{
+                key: "analysis.tiering",
+                name: "Multi-Tier Analysis Configuration",
+                value: {
+                    label_limit: 10,
+                    multi_tier_analysis_enabled: false,
+                    tier_limit: 3
+                }
+            }],
+        });
 
         (mockParseTieringConfiguration).mockReturnValue({
             value: { multi_tier_analysis_enabled: false },
@@ -39,9 +45,17 @@ describe('SalesMessage', async () => {
     });
 
     it('does not render sales message when multi_tier_analysis_enabled is true', () => {
-        // (mockUseGetConfiguration).mockReturnValue({
-        //     data: {},
-        // });
+        (mockUseGetConfiguration).mockReturnValue({
+            data: [{
+                key: "analysis.tiering",
+                name: "Multi-Tier Analysis Configuration",
+                value: {
+                    label_limit: 10,
+                    multi_tier_analysis_enabled: true,
+                    tier_limit: 3
+                }
+            }],
+        });
 
         (mockParseTieringConfiguration).mockReturnValue({
             value: { multi_tier_analysis_enabled: true },
