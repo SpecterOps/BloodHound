77e6c38d5e1cec6ef05ef3f15c8e4d2f5fe42e19
Leah Miller <lmiller@specterops.io>
2025-06-26 13:31:32 -0700

    non-passing tests

---
+++ packages/javascript/bh-shared-ui/src/views/ZoneManagement/SalesMessage.test.tsx
@@ -0,0 +1,56 @@
+import { render, screen } from '@testing-library/react';
+import SalesMessage from './SalesMessage';
+
+// const mockUseGetConfiguration = vi.fn();
+// vi.mock('../../hooks', async () => {
+//     const actual = await vi.importActual('../../hooks');
+//     return {
+//         ...actual,
+//         useGetConfiguration: () => mockUseGetConfiguration,
+//     };
+// });
+
+const mockParseTieringConfiguration = vi.fn();
+vi.mock('js-client-library', async () => {
+    const actual = await vi.importActual('js-client-library');
+    return {
+        ...actual,
+        parseTieringConfiguration: () => mockParseTieringConfiguration,
+    };
+});
+
+describe('SalesMessage', async () => {
+
+
+    it('renders sales message when multi_tier_analysis_enabled is false', () => {
+        // (mockUseGetConfiguration).mockReturnValue({
+        //     data: {},
+        // });
+
+        (mockParseTieringConfiguration).mockReturnValue({
+            value: { multi_tier_analysis_enabled: false },
+        });
+
+        render(<SalesMessage />);
+
+        expect(
+            screen.getByText(/Upgrade Privilege Zones/i)
+        ).toBeInTheDocument();
+    });
+
+    it('does not render sales message when multi_tier_analysis_enabled is true', () => {
+        // (mockUseGetConfiguration).mockReturnValue({
+        //     data: {},
+        // });
+
+        (mockParseTieringConfiguration).mockReturnValue({
+            value: { multi_tier_analysis_enabled: true },
+        });
+
+        render(<SalesMessage />);
+
+        expect(
+            screen.queryByText(/Upgrade Privilege Zones/i)
+        ).not.toBeInTheDocument();
+    });
+});
