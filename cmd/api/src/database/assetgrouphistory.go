6c57b65ed3f0b36fd941fcc33340718ea6caa848
Elijah Brown <ebrown@specterops.io>
2025-06-26 00:01:42 -0700

    Fix error return

--- cmd/api/src/database/assetgrouphistory.go
+++ cmd/api/src/database/assetgrouphistory.go
@@ -78,12 +78,12 @@ func (s *BloodhoundDB) GetAssetGroupHistoryRecords(ctx context.Context, sqlFilte
 		sqlFilter.SQLString)
 
 	if result := s.db.WithContext(ctx).Raw(sqlStr, sqlFilter.Params...).Find(&historyRecs); result.Error != nil {
-		return []model.AssetGroupHistory{{}}, 0, CheckError(result)
+		return []model.AssetGroupHistory{}, 0, CheckError(result)
 	}
 
 	if limit > 0 || skip > 0 {
 		if result := s.db.WithContext(ctx).Raw(sqlCountStr, sqlFilter.Params...).Scan(&rowCount); result.Error != nil {
-			return []model.AssetGroupHistory{{}}, 0, CheckError(result)
+			return []model.AssetGroupHistory{}, 0, CheckError(result)
 		}
 	} else {
 		rowCount = len(historyRecs)
