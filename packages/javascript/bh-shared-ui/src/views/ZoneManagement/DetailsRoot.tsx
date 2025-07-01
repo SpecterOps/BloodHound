e854f9c5a525dfbfdeb0b375bd9c91466e89d2cd
Jeff Vacca <jvacca@specterops.io>
2025-07-01 08:27:50 -0700

    BED-6102 - wait for tier0 value before redirecting

--- packages/javascript/bh-shared-ui/src/views/ZoneManagement/DetailsRoot.tsx
+++ packages/javascript/bh-shared-ui/src/views/ZoneManagement/DetailsRoot.tsx
@@ -1,11 +1,15 @@
+import { Skeleton } from '@mui/material';
 import { FC } from 'react';
 import { AppNavigate } from '../../components';
 import { useHighestPrivilegeTag } from '../../hooks';
 import { DEFAULT_ZONE_MANAGEMENT_ROUTE } from '../../routes';
-
 const DetailsRoot: FC = () => {
     const topTagId = useHighestPrivilegeTag()?.id;
-    return <AppNavigate to={'/zone-management/' + DEFAULT_ZONE_MANAGEMENT_ROUTE + topTagId} replace />;
+    if (topTagId) {
+        return <AppNavigate to={'/zone-management/' + DEFAULT_ZONE_MANAGEMENT_ROUTE + topTagId} replace />;
+    } else {
+        return <Skeleton className='h-24' />;
+    }
 };
 
 export default DetailsRoot;
