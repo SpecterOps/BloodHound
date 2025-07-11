66c2481ef02a540e9bef3c9bd0979daaf6c6bacf
Katherine Powderly <kpowderly@specterops.io>
2025-07-11 14:57:21 -0500

    wip: code rabbit fix

--- packages/go/graphify/graph/graph.go
+++ packages/go/graphify/graph/graph.go
@@ -154,7 +154,7 @@ func (s *CommunityGraphService) TeardownService(ctx context.Context) {
 	if s.db != nil {
 		err := s.db.Wipe(ctx)
 		if err != nil {
-			slog.Error(fmt.Sprintf("failed to wipe database after command completion: %v", err))
+			slog.Error("failed to wipe database after command completion", slog.String("error", err.Error()))
 		} else {
 			slog.Info("successfully wiped database")
 		}
