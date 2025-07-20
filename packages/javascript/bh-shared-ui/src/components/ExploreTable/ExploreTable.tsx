17f70c4c0ec39e96efa87a830a7a6a70cf65e4c3
Dani Cairns <dcairns@specterops.io>
2025-07-19 20:36:35 -0700

    comment prop belonging to doodleui PR

--- packages/javascript/bh-shared-ui/src/components/ExploreTable/ExploreTable.tsx
+++ packages/javascript/bh-shared-ui/src/components/ExploreTable/ExploreTable.tsx
@@ -28,7 +28,7 @@ type DataTableProps = React.ComponentProps<typeof MemoDataTable>;
 
 const tableProps: DataTableProps['TableProps'] = {
     className: 'w-[default] w-full table-fixed',
-    disableDefaultOverflowAuto: true,
+    // disableDefaultOverflowAuto: true,
 };
 
 const tableHeaderProps: DataTableProps['TableHeaderProps'] = {
