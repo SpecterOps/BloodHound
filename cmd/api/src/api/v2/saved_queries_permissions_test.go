2243dca2c43bfda9ca565b583ef9887dfc448143
Lawson Willard <46655228+LawsonWillard@users.noreply.github.com>
2025-07-22 13:24:31 -0700

    run GetSavedQueryPermissions tests in parallel

--- cmd/api/src/api/v2/saved_queries_permissions_test.go
+++ cmd/api/src/api/v2/saved_queries_permissions_test.go
@@ -1810,7 +1810,7 @@ func TestResources_GetPermissionsForSavedQuery(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// t.Parallel()
+			t.Parallel()
 			mockCtrl := gomock.NewController(t)
 			databaseMock := &mock{
 				mockDatabase: mocks.NewMockDatabase(mockCtrl),
