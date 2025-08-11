2aa260798efac781e535230d75072b1365974e3e
Jeff Vacca <jvacca@specterops.io>
2025-08-11 12:03:57 -0400

    BED-4620 - tests - list item action menu

---
+++ packages/javascript/bh-shared-ui/src/components/PrebuiltSearchList/ListItemActionMenu.test.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ListItemActionMenu from './ListItemActionMenu';
+
+describe('ListItemActionMenu', () => {
+    const testDeleteHandler = vitest.fn();
+    const testEditHandler = vitest.fn();
+    const user = userEvent.setup();
+    it('renders a ListItemActionMenu component', async () => {
+        render(<ListItemActionMenu id={1} deleteQuery={testDeleteHandler} editQuery={testEditHandler} />);
+
+        expect(screen.getByTestId('saved-query-action-menu-trigger')).toBeInTheDocument();
+    });
+
+    it('renders the popup content with run, edit/share, and delete when the menu trigger', async () => {
+        render(<ListItemActionMenu id={1} deleteQuery={testDeleteHandler} editQuery={testEditHandler} />);
+
+        expect(screen.getByTestId('saved-query-action-menu-trigger')).toBeInTheDocument();
+
+        await user.click(screen.getByRole('button'));
+        expect(screen.getByText(/run/i)).toBeInTheDocument();
+        expect(screen.getByText(/edit\/share/i)).toBeInTheDocument();
+        expect(screen.getByText(/delete/i)).toBeInTheDocument();
+    });
+
+    it('fires edit when edit is clicked', async () => {
+        render(<ListItemActionMenu id={1} deleteQuery={testDeleteHandler} editQuery={testEditHandler} />);
+
+        expect(screen.getByTestId('saved-query-action-menu-trigger')).toBeInTheDocument();
+
+        await user.click(screen.getByRole('button'));
+
+        await user.click(screen.getByText(/edit\/share/i));
+        expect(testEditHandler).toBeCalled();
+    });
+
+    it('fires delete when edit is clicked', async () => {
+        render(<ListItemActionMenu id={1} deleteQuery={testDeleteHandler} editQuery={testEditHandler} />);
+
+        expect(screen.getByTestId('saved-query-action-menu-trigger')).toBeInTheDocument();
+
+        await user.click(screen.getByRole('button'));
+
+        await user.click(screen.getByText(/delete/i));
+        expect(testDeleteHandler).toBeCalled();
+    });
+
+    it('closes', async () => {
+        render(<ListItemActionMenu id={1} deleteQuery={testDeleteHandler} editQuery={testEditHandler} />);
+
+        expect(screen.getByTestId('saved-query-action-menu-trigger')).toBeInTheDocument();
+
+        await user.click(screen.getByRole('button'));
+        expect(screen.getByText(/edit\/share/i)).toBeInTheDocument();
+
+        await user.click(screen.getByRole('button'));
+        expect(screen.queryByText(/edit\/share/i)).not.toBeInTheDocument();
+    });
+});
