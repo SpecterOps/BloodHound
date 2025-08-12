81b4409e282fe5d4e8e9074be702867d6d745c94
Jeff Vacca <jvacca@specterops.io>
2025-08-12 12:51:57 -0400

    BED-4620 - tests

--- packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/CypherSearch.test.tsx
+++ packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/CypherSearch.test.tsx
@@ -25,20 +25,27 @@ const CYPHER = 'match (n) return n limit 5';
 
 describe('CypherSearch', () => {
     const setup = async () => {
+        const testPerformSearch = vi.fn();
         const state = {
             cypherQuery: '',
             setCypherQuery: vi.fn(),
-            performSearch: vi.fn(),
+            performSearch: testPerformSearch,
         };
         const autoRun = true;
         const handleAutoRun = () => {};
+        const testOnRunSearchClick = vi.fn();
 
         const screen = await render(
-            <CypherSearch cypherSearchState={state} autoRun={autoRun} setAutoRun={handleAutoRun} />
+            <CypherSearch
+                cypherSearchState={state}
+                autoRun={autoRun}
+                setAutoRun={handleAutoRun}
+                onRunSearchClick={testOnRunSearchClick}
+            />
         );
         const user = await userEvent.setup();
 
-        return { state, screen, user };
+        return { state, screen, user, testOnRunSearchClick };
     };
 
     const server = setupServer(
@@ -53,6 +60,45 @@ describe('CypherSearch', () => {
             return res(
                 ctx.json({
                     data: [{ id: 1, key: 'tier_management_engine', enabled: true }],
+                })
+            );
+        }),
+        rest.get('/api/v2/config', async (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    data: [
+                        {
+                            key: 'analysis.tiering',
+                            name: 'Multi-Tier Analysis Configuration',
+                            value: {
+                                tier_limit: 3,
+                                label_limit: 10,
+                                multi_tier_analysis_enabled: true,
+                            },
+                            id: 8,
+                        },
+                    ],
+                })
+            );
+        }),
+        rest.get('/api/v2/saved-queries', async (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    data: [],
+                })
+            );
+        }),
+        rest.get('/api/v2/self', async (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    data: {},
+                })
+            );
+        }),
+        rest.get('/api/v2/asset-group-tags', async (req, res, ctx) => {
+            return res(
+                ctx.json({
+                    data: [],
                 })
             );
         })
@@ -72,8 +118,8 @@ describe('CypherSearch', () => {
         await waitForElementToBeRemoved(() => screen.queryByText('Loading...'));
         expect(screen.getByText(/cypher query/i)).toBeInTheDocument();
 
-        expect(screen.getByRole('link', { name: /help/i })).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /run/ })).toBeInTheDocument();
+        expect(screen.getByRole('link', { name: /app-icon-info/i })).toBeInTheDocument();
+        // expect(screen.getByRole('button', { name: /run/ })).toBeInTheDocument();
     });
 
     // Disabling this test for now, tailwind does not output any css in shared-ui tests so we can't check for visibility
@@ -91,20 +137,21 @@ describe('CypherSearch', () => {
     it('should call the setCypherQuery handler when the value in the editor changes', async () => {
         const { screen, user, state } = await setup();
         const searchbox = screen.getAllByRole('textbox');
+        // screen.debug(searchbox[0]);
 
         await user.type(searchbox[0], CYPHER);
+        expect(state.setCypherQuery).toBeCalled();
         expect(state.setCypherQuery).toHaveBeenCalledTimes(CYPHER.length);
     });
 
     it('should call performSearch when a value is in the searchbox and the "Run" button is clicked', async () => {
-        const { screen, user, state } = await setup();
+        const { screen, user, testOnRunSearchClick } = await setup();
         await waitForElementToBeRemoved(() => screen.queryByText('Loading...'));
         const searchbox = screen.getAllByRole('textbox');
         const run = screen.getByRole('button', { name: /run/i });
 
         await user.type(searchbox[0], CYPHER);
         await user.click(run);
-
-        expect(state.performSearch).toHaveBeenCalledTimes(1);
+        expect(testOnRunSearchClick).toBeCalled();
     });
 });
