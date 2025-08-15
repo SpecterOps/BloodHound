e7d025f564e56b705ec33d12256f42d22e39171b
ykaiboussiSO <ykaiboussi@specterops.io>
2025-08-15 10:54:26 -0500

    add useListDisplayRoles test

---
+++ packages/javascript/bh-shared-ui/src/hooks/useListDisplayRoles/useListDisplayRoles.test.tsx
@@ -0,0 +1,381 @@
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+import { Role } from 'js-client-library';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { SetUpQueryClient, renderHook, waitFor } from '../../test-utils';
+import { useListDisplayRoles } from './useListDisplayRoles';
+
+const MockRoles: Role[] = [
+    {
+        name: 'Client-Tasking',
+        description: 'Used for data collection clients, can post data but cannot read data',
+        permissions: [
+            {
+                authority: 'graphdb',
+                name: 'Ingest',
+                id: 30,
+            },
+            {
+                authority: 'clients',
+                name: 'Tasking',
+                id: 5,
+            },
+        ],
+        id: 2,
+    },
+    {
+        name: 'Upload-Only',
+        description: 'Used for users to ingest files manually',
+        permissions: [
+            {
+                authority: 'graphdb',
+                name: 'Ingest',
+                id: 30,
+            },
+        ],
+        id: 6,
+    },
+    {
+        name: 'Read-Only',
+        description: 'Used for integrations',
+        permissions: [
+            {
+                authority: 'app',
+                name: 'ReadAppConfig',
+                id: 13,
+            },
+            {
+                authority: 'risks',
+                name: 'GenerateReport',
+                id: 10,
+            },
+            {
+                authority: 'auth',
+                name: 'CreateToken',
+                id: 6,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageSelf',
+                id: 9,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Read',
+                id: 2,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Read',
+                id: 15,
+            },
+        ],
+        id: 1,
+    },
+    {
+        name: 'User',
+        description: 'Can read data, modify asset group memberships',
+        permissions: [
+            {
+                authority: 'app',
+                name: 'ReadAppConfig',
+                id: 13,
+            },
+            {
+                authority: 'risks',
+                name: 'GenerateReport',
+                id: 10,
+            },
+            {
+                authority: 'auth',
+                name: 'CreateToken',
+                id: 6,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageSelf',
+                id: 9,
+            },
+            {
+                authority: 'clients',
+                name: 'Read',
+                id: 17,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Read',
+                id: 2,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Read',
+                id: 15,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Write',
+                id: 16,
+            },
+        ],
+        id: 3,
+    },
+    {
+        name: 'Power User',
+        description: 'Can upload data, manage clients, and perform any action a User can',
+        permissions: [
+            {
+                authority: 'graphdb',
+                name: 'Ingest',
+                id: 30,
+            },
+            {
+                authority: 'app',
+                name: 'ReadAppConfig',
+                id: 13,
+            },
+            {
+                authority: 'risks',
+                name: 'GenerateReport',
+                id: 10,
+            },
+            {
+                authority: 'risks',
+                name: 'ManageRisks',
+                id: 11,
+            },
+            {
+                authority: 'auth',
+                name: 'CreateToken',
+                id: 6,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageSelf',
+                id: 9,
+            },
+            {
+                authority: 'clients',
+                name: 'Manage',
+                id: 4,
+            },
+            {
+                authority: 'clients',
+                name: 'Read',
+                id: 17,
+            },
+            {
+                authority: 'clients',
+                name: 'Tasking',
+                id: 5,
+            },
+            {
+                authority: 'collection',
+                name: 'ManageJobs',
+                id: 3,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Mutate',
+                id: 19,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Read',
+                id: 2,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Write',
+                id: 1,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Read',
+                id: 15,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Write',
+                id: 16,
+            },
+        ],
+        id: 5,
+    },
+    {
+        name: 'Administrator',
+        description: 'Can manage users, clients, and application configuration',
+        permissions: [
+            {
+                authority: 'graphdb',
+                name: 'Ingest',
+                id: 30,
+            },
+            {
+                authority: 'app',
+                name: 'ReadAppConfig',
+                id: 13,
+            },
+            {
+                authority: 'app',
+                name: 'WriteAppConfig',
+                id: 14,
+            },
+            {
+                authority: 'risks',
+                name: 'GenerateReport',
+                id: 10,
+            },
+            {
+                authority: 'risks',
+                name: 'ManageRisks',
+                id: 11,
+            },
+            {
+                authority: 'auth',
+                name: 'CreateToken',
+                id: 6,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageAppConfig',
+                id: 12,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageProviders',
+                id: 8,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageSelf',
+                id: 9,
+            },
+            {
+                authority: 'auth',
+                name: 'ManageUsers',
+                id: 7,
+            },
+            {
+                authority: 'clients',
+                name: 'Manage',
+                id: 4,
+            },
+            {
+                authority: 'clients',
+                name: 'Read',
+                id: 17,
+            },
+            {
+                authority: 'clients',
+                name: 'Tasking',
+                id: 5,
+            },
+            {
+                authority: 'collection',
+                name: 'ManageJobs',
+                id: 3,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Mutate',
+                id: 19,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Read',
+                id: 2,
+            },
+            {
+                authority: 'graphdb',
+                name: 'Write',
+                id: 1,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Read',
+                id: 15,
+            },
+            {
+                authority: 'saved_queries',
+                name: 'Write',
+                id: 16,
+            },
+            {
+                authority: 'db',
+                name: 'Wipe',
+                id: 18,
+            },
+        ],
+        id: 4,
+    },
+];
+
+const fetchRolesRequest = () => {
+    return rest.get('/api/v2/roles', async (_req, rest, ctx) => {
+        return rest(
+            ctx.json({
+                data: {
+                    roles: MockRoles,
+                },
+            })
+        );
+    });
+};
+
+const server = setupServer(fetchRolesRequest());
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe('useListDisplayRoles', () => {
+    it('List Display Roles Hook is Loading', async () => {
+        const mockState = [
+            {
+                key: ['getRoles'],
+                data: MockRoles,
+            },
+            { key: ['listSSOProviders'], data: null },
+        ];
+
+        // set setQueryData
+        const queryClient = SetUpQueryClient(mockState);
+
+        const { result } = renderHook(() => useListDisplayRoles(), {
+            queryClient: queryClient,
+        });
+
+        await waitFor(() => expect(result.current.isLoading).toEqual(true));
+    });
+    it('Validate Client Tasking Roles is not on List Display Roles', async () => {
+        const mockState = [
+            {
+                key: ['getRoles'],
+                data: MockRoles,
+            },
+            { key: ['listSSOProviders'], data: null },
+        ];
+
+        // set setQueryData
+        const queryClient = SetUpQueryClient(mockState);
+
+        const { result } = renderHook(() => useListDisplayRoles(), {
+            queryClient: queryClient,
+        });
+
+        await waitFor(() => expect(result.current.data));
+    });
+});
