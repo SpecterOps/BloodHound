f10a3a13e2ae6fee57c6859f6ee7ff5a808c75cf
Jeff Vacca <jvacca@specterops.io>
2025-08-18 11:23:27 -0700

    BED-4620 - ImportQueryDialog Tests

---
+++ packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/ImportQueryDialog.test.tsx
@@ -0,0 +1,44 @@
+// import { rest } from 'msw';
+// import { setupServer } from 'msw/node';
+// import FileIngest from '.';
+// import { createAuthStateWithPermissions } from '../../mocks';
+// import { fireEvent, render, screen, waitFor } from '../../test-utils';
+
+import { fireEvent, render, screen, waitFor } from '../../../test-utils';
+import ImportQueryDialog from './ImportQueryDialog';
+
+describe('ImportQueryDialog', () => {
+    const testHandleClose = vi.fn();
+
+    const testFile = new File([JSON.stringify({ value: 'test' })], 'test.json', { type: 'application/json' });
+    const errorFile = new File(['test text'], 'test.txt', { type: 'text/plain' });
+
+    it('renders the Import Query Dialog', () => {
+        render(<ImportQueryDialog open={true} onClose={testHandleClose} />);
+        expect(screen.getByRole('dialog')).toBeInTheDocument();
+        expect(screen.getByText('Upload Files')).toBeInTheDocument();
+    });
+
+    it('hanldes close event', () => {
+        render(<ImportQueryDialog open={true} onClose={testHandleClose} />);
+        const cancelButton = screen.getByText('Cancel');
+        expect(cancelButton).toBeInTheDocument();
+        fireEvent.click(cancelButton);
+        expect(testHandleClose).toBeCalledTimes(1);
+    });
+
+    it('prevents a user from proceeding if the file is not valid', async () => {
+        render(<ImportQueryDialog open={true} onClose={testHandleClose} />);
+
+        const testUploadBtn = screen.getByRole('button', { name: 'Upload' });
+        expect(testUploadBtn).toBeDisabled();
+
+        const fileInput = screen.getByTestId('ingest-file-upload');
+
+        await waitFor(() => expect(fileInput).toBeEnabled());
+        expect(fileInput).toBeInTheDocument();
+
+        await waitFor(() => fireEvent.change(fileInput, { target: { files: [errorFile] } }));
+        expect(testUploadBtn).toBeDisabled();
+    });
+});
