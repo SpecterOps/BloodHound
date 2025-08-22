4af736409ef0686ff44865b0e41f03d2adca3c98
Jeff Vacca <jvacca@specterops.io>
2025-08-22 13:28:03 -0700

    BED-4620 - cleanup

--- packages/javascript/bh-shared-ui/src/views/Explore/providers/SavedQueriesProvider/reducers.ts
+++
@@ -1,9 +0,0 @@
-export const messageReducer = (state: any, action: any) => {
-    switch (action.type) {
-        case 'UPDATE':
-            return { ...state, message: state.message + ' and things...' };
-
-        default:
-            return state;
-    }
-};
