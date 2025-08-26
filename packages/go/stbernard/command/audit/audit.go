7178d3d83a18aae5468aa6d9f8d977df2d2561ec
Alyx Holms <aholms@specterops.io>
2025-08-26 10:49:12 -0700

    chore: pr feedback

--- packages/go/stbernard/command/audit/audit.go
+++ packages/go/stbernard/command/audit/audit.go
@@ -1,3 +1,18 @@
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 package audit
 
 import (
@@ -117,7 +132,7 @@ func getTicketAudit(env environment.Environment, cwd, startTimestamp, endTimesta
 
 		tickets    = make([]string, 0, 64)
 		exceptions = make([]string, 0, 64)
-		args       = []string{"pr", "list", "--state", "merged", "--json", "url,body"}
+		args       = []string{"pr", "list", "--state", "merged", "--json", "url,body", "--limit", "1000"}
 	)
 
 	if baseBranch != "" {
