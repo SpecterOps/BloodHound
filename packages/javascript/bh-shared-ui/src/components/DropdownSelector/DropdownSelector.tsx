59a583fbb30837200db6f6eef9dc5e1c28b7b932
sirisjo <ssjoboen@specterops.io>
2025-09-05 09:56:35 -0500

    feat: Dropdown Certification Selector

--- packages/javascript/bh-shared-ui/src/components/DropdownSelector/DropdownSelector.tsx
+++ packages/javascript/bh-shared-ui/src/components/DropdownSelector/DropdownSelector.tsx
@@ -33,98 +33,6 @@ import { AppIcon } from '../AppIcon';
 import { DropdownOption } from './types';
 
 const DropdownSelector: FC<{
-    // options: DropdownOption[];
-    // selectedText: JSX.Element | string;
-    // onChange: (selection: DropdownOption) => void;
-    // buttonClasses?: string;
-    // buttonStyle?: CSSProperties;
-    // menuItemSize?: number;
-    // toUpper?: boolean;
-    // variant: ButtonProps['variant'];
-
-    // }> = ({ options, selectedText, onChange, buttonClasses, buttonStyle, variant = 'primary' }) => {
-    // const [anchorEl, setAnchorEl] = useState(null);
-    // const open = Boolean(anchorEl);
-
-    // const handleClick = (e: any) => {
-    //     setAnchorEl(e.currentTarget);
-    // };
-
-    // const handleClose = () => {
-    //     setAnchorEl(null);
-    // };
-
-    // const defaultClassName = 'w-full truncate uppercase';
-
-    //     return (
-    //         <Box p={1}>
-    //             <Button
-    //                 variant={variant}
-    //                 style={buttonStyle}
-    //                 className={buttonClasses ?? defaultClassName}
-    //                 onClick={handleClick}
-    //                 data-testid='dropdown_context-selector'>
-    //                 <span className='inline-flex justify-between gap-4 items-center w-full'>
-    //                     <span>{selectedText}</span>
-    //                     <span className={cn({ 'rotate-180 transition-transform': open })}>
-    //                         <AppIcon.CaretDown size={12} />
-    //                     </span>
-    //                 </span>
-    //             </Button>
-    //             <Popover
-    //                 open={open}
-    //                 anchorEl={anchorEl}
-    //                 onClose={handleClose}
-    //                 anchorOrigin={{
-    //                     vertical: 'bottom',
-    //                     horizontal: 'left',
-    //                 }}
-    //                 transformOrigin={{
-    //                     vertical: -10,
-    //                     horizontal: 0,
-    //                 }}
-    //                 data-testid='dropdown_context-selector-popover'>
-    //                 {options.map((option) => {
-    //                     return (
-    //                         <MenuItem
-    //                             style={{
-    //                                 display: 'flex',
-    //                                 justifyContent: 'space-between',
-    //                                 width: 200,
-    //                                 maxWidth: 200,
-    //                             }}
-    //                             key={option.key}
-    //                             onClick={() => {
-    //                                 onChange(option);
-    //                                 handleClose();
-    //                             }}>
-    //                             <Tooltip title={option.value}>
-    //                                 <Typography
-    //                                     style={{
-    //                                         overflow: 'hidden',
-    //                                         textTransform: 'uppercase',
-    //                                         display: 'inline-block',
-    //                                         textOverflow: 'ellipsis',
-    //                                         maxWidth: 350,
-    //                                     }}>
-    //                                     {option.value}
-    //                                 </Typography>
-    //                             </Tooltip>
-    //                             {option.icon && (
-    //                                 <FontAwesomeIcon
-    //                                     style={{ width: '10%', alignSelf: 'center' }}
-    //                                     icon={option.icon}
-    //                                     size='sm'
-    //                                 />
-    //                             )}
-    //                         </MenuItem>
-    //                     );
-    //                 })}
-    //             </Popover>
-    //         </Box>
-    //     );
-    // };
-
     variant?: ButtonProps['variant'];
     options: DropdownOption[];
     selectedText: JSX.Element | string;
@@ -145,7 +53,7 @@ const DropdownSelector: FC<{
                 <Button
                     variant={variant}
                     className={cn({
-                        'bg-transparent rounded-md border shadow-outer-0 hover:bg-neutral-3 text-black dark:text-white truncate':
+                        'bg-transparent rounded-md border shadow-outer-0 hover:bg-neutral-3 hover:text-primary text-black dark:text-white truncate':
                             !buttonPrimary,
                         'w-full uppercase': buttonPrimary,
                     })}
@@ -165,9 +73,9 @@ const DropdownSelector: FC<{
             <PopoverContent
                 data-testid='dropdown_context-selector-popover'
                 align={align}
-                className='flex flex-col gap-2 p-4 border border-neutral-light-5 w-80'>
+                className='flex flex-col gap-2 p-1 border border-neutral-light-5 w-80'>
                 <ul>
-                    {options.map((option: DropdownOption, index: number) => {
+                    {options.map((option: DropdownOption) => {
                         return (
                             <li key={option.key}>
                                 <Button
@@ -180,7 +88,9 @@ const DropdownSelector: FC<{
                                     <TooltipProvider>
                                         <TooltipRoot>
                                             <TooltipTrigger>
-                                                <span className='uppercase max-w-96 truncate'>{option.value}</span>
+                                                <span className={cn('max-w-96 truncate', { uppercase: buttonPrimary })}>
+                                                    {option.value}
+                                                </span>
                                             </TooltipTrigger>
                                             <TooltipPortal>
                                                 <TooltipContent side='left' className='dark:bg-neutral-dark-5 border-0'>
