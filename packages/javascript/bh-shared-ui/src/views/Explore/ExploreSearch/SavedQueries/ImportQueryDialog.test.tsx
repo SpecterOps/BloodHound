029bc14a124e6e2046e6e17bff417ae9b94096ba
Jeff Vacca <jvacca@specterops.io>
2025-09-08 11:38:24 -0700

    BED-4620 - CR - vi import

--- packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/SavedQueries/ImportQueryDialog.test.tsx
+++ packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/SavedQueries/ImportQueryDialog.test.tsx
@@ -13,6 +13,7 @@
 // limitations under the License.
 //
 // SPDX-License-Identifier: Apache-2.0
+import { vi } from 'vitest';
 import { fireEvent, render, screen, waitFor } from '../../../../test-utils';
 import ImportQueryDialog from './ImportQueryDialog';
 
