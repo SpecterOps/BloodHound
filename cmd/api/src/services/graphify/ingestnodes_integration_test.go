de0bd8cef753b272ab9b76d102c6c4bd4cf7f614
Brandon Shearin <bshearin@specterops.io>
2025-09-08 09:53:16 -0400

    fix test tag

--- cmd/api/src/services/graphify/ingestnodes_integration_test.go
+++ cmd/api/src/services/graphify/ingestnodes_integration_test.go
@@ -13,9 +13,7 @@
 // limitations under the License.
 //
 // SPDX-License-Identifier: Apache-2.0
-
-//go:build serial_integration
-// +build serial_integration
+//go:build slow_integration
 
 package graphify_test
 
