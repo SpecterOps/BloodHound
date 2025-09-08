82fccb60584c4c02bea188389cfe47ba2133f56f
Michael Lipka <mvlipka@gmail.com>
2025-09-08 14:20:27 -0500

    BED-6377 just generate & imports fix

--- cmd/api/src/api/v2/auth/etac.go
+++ cmd/api/src/api/v2/auth/etac.go
@@ -1,6 +1,23 @@
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 package auth
 
 import (
+	"errors"
+
 	"github.com/specterops/bloodhound/cmd/api/src/api"
 	v2 "github.com/specterops/bloodhound/cmd/api/src/api/v2"
 	"github.com/specterops/bloodhound/cmd/api/src/auth"
@@ -9,7 +26,7 @@ import (
 
 // handleETACRequest will modify the user passed in to assign an etac list or grant all environment access
 // and will return an error on bad requests
-func handleETACRequest(etacRequest v2.UpdateUserETACListRequest, roles model.Roles, user *model.User, ) error {
+func handleETACRequest(etacRequest v2.UpdateUserETACListRequest, roles model.Roles, user *model.User) error {
 	// Administrators and Power Users may not have an ETAC list applied to them
 	if roles.Has(model.Role{Name: auth.RoleAdministrator}) || roles.Has(model.Role{Name: auth.RolePowerUser}) {
 		return errors.New(api.ErrorResponseETACInvalidRoles)
