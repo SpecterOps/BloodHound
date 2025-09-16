a0570565de8020c7328d4417406da953a9f66a2b
Jeff Vacca <jvacca@specterops.io>
2025-09-16 14:38:16 -0700

    BED-6494 - disable delete for shared queries

--- packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/SavedQueries/QuerySearchFilter.tsx
+++ packages/javascript/bh-shared-ui/src/views/Explore/ExploreSearch/SavedQueries/QuerySearchFilter.tsx
@@ -74,6 +74,7 @@ const QuerySearchFilter = (props: QuerySearchProps) => {
     };
 
     const exportEnabled = selectedQuery?.id ? true : false;
+    const deleteEnabled = selectedQuery?.id && selectedQuery?.canEdit ? true : false;
 
     const importHandler = () => {
         setShowImportDialog(true);
@@ -108,7 +109,7 @@ const QuerySearchFilter = (props: QuerySearchProps) => {
                         </Button>
                         <Button
                             aria-label='delete'
-                            disabled={!exportEnabled}
+                            disabled={!deleteEnabled}
                             className='ml-2'
                             variant='icon'
                             onClick={() => deleteHandler(selectedQuery?.id as number)}>
