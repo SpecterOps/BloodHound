53cf51ec8cfcfd99afb1df02fde08d8d2b39d7fb
Michael Lipka <mvlipka@gmail.com>
2025-09-18 12:59:08 -0500

    BED-6038 generate

--- cmd/api/src/api/middleware/etac_test.go
+++ cmd/api/src/api/middleware/etac_test.go
@@ -1,3 +1,19 @@
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package middleware
 
 import (
@@ -11,7 +27,7 @@ import (
 	"github.com/specterops/bloodhound/cmd/api/src/api"
 	"github.com/specterops/bloodhound/cmd/api/src/auth"
 	"github.com/specterops/bloodhound/cmd/api/src/ctx"
-	dbmocks "github.com/specterops/bloodhound/cmd/api/src/database/mocks"
+	"github.com/specterops/bloodhound/cmd/api/src/database/mocks"
 	"github.com/specterops/bloodhound/cmd/api/src/model"
 	"github.com/specterops/bloodhound/cmd/api/src/model/appcfg"
 	"github.com/stretchr/testify/assert"
@@ -22,7 +38,7 @@ func TestSupportsETACMiddleware(t *testing.T) {
 
 	var (
 		mockCtrl = gomock.NewController(t)
-		mockDB   = dbmocks.NewMockDatabase(mockCtrl)
+		mockDB   = mocks.NewMockDatabase(mockCtrl)
 	)
 
 	defer mockCtrl.Finish()
