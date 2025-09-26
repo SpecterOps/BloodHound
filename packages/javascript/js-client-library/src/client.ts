1c81bab0bf7a8afb7a931e74a18aa25df3fab35a
Catherine Siller <csiller@specterops.io>
2025-09-26 10:44:47 -0500

    chore: create user select environments post update

--- packages/javascript/js-client-library/src/client.ts
+++ packages/javascript/js-client-library/src/client.ts
@@ -1096,7 +1096,7 @@ class BHEAPIClient {
                 needs_password_reset: user.needsPasswordReset,
                 all_environments: user.all_environments,
                 environment_access_control: {
-                    environments: user.environment_access_control?.environments || null,
+                    environments: user.environment_access_control?.environments,
                 },
             },
             options
