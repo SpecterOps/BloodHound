34c676a722de8caf70b09a89f605015a2e69e8bd
Michael Lipka <mlipka@specterops.io>
2025-10-17 14:35:39 -0500

    BED-6604: fix naming inconsistencies with ETAC naming (#1973)
    * BED-6604 fix naming inconsistencies with ETAC naming

--- cmd/api/src/model/etac.go
+++ cmd/api/src/model/etac.go
@@ -15,13 +15,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package model
 
-// EnvironmentAccess defines the model for a row in the environment_access_control table
-type EnvironmentAccess struct {
+// EnvironmentTargetedAccessControl defines the model for a row in the environment_targeted_access_control table
+type EnvironmentTargetedAccessControl struct {
 	UserID        string `json:"user_id"`
 	EnvironmentID string `json:"environment_id"`
 	BigSerial
 }
 
-func (EnvironmentAccess) TableName() string {
-	return "environment_access_control"
+func (EnvironmentTargetedAccessControl) TableName() string {
+	return "environment_targeted_access_control"
 }
