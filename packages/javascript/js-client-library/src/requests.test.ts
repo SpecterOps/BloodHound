65a23f13e32630dd3d04e25e3f43e4c95c4450ba
ykaiboussiSO <ykaiboussi@specterops.io>
2025-10-23 17:17:59 -0500

    fix typescript naming convension

--- packages/javascript/js-client-library/src/requests.test.ts
+++ packages/javascript/js-client-library/src/requests.test.ts
@@ -4,9 +4,9 @@ import { LoginRequest } from './requests';
 
 test('LoginRequest', async () => {
     await allure.step('Type Assertions Validations', async () => {
-        const LoginRequest: LoginRequest = { username: 'jdoe', secret: 'secret', login_method: 'secret', otp: 'opts' };
-        Object.keys(LoginRequest).every(function (key: string) {
-            return expect(typeof LoginRequest[key as keyof LoginRequest]).toBe('string');
+        const loginRequest: LoginRequest = { username: 'jdoe', secret: 'secret', login_method: 'secret', otp: 'opts' };
+        Object.keys(loginRequest).every(function (key: string) {
+            return expect(typeof loginRequest[key as keyof LoginRequest]).toBe('string');
         });
     });
 });
