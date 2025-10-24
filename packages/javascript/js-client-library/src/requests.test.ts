5b70b59065a6b3774514c28738011c648801fafc
ykaiboussiSO <ykaiboussi@specterops.io>
2025-10-24 08:29:27 -0500

    update demo test implementation

--- packages/javascript/js-client-library/src/requests.test.ts
+++ packages/javascript/js-client-library/src/requests.test.ts
@@ -5,8 +5,8 @@ import { LoginRequest } from './requests';
 test('LoginRequest', async () => {
     await allure.step('Type Assertions Validations', async () => {
         const loginRequest: LoginRequest = { username: 'jdoe', secret: 'secret', login_method: 'secret', otp: 'opts' };
-        Object.keys(loginRequest).every(function (key: string) {
-            return expect(typeof loginRequest[key as keyof LoginRequest]).toBe('string');
-        });
+        for (const value of Object.values(loginRequest)) {
+            expect(typeof value).toBe('string');
+        }
     });
 });
