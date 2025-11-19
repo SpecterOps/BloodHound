aced6d6e85665b8417043e4c7fb1ccba7f6f83f6
Elijah Brown <ebrown@specterops.io>
2025-11-19 11:04:12 -0700

    Move db migration to 8.4.5.sql

---
+++ cmd/api/src/database/migration/migrations/v8.5.0.sql
@@ -0,0 +1,29 @@
+-- Copyright 2025 Specter Ops, Inc.
+--
+-- Licensed under the Apache License, Version 2.0
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- SPDX-License-Identifier: Apache-2.0
+
+
+-- OpenGraph graph schema - extensions (collectors)
+CREATE TABLE IF NOT EXISTS schema_extensions (
+    id SERIAL NOT NULL,
+    name TEXT UNIQUE NOT NULL,
+    display_name TEXT NOT NULL,
+    version TEXT NOT NULL,
+    is_builtin BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    PRIMARY KEY (id)
+);
