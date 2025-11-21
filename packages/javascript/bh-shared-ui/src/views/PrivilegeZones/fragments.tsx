dd5a163822c37f783d34a308af5a550cf3212dd2
Michael Smith <msmith@specterops.io>
2025-11-21 09:24:27 -0800

    feedback

--- packages/javascript/bh-shared-ui/src/views/PrivilegeZones/fragments.tsx
+++ packages/javascript/bh-shared-ui/src/views/PrivilegeZones/fragments.tsx
@@ -18,7 +18,7 @@ export const ZonesLink: FC = () => {
             href='https://bloodhound.specterops.io/analyze-data/privilege-zones/zones'
             target='_blank'
             rel='noopener noreferrer'
-            className='text-secondary dark:text-secondary-variant-2 underline'>
+            className='text-link underline'>
             Learn more about zones
         </a>
     );
