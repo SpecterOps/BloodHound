98908594d243a65431969b7be939654f987546e9
Ulises Rangel <urangel@specterops.io>
2025-12-03 16:26:01 -0600

    fix: tier zero glyph rendering when pz ff is disabled BED-6936

--- cmd/ui/src/views/Explore/utils.test.ts
+++ cmd/ui/src/views/Explore/utils.test.ts
@@ -16,7 +16,8 @@
 
 import { Theme } from '@mui/material';
 import * as layoutDagre from 'src/hooks/useLayoutDagre/useLayoutDagre';
-import { initGraph } from './utils';
+import { GlyphLocation } from 'src/rendering/programs/node.glyphs';
+import { getNodeGlyphs, initGraph } from './utils';
 
 const layoutDagreSpy = vi.spyOn(layoutDagre, 'layoutDagre');
 
@@ -24,20 +25,20 @@ const testNodes = {
     '1': {
         label: 'User 1',
         kind: 'User',
-        kinds: ['User'],
+        kinds: ['User', 'Tag_Tier_Zero'],
         objectId: 'test-user-1',
         lastSeen: '',
-        isTierZero: false,
+        isTierZero: true,
         isOwnedObject: false,
     },
     '2': {
         label: 'Group 1',
         kind: 'Group',
-        kinds: ['Group'],
+        kinds: ['Group', 'Tag_Owned'],
         objectId: 'test-group-1',
         lastSeen: '',
         isTierZero: false,
-        isOwnedObject: false,
+        isOwnedObject: true,
     },
 };
 
@@ -72,6 +73,8 @@ const testEdgesWithDuplicate = [
     },
 ];
 
+const pzFeatureFlagEnabled = true;
+
 describe('Explore utils', () => {
     describe('initGraph', () => {
         const mockTheme = {
@@ -90,6 +93,7 @@ describe('Explore utils', () => {
                     customIcons: {},
                     darkMode: false,
                     tagGlyphs: {},
+                    pzFeatureFlagEnabled,
                 }
             );
 
@@ -104,6 +108,7 @@ describe('Explore utils', () => {
                     customIcons: {},
                     darkMode: false,
                     tagGlyphs: {},
+                    pzFeatureFlagEnabled,
                 }
             );
 
@@ -112,5 +117,135 @@ describe('Explore utils', () => {
             expect(graph.hasEdge('2_MemberOf_1')).toBeTruthy();
             expect(graph.hasEdge('1_GetChangesAll_2')).toBeTruthy();
         });
+    });
+});
+
+describe('getNodeGlyphs', () => {
+    const mockTheme = {
+        palette: {
+            color: { primary: '', links: '' },
+            neutral: { primary: '', secondary: '' },
+            common: { black: '', white: '' },
+        },
+    };
+    const themedOptions = {
+        labels: {
+            labelColor: '#1D1B20',
+            backgroundColor: '#F4F4F4',
+            highlightedBackground: '#1A30FF',
+            highlightedText: '#fff',
+        },
+        nodeBorderColor: '#1D1B20',
+        glyph: {
+            colors: {
+                backgroundColor: '#1D1B20',
+                color: '#FFFFFF',
+            },
+        },
+    };
+
+    const standardGlyphs = {
+        TIER_ZERO: 'tierZero',
+        TIER_ZERO_DARK: 'tierZeroDark',
+        OWNED_OBJECT: 'owned',
+        OWNED_OBJECT_DARK: 'ownedDark',
+    };
+
+    const tagGlyphs = { Tag_Tier_Zero: 'gem', owned: 'Tag_Owned', ownedGlyph: 'skull' };
+
+    it('determines glyphs with the tier_managent_engine feature flag disabled', () => {
+        const firstNodeGlyphs = getNodeGlyphs(
+            testNodes[1],
+            {
+                theme: mockTheme as Theme,
+                hideNodes: false,
+                customIcons: {},
+                darkMode: false,
+                themedOptions,
+                tagGlyphs,
+                pzFeatureFlagEnabled: false,
+            },
+            standardGlyphs
+        );
+
+        expect(firstNodeGlyphs).toHaveLength(1);
+
+        const firstNodeGlyph = firstNodeGlyphs[0];
+
+        expect(firstNodeGlyph.location).toBe(GlyphLocation.TOP_RIGHT);
+        expect(firstNodeGlyph.backgroundColor).toBe(themedOptions.glyph.colors.backgroundColor);
+        expect(firstNodeGlyph.color).toBe(themedOptions.glyph.colors.color);
+        expect(firstNodeGlyph.image).toBe('tierZero');
+
+        const secondNodeGlyphs = getNodeGlyphs(
+            testNodes[2],
+            {
+                theme: mockTheme as Theme,
+                hideNodes: false,
+                customIcons: {},
+                darkMode: false,
+                themedOptions,
+                tagGlyphs,
+                pzFeatureFlagEnabled: false,
+            },
+            standardGlyphs
+        );
+
+        expect(secondNodeGlyphs).toHaveLength(1);
+
+        const secondNodeGlyph = secondNodeGlyphs[0];
+
+        expect(secondNodeGlyph.location).toBe(GlyphLocation.BOTTOM_RIGHT);
+        expect(secondNodeGlyph.backgroundColor).toBe(themedOptions.glyph.colors.backgroundColor);
+        expect(secondNodeGlyph.color).toBe(themedOptions.glyph.colors.color);
+        expect(secondNodeGlyph.image).toBe('owned');
+    });
+
+    it('determines glyphs with the tier_managent_engine feature flag enabled', () => {
+        const firstNodeGlyphs = getNodeGlyphs(
+            testNodes[1],
+            {
+                theme: mockTheme as Theme,
+                hideNodes: false,
+                customIcons: {},
+                darkMode: false,
+                themedOptions,
+                tagGlyphs,
+                pzFeatureFlagEnabled: true,
+            },
+            standardGlyphs
+        );
+
+        expect(firstNodeGlyphs).toHaveLength(1);
+
+        const firstNodeGlyph = firstNodeGlyphs[0];
+
+        expect(firstNodeGlyph.location).toBe(GlyphLocation.TOP_RIGHT);
+        expect(firstNodeGlyph.backgroundColor).toBe(themedOptions.glyph.colors.backgroundColor);
+        expect(firstNodeGlyph.color).toBe(themedOptions.glyph.colors.color);
+        expect(firstNodeGlyph.image).toBe('gem');
+
+        const secondNodeGlyphs = getNodeGlyphs(
+            testNodes[2],
+            {
+                theme: mockTheme as Theme,
+                hideNodes: false,
+                customIcons: {},
+                darkMode: false,
+                themedOptions,
+                tagGlyphs,
+                pzFeatureFlagEnabled: true,
+            },
+            standardGlyphs
+        );
+
+        expect(secondNodeGlyphs).toHaveLength(1);
+
+        const secondNodeGlyph = secondNodeGlyphs[0];
+
+        expect(secondNodeGlyph.location).toBe(GlyphLocation.BOTTOM_RIGHT);
+        expect(secondNodeGlyph.backgroundColor).toBe(themedOptions.glyph.colors.backgroundColor);
+        expect(secondNodeGlyph.color).toBe(themedOptions.glyph.colors.color);
+        expect(secondNodeGlyph.image).toBe('skull');
     });
 });
