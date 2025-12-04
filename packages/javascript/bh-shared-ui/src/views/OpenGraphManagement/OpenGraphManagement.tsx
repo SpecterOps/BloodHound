0a1a4b4db796ee969cd58264e2032faeec91c19b
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2025-12-04 15:14:19 -0800

    saving work

--- packages/javascript/bh-shared-ui/src/views/OpenGraphManagement/OpenGraphManagement.tsx
+++ packages/javascript/bh-shared-ui/src/views/OpenGraphManagement/OpenGraphManagement.tsx
@@ -1,4 +1,4 @@
-import { PageWithTitle } from '../..';
+import PageWithTitle from '../../components/PageWithTitle';
 import { SchemaUploadCard } from './SchemaUploadCard';
 
 export const OpenGraphManagement: React.FC = () => {
@@ -6,7 +6,7 @@ export const OpenGraphManagement: React.FC = () => {
         <PageWithTitle
             title='OpenGraph Management'
             pageDescription={
-                <p>
+                <p className='text-sm'>
                     OpenGraph Management provides a centralized space to define and maintain the structures that shape
                     how BloodHound understands relationships in an environment. Review schema examples in the OpenGraph
                     Library to discover effective modeling patterns.
--- packages/javascript/bh-shared-ui/src/views/OpenGraphManagement/SchemaUploadCard.tsx
+++ packages/javascript/bh-shared-ui/src/views/OpenGraphManagement/SchemaUploadCard.tsx
@@ -1,6 +1,6 @@
 export const SchemaUploadCard = () => {
     return (
-        <div className='p-4 border rounded-lg bg-neutral-2 border-transparent shadow-outer-1'>
+        <div className='p-4 border rounded-lg bg-neutral-2 border-neutral-5 mt-4'>
             <h2 className='font-bold text-lg'>Custom Schema Upload</h2>
             <p>
                 Upload custom schema JSON files to introduce new node and edge types. Then apply and validate schema
