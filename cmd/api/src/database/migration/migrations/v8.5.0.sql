b3a374abb3d1034fcadacd99423bb4832ed4011a
Elijah Brown <ebrown@specterops.io>
2025-12-11 16:44:05 -0700

    Add comment to db migration

--- cmd/api/src/database/migration/migrations/v8.5.0.sql
+++ cmd/api/src/database/migration/migrations/v8.5.0.sql
@@ -145,7 +145,7 @@ CREATE TABLE IF NOT EXISTS schema_environments_principal_kinds (
 CREATE INDEX idx_schema_environments_principal_kinds_principal_kind ON schema_environments_principal_kinds (principal_kind);
 
 
-
+-- Added to report warnings for opengraph files that attempt to create invalid relationships. 
 ALTER TABLE ingest_jobs
     ADD COLUMN IF NOT EXISTS partial_failed_files integer DEFAULT 0;
 
