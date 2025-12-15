528f95feece1ba35f89b2b1f3f160846eac3b4dd
Francisco Quintero Abbo <fquintero@specterops.io>
2025-12-15 13:25:48 -0500

    chore: reverted a change that was not needed for data-testid

--- packages/javascript/bh-shared-ui/src/views/PrivilegeZones/Details/SelectedDetails.tsx
+++ packages/javascript/bh-shared-ui/src/views/PrivilegeZones/Details/SelectedDetails.tsx
@@ -37,7 +37,7 @@ export const SelectedDetails: FC = () => {
             type: memberQuery.data.primary_kind as EntityKinds,
         };
         return (
-            <div className='h-full' data-testid='selected-details-object-panel'>
+            <div className='h-full'>
                 <EntityInfoPanel
                     DataTable={EntityInfoDataTable}
                     selectedNode={selectedNode}
