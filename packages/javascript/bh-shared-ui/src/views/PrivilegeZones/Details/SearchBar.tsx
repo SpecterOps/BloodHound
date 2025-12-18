96989fa6507dde827062f1ea9bb9d26a8dbef780
Dani Cairns <dcairns@specterops.io>
2025-12-17 17:43:04 -0800

    remove autofocus

--- packages/javascript/bh-shared-ui/src/views/PrivilegeZones/Details/SearchBar.tsx
+++ packages/javascript/bh-shared-ui/src/views/PrivilegeZones/Details/SearchBar.tsx
@@ -110,7 +110,6 @@ const SearchBar: React.FC<{ showTags?: boolean }> = ({ showTags = true }) => {
                     <div className='flex items-center'>
                         <AppIcon.MagnifyingGlass className='-mr-4' />
                         <Input
-                            autoFocus
                             variant={'underlined'}
                             placeholder='Search'
                             className='pl-8'
