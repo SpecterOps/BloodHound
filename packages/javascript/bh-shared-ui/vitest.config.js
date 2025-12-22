48c0709fd864a53a570996dee8693514dd34f11a
Catherine Siller <csiller@specterops.io>
2025-12-22 13:45:39 -0600

    chore: update yml with status on pull request

--- packages/javascript/bh-shared-ui/vitest.config.js
+++ packages/javascript/bh-shared-ui/vitest.config.js
@@ -35,6 +35,12 @@ export default defineConfig({
             reportOnFailure: true, // report coverage even if fails
             reporter: ['text', 'json', 'json-summary', 'html'],
             exclude: ['**/types/**', '**/constants/**', 'dist', '**/components/HelpTexts/**'],
+            thresholds: {
+                lines: 60,
+                functions: 60,
+                branches: 60,
+                statements: 60,
+            },
         },
         reporters: [
             'default',
