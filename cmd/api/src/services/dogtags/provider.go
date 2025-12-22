437128ebb35b433735a42797bc59906c2c1acca5
kpom <kpomeroy@specterops.io>
2025-12-22 10:02:44 -0600

    Licenses

--- cmd/api/src/services/dogtags/provider.go
+++ cmd/api/src/services/dogtags/provider.go
@@ -1,9 +1,24 @@
+// Copyright 2025 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
 package dogtags
 
 import "errors"
 
-// ErrNotFound is returned when a dogtag key doesn't exist in the provider
-var ProviderNotImplemented = errors.New("no-op provider not implemented")
+// ErrProviderNotImplemented is returned by the no-op provider
+var ErrProviderNotImplemented = errors.New("no-op provider not implemented")
 
 // Provider is the interface for dogtag backends.
 // Providers are simple key-value stores - they don't know about defaults.
@@ -23,13 +38,13 @@ func NewNoopProvider() *NoopProvider {
 }
 
 func (p *NoopProvider) GetFlagAsBool(key string) (bool, error) {
-	return false, ProviderNotImplemented
+	return false, ErrProviderNotImplemented
 }
 
 func (p *NoopProvider) GetFlagAsString(key string) (string, error) {
-	return "", ProviderNotImplemented
+	return "", ErrProviderNotImplemented
 }
 
 func (p *NoopProvider) GetFlagAsInt(key string) (int64, error) {
-	return 0, ProviderNotImplemented
+	return 0, ErrProviderNotImplemented
 }
