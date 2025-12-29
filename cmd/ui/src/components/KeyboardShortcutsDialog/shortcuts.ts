1950a3d9b470da150bc0e3e19d75d6f068f89777
Dani Cairns <dcairns@specterops.io>
2025-12-29 14:53:20 -0800

    update shortcuts

--- cmd/ui/src/components/KeyboardShortcutsDialog/shortcuts.ts
+++ cmd/ui/src/components/KeyboardShortcutsDialog/shortcuts.ts
@@ -17,21 +17,21 @@ export type ShortCutsMap = Record<string, string[][]>;
 
 export const EXPLORE_SHORTCUTS = {
     'Explore Page': [
-        ['S', 'Jump to Node Search'],
+        ['N', 'Jump to Node Search'],
         ['P', 'Jump to Pathfinding'],
         ['C', 'Focus Cypher Query Editor'],
-        ['Shift + S', 'Save Current Query'],
+        ['S', 'Save Current Query'],
         ['R', 'Run Current Cypher Query'],
         ['/', 'Search Current Nodes'],
         ['T', 'Toggle Table View'],
         ['I', 'Toggle Node Info Panel'],
-        ['Shift + G', 'Reset Graph View'],
+        ['G', 'Reset Graph View'],
     ],
 };
 
 export const GLOBAL_SHORTCUTS = {
     Global: [
-        ['<integer>', 'Navigate sidebar pages'],
+        ['[1-5]', 'Navigate sidebar pages'],
         ['H', 'Launch keyboard shortcuts list'],
         ['U', 'Launch File Upload dialog'],
         ['M', 'Toggle Dark Mode'],
