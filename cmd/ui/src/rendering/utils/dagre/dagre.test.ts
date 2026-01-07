1506535b20115547e1ebd716002b7f4164781ae4
Wes <169498386+wes-mil@users.noreply.github.com>
2026-01-07 18:20:40 -0500

    BED-6923: fix dagre test

--- cmd/ui/src/rendering/utils/dagre/dagre.test.ts
+++ cmd/ui/src/rendering/utils/dagre/dagre.test.ts
@@ -14,7 +14,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import dagre from 'dagrejs';
+import dagre from '@dagrejs/dagre';
 import Graph from 'graphology';
 import { NODE_DEFAULT_SIZE, applyNodePositionsFromGraphlibGraph, copySigmaNodesToGraphlibGraph } from './';
 
