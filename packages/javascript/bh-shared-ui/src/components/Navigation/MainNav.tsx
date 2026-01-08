33f72b5e39df9f4ee42f252a4166e69b099d5836
Ulises Rangel <urangel@specterops.io>
2026-01-08 16:29:20 -0600

    fix: discard params for pz nav bar navigation

--- packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.tsx
+++ packages/javascript/bh-shared-ui/src/components/Navigation/MainNav.tsx
@@ -17,6 +17,7 @@
 import { FC, ReactNode } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useApiVersion, useIsMouseDragging } from '../../hooks';
+import { privilegeZonesPath } from '../../routes';
 import { cn } from '../../utils';
 import { adaptClickHandlerToKeyDown } from '../../utils/adaptClickHandlerToKeyDown';
 import { AppLink } from './AppLink';
@@ -108,10 +109,15 @@ const MainNavItemLink: FC<{
     allowHover: boolean;
     testId: string;
 }> = ({ route, children, allowHover, testId }) => {
+    // PZ pages need to discard environment query params so that all zone objects are counted
+    // Some Objects do not have an environmentId (domain sid or tenant id) and as such even using the "all" environments param does not capture everything
+    const discardQueryParams = route.includes(privilegeZonesPath) ? true : false;
+
     return (
         // Note: The w-full is to avoid the hover area to overflow out of the nav when its collapsed
         <AppLink
             to={{ pathname: route }}
+            discardQueryParams={discardQueryParams}
             className={cn('h-10 w-auto flex items-center gap-x-2 hover:underline cursor-default', {
                 'group-hover:w-full cursor-pointer': allowHover,
             })}
