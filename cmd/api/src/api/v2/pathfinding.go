1d32dfa595ddbb905af94c695ce08ee24d24d984
Michael Lipka <mvlipka@gmail.com>
2026-01-28 11:29:52 -0600

    BED-7183 fix GetShortestPath ETAC

--- cmd/api/src/api/v2/pathfinding.go
+++ cmd/api/src/api/v2/pathfinding.go
@@ -209,14 +209,7 @@ func (s Resources) GetShortestPath(response http.ResponseWriter, request *http.R
 			slog.Error("Unable to get user from auth context")
 			api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusInternalServerError, "unknown user", request), response)
 		} else {
-			shouldFilterETAC, err := ShouldFilterForETAC(s.DogTags, user)
-			if err != nil {
-				slog.Error("Unable to check ETAC filtering")
-				api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusInternalServerError, "error determining if ETAC should filter", request), response)
-				return
-			} else {
-				writeShortestPathsResult(paths, shouldFilterETAC, user, response, request)
-			}
+			writeShortestPathsResult(paths, ShouldFilterForETAC(s.DogTags, user), user, response, request)
 		}
 	}
 }
