e84df82cb2a8e696267d990d77f6a8774a9604d2
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2026-01-28 14:49:20 -0800

    feat(UI): OpenGraph Edge Filtering: BED-6725 (#2291)
    * loading custom edges into entity panel
    
    * wire up OG edges to query params for deep linking
    
    * adding edge type comparison test
    
    * updated tests and added feature flag
    
    * saving work
    
    * update logic for handling empty filters
    
    * added more tests and comments
    
    * cleanup and updating comments
    
    * run pfc
    
    * remove symmetricDifference method
    
    * remove unnecessary formatting
    
    * updated tests from CR feedback
    
    * address pr feedback
    
    * updated imports

--- packages/javascript/bh-shared-ui/src/views/Explore/EdgeInfo/EdgeObjectInformation.test.tsx
+++ packages/javascript/bh-shared-ui/src/views/Explore/EdgeInfo/EdgeObjectInformation.test.tsx
@@ -15,8 +15,8 @@
 // SPDX-License-Identifier: Apache-2.0
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
-import { SelectedEdge } from '../../../edgeTypes';
 import { render, screen } from '../../../test-utils';
+import { SelectedEdge } from '../ExploreSearch/EdgeFilter/edgeCategories';
 import { ObjectInfoPanelContextProvider } from '../providers';
 import EdgeObjectInformation from './EdgeObjectInformation';
 
