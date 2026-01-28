792d04309e0a584d82b3ea7d216c7d682f9c7ee3
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2026-01-28 11:15:10 -0800

    additional comments

--- packages/javascript/bh-shared-ui/src/mocks/stderr.ts
+++ packages/javascript/bh-shared-ui/src/mocks/stderr.ts
@@ -5,7 +5,8 @@ type ErrorSilencer = {
 
 /**
  * Returns a set of helper functions for silencing/restoring console.error output. Can be used for tests where an error is
- * expected to keep logging clean.
+ * expected to keep logging clean. If `silence()` is called without later calling `restore()`, error logging may be disabled for additional
+ * tests.
  */
 export const errorSilencer = (): ErrorSilencer => {
     let originalError: typeof console.error | null = null;
@@ -26,6 +27,10 @@ export const errorSilencer = (): ErrorSilencer => {
     };
 };
 
+/**
+ * Can be used to wrap any test logic for which an error is expected, ensuring that the cleanup function is always called after the
+ * callback is executed.
+ */
 export const withoutErrorLogging = async <T>(cb: () => T | Promise<T>): Promise<T> => {
     const silencer = errorSilencer();
     silencer.silence();
