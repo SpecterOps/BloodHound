acfc8b80ec8391a50a28621550f3ada4c96f3830
Catherine Siller <csiller@specterops.io>
2026-01-30 12:40:09 -0600

    chore(ui): move code up

--- packages/javascript/js-client-library/src/utils/config.ts
+++ packages/javascript/js-client-library/src/utils/config.ts
@@ -14,62 +14,175 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-    ConfigurationPayload,
-    parseAPITokensConfiguration,
-    parseTieringConfiguration,
-    parseTimeoutLimitConfiguration,
-    RequestOptions,
-} from 'js-client-library';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { apiClient } from '../utils';
+import { ConfigurationWithMetadata, GetConfigurationResponse } from '../responses';
 
-export const configurationKeys = {
-    all: ['configuration'] as const,
+/* 
+A collection of types and helper functions for working with values from the config endpoint.
+
+Intended Usage:
+
+const { data } = useGetConfiguration();                   // get data from query as <GetConfigurationResponse | undefined>
+const neo4jConfig = parseNeo4jConfiguration(data);        // narrow to <ConfigurationWithMetadata<Neo4jConfiguration> | undefined>
+
+if (neo4jConfig) {
+    console.log(neo4jConfig.key)                          // ok
+    console.log(neo4jConfig.value.batch_write_size)       // ok
+    console.log(neo4jConfig.value.enabled)                // type error
+}
+*/
+export enum ConfigurationKey {
+    PasswordExpiration = 'auth.password_expiration_window',
+    Neo4j = 'neo4j.configuration',
+    Citrix = 'analysis.citrix_rdp_support',
+    Reconciliation = 'analysis.reconciliation',
+    PruneTTL = 'prune.ttl',
+    Tiering = 'analysis.tiering',
+    TimeoutLimit = 'api.timeout_limit',
+    APITokens = 'auth.api_tokens',
+}
+
+export type PasswordExpirationConfiguration = {
+    key: ConfigurationKey.PasswordExpiration;
+    value: {
+        duration: string;
+    };
 };
 
-const getConfiguration = (options?: RequestOptions) => {
-    return apiClient.getConfiguration(options).then((res) => res.data);
+export type Neo4jConfiguration = {
+    key: ConfigurationKey.Neo4j;
+    value: {
+        batch_write_size: number;
+        write_flush_size: number;
+    };
 };
 
-export const useGetConfiguration = () => {
-    return useQuery(configurationKeys.all, ({ signal }) => getConfiguration({ signal }), {
-        refetchOnWindowFocus: false,
-    });
+export type CitrixConfiguration = {
+    key: ConfigurationKey.Citrix;
+    value: {
+        enabled: boolean;
+    };
 };
 
-export const usePrivilegeZoneAnalysis = () => {
-    const { data, isLoading } = useGetConfiguration();
-    const tieringConfig = parseTieringConfiguration(data);
-    const privilegeZoneAnalysisEnabled = tieringConfig?.value.multi_tier_analysis_enabled;
-
-    return isLoading ? undefined : privilegeZoneAnalysisEnabled;
+export type ReconciliationConfiguration = {
+    key: ConfigurationKey.Reconciliation;
+    value: {
+        enabled: boolean;
+    };
 };
 
-export const useAPITokensConfiguration = () => {
-    const { data } = useGetConfiguration();
-    const apiTokensConfig = parseAPITokensConfiguration(data)?.value.enabled;
-
-    return apiTokensConfig;
+export type TieringConfiguration = {
+    key: ConfigurationKey.Tiering;
+    value: {
+        tier_limit: number;
+        label_limit: number;
+        multi_tier_analysis_enabled: boolean;
+    };
 };
 
-export const useTimeoutLimitConfiguration = () => {
-    const { data } = useGetConfiguration();
-    const timeoutLimitConfig = parseTimeoutLimitConfiguration(data)?.value.enabled;
-
-    return timeoutLimitConfig;
+export type PruneTTLConfiguration = {
+    key: ConfigurationKey.PruneTTL;
+    value: {
+        has_session_edge_ttl: string;
+        base_ttl: string;
+    };
 };
 
-const updateConfiguration = (payload: ConfigurationPayload, options?: RequestOptions) => {
-    return apiClient.updateConfiguration(payload, options).then((res) => res.data);
+export type TimeoutLimitConfiguration = {
+    key: ConfigurationKey.TimeoutLimit;
+    value: {
+        enabled: boolean;
+    };
 };
 
-export const useUpdateConfiguration = () => {
-    const queryClient = useQueryClient();
+export type APITokensConfiguration = {
+    key: ConfigurationKey.APITokens;
+    value: {
+        enabled: boolean;
+    };
+};
 
-    return useMutation(updateConfiguration, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(configurationKeys.all);
-        },
-    });
+export type ConfigurationPayload =
+    | PasswordExpirationConfiguration
+    | Neo4jConfiguration
+    | CitrixConfiguration
+    | ReconciliationConfiguration
+    | PruneTTLConfiguration
+    | TieringConfiguration
+    | TimeoutLimitConfiguration
+    | APITokensConfiguration;
+
+export const getConfigurationFromKey = (config: GetConfigurationResponse | undefined, key: ConfigurationKey) => {
+    return config?.data.find((c) => c.key === key);
+};
+
+export const parsePasswordExpirationConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<PasswordExpirationConfiguration> | undefined => {
+    const key = ConfigurationKey.PasswordExpiration;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseNeo4jConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<Neo4jConfiguration> | undefined => {
+    const key = ConfigurationKey.Neo4j;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseCitrixConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<CitrixConfiguration> | undefined => {
+    const key = ConfigurationKey.Citrix;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseReconciliationConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<ReconciliationConfiguration> | undefined => {
+    const key = ConfigurationKey.Reconciliation;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parsePruneTTLConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<PruneTTLConfiguration> | undefined => {
+    const key = ConfigurationKey.PruneTTL;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseTieringConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<TieringConfiguration> | undefined => {
+    const key = ConfigurationKey.Tiering;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseTimeoutLimitConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<TimeoutLimitConfiguration> | undefined => {
+    const key = ConfigurationKey.TimeoutLimit;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
+};
+
+export const parseAPITokensConfiguration = (
+    response: GetConfigurationResponse | undefined
+): ConfigurationWithMetadata<APITokensConfiguration> | undefined => {
+    const key = ConfigurationKey.APITokens;
+    const config = getConfigurationFromKey(response, key);
+
+    return config?.key === key ? config : undefined;
 };
