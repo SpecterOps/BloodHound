73c29bafbd121b44dc4060db69c2f7e091d11b83
Stephanie Lamb <slamb@specterops.io>
2026-01-30 10:06:48 -0600

    added check for apikeys being disabled for token creation

--- cmd/api/src/model/appcfg/parameter.go
+++ cmd/api/src/model/appcfg/parameter.go
@@ -540,7 +540,7 @@ func GetTimeoutLimitParameter(ctx context.Context, service ParameterService) boo
 }
 
 type APITokensParameter struct {
-	Enabled bool `json:"enabled,omitempty"`
+	Enabled bool `json:"enabled"`
 }
 
 func GetAPITokensParameter(ctx context.Context, service ParameterService) bool {
