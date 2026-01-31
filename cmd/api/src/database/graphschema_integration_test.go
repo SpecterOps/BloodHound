5422e1714fd0f612749efbbc0c1378098220676e
Lawson Willard <46655228+LawsonWillard@users.noreply.github.com>
2026-01-30 22:25:38 -0700

    just prepare-for-codereview

--- cmd/api/src/database/graphschema_integration_test.go
+++ cmd/api/src/database/graphschema_integration_test.go
@@ -1477,7 +1477,7 @@ func TestDatabase_GetSchemaEnvironmentByGraphSchemaExtensionId(t *testing.T) {
 			},
 			teardown: func(t *testing.T, extensionId int32) {
 				t.Helper()
-				var err error = testSuite.BHDatabase.DeleteGraphSchemaExtension(testSuite.Context, extensionId)
+				var err = testSuite.BHDatabase.DeleteGraphSchemaExtension(testSuite.Context, extensionId)
 				require.NoError(t, err)
 			},
 		},
@@ -1543,7 +1543,7 @@ func TestDatabase_GetSchemaEnvironmentByGraphSchemaExtensionId(t *testing.T) {
 			},
 			teardown: func(t *testing.T, extensionId int32) {
 				t.Helper()
-				var err error = testSuite.BHDatabase.DeleteGraphSchemaExtension(testSuite.Context, extensionId)
+				var err = testSuite.BHDatabase.DeleteGraphSchemaExtension(testSuite.Context, extensionId)
 				require.NoError(t, err)
 			},
 		},
