ecae846975d8e84e676b09a9830c169d74567e52
Michael Lipka <mvlipka@gmail.com>
2026-02-23 15:52:50 -0600

    BED-7403 slog lint

--- cmd/api/src/api/v2/azure.go
+++ cmd/api/src/api/v2/azure.go
@@ -426,7 +426,7 @@ func (s *Resources) GetAZEntity(response http.ResponseWriter, request *http.Requ
 	if objectID := queryVars.Get(objectIDQueryParameterName); objectID == "" {
 		api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusBadRequest, fmt.Sprintf("query parameter %s is required", objectIDQueryParameterName), request), response)
 	} else if azKind, err := azEntityParamToKind(entityType); err != nil {
-		slog.WarnContext(request.Context(), "Could not determine AZ type from entityType request var", "type", entityType, "err", err)
+		slog.WarnContext(request.Context(), "Could not determine AZ type from entityType request var", slog.String("type", entityType), attr.Error(err))
 		api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusBadRequest, api.ErrorResponseDetailsBadQueryParameterFilters, request), response)
 	} else if hasAccess, err := CheckUserHasAccessToNodeById(request.Context(), s.DB, s.GraphQuery, s.DogTags, user, objectID, azKind); err != nil {
 		slog.ErrorContext(request.Context(), "Error checking if user has access to node for ETAC", attr.Error(err))
