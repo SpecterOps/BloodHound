41403a0e3bc1f45bd9a4657e3fb9a28a7add2884
Raymond Laubert <rlaubert@specterops.io>
2026-02-26 10:46:10 -0500

    feat: Resolved comments regarding APITokenExpiration ExpirationPeriod validation and return. BED-7448

--- cmd/api/src/model/appcfg/parameter.go
+++ cmd/api/src/model/appcfg/parameter.go
@@ -585,7 +585,7 @@ func GetEnvironmentTargetedAccessControlParameters(ctx context.Context, service 
 
 type APITokenExpirationParameter struct {
 	Enabled 		 bool `json:"enabled"`
-	ExpirationPeriod int  `json:"expiration_period"`
+	ExpirationPeriod int  `json:"expiration_period" validate:"min=1,max=365"`
 }
 
 func GetAPITokenExpirationParameter(ctx context.Context, service ParameterService) APITokenExpirationParameter {
@@ -597,6 +597,11 @@ func GetAPITokenExpirationParameter(ctx context.Context, service ParameterServic
 		slog.WarnContext(ctx, "Invalid API tokens expiration configuration supplied, returning default values.",
 			slog.String("invalid_configuration", err.Error()),
 			slog.String("parameter_key", string(APITokenExpiration)))
+	} else if result.ExpirationPeriod <= 0 || result.ExpirationPeriod > 365 {
+		slog.WarnContext(ctx, "Invalid API token expiration period supplied, returning default values.",
+			slog.Int("invalid_expiration_period", result.ExpirationPeriod),
+			slog.String("parameter_key", string(APITokenExpiration)))
+		result.ExpirationPeriod = 90
 	}
 
 	return result
