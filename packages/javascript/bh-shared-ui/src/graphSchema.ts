eb26cd368c593b569069f31486f927247e14708f
Francisco Quintero Abbo <fquintero@specterops.io>
2026-03-04 12:14:06 -0500

    fix: removed unnecessary changes

--- packages/javascript/bh-shared-ui/src/graphSchema.ts
+++ packages/javascript/bh-shared-ui/src/graphSchema.ts
@@ -917,7 +917,6 @@ export enum AzureNodeKind {
     VMScaleSet = 'AZVMScaleSet',
     App = 'AZApp',
     Role = 'AZRole',
-    EligibleRole = 'AZEligibleRole',
     Device = 'AZDevice',
     FunctionApp = 'AZFunctionApp',
     Group = 'AZGroup',
@@ -946,8 +945,6 @@ export function AzureNodeKindToDisplay(value: AzureNodeKind): string | undefined
             return 'App';
         case AzureNodeKind.Role:
             return 'Role';
-        case AzureNodeKind.EligibleRole:
-            return 'EligibleRole';
         case AzureNodeKind.Device:
             return 'Device';
         case AzureNodeKind.FunctionApp:
