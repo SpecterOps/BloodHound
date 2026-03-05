5ecff433f0f71e5479f3d630c804ece42dda1817
Raymond Laubert <rlaubert@specterops.io>
2026-03-05 10:33:29 -0500

    feat: Modified comment formatting to re-push so CLA will trigger for PR. BED-7449

--- cmd/api/src/database/migration/migrations/v8.8.0.sql
+++ cmd/api/src/database/migration/migrations/v8.8.0.sql
@@ -74,6 +74,6 @@ CREATE TABLE IF NOT EXISTS schema_findings_subtypes (
   PRIMARY KEY(schema_finding_id, subtype)
 );
 
--- Update the 'auth_tokens' Table adding Expiration Column
+-- Update the 'auth_tokens' table adding expiration column
 ALTER TABLE auth_tokens
 ADD COLUMN IF NOT EXISTS expires_at timestamp with time zone;
