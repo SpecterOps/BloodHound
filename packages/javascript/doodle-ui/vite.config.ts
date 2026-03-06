a75369d81845c0a4064e47d728b6149df4b8980a
Wesley Maffly-Kipp <wmaffly-kipp@specterops.io>
2026-03-05 16:39:08 -0800

    remove hoisting limits and separate vendor chunks

--- packages/javascript/doodle-ui/vite.config.ts
+++ packages/javascript/doodle-ui/vite.config.ts
@@ -3,16 +3,10 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), dts({ rollupTypes: true, exclude: ['**/*.stories.{ts,tsx}'] })],
-    resolve: {
-        alias: {
-            components: resolve(__dirname, 'src/components'),
-        },
-    },
     build: {
         lib: {
             entry: resolve(__dirname, 'src/index.ts'),
@@ -20,11 +14,6 @@ export default defineConfig({
         },
         rollupOptions: {
             external: ['react', 'react-dom', 'react/jsx-runtime', 'tailwindcss'],
-            output: {
-                manualChunks: {
-                    vendor: Object.keys(packageJson.dependencies),
-                },
-            },
         },
     },
     test: {
