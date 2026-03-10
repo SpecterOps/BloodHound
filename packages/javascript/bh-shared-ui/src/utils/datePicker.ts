577a579f01c4e335388e4e7286e6ed4d29b665fc
Nicole Martin <n.martinpdx@gmail.com>
2026-03-10 16:14:12 -0700

    feat: adds FUTURE_END_TIME error constant message

--- packages/javascript/bh-shared-ui/src/utils/datePicker.ts
+++ packages/javascript/bh-shared-ui/src/utils/datePicker.ts
@@ -17,6 +17,7 @@ export enum CustomRangeError {
     INVALID_DATE = 'Must be a valid date in yyyy-mm-dd format.',
     INVALID_RANGE_START = 'Start date must be before end date.',
     INVALID_RANGE_END = 'End date must be after start date.',
+    FUTURE_END_TIME = 'End time must be before current time.',
 }
 
 export const START_TIME = 'start-time' as const;
