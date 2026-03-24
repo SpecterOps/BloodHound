c6f9537d04f2d151211a88c5e7a498c848dadeeb
mistahj67 <26472282+mistahj67@users.noreply.github.com>
2026-03-24 10:53:15 -0700

    fix: missing og env key in PZ property parsing BED-7687 (#2517)

--- cmd/api/src/queries/graph.go
+++ cmd/api/src/queries/graph.go
@@ -39,7 +39,6 @@ import (
 	"github.com/specterops/bloodhound/cmd/api/src/database"
 	"github.com/specterops/bloodhound/cmd/api/src/model"
 	"github.com/specterops/bloodhound/cmd/api/src/services/agi"
-	"github.com/specterops/bloodhound/cmd/api/src/services/graphify"
 	"github.com/specterops/bloodhound/cmd/api/src/utils"
 	"github.com/specterops/bloodhound/packages/go/analysis"
 	"github.com/specterops/bloodhound/packages/go/bhlog/attr"
@@ -632,7 +631,7 @@ func filterNodesToSearchResult(validPrimaryKinds graphschema.ValidPrimaryKinds, 
 				} else {
 					nodeId = id
 				}
-			} else if id, err := node.Properties.Get(graphify.EnvironmentIDKey).String(); err != nil {
+			} else if id, err := node.Properties.Get(graphschema.EnvironmentIDKey).String(); err != nil {
 				continue
 			} else {
 				nodeId = id
