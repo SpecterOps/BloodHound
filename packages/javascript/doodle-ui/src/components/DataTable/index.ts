2699f90f30baf5ce94f3d40944a2bd14c42641dc
Jeff Vacca <jvacca@specterops.io>
2026-03-26 13:19:59 -0700

    BED-7000 - add explicit export for pinDialog component

--- packages/javascript/doodle-ui/src/components/DataTable/index.ts
+++ packages/javascript/doodle-ui/src/components/DataTable/index.ts
@@ -14,4 +14,5 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 export { createColumnHelper, flexRender, type ColumnDef } from '@tanstack/react-table';
+export * from './components/PinDialog';
 export * from './DataTable';
