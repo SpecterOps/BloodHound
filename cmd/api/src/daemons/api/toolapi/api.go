3cf15c6b3b1ede110026388f34ec9b574b1a5e55
Elijah Brown <ebrown@specterops.io>
2026-03-30 13:19:00 -0700

    chore: pfc

--- cmd/api/src/daemons/api/toolapi/api.go
+++ cmd/api/src/daemons/api/toolapi/api.go
@@ -33,8 +33,8 @@ import (
 	"github.com/specterops/bloodhound/cmd/api/src/bootstrap"
 	"github.com/specterops/bloodhound/cmd/api/src/config"
 	"github.com/specterops/bloodhound/cmd/api/src/database"
-	"github.com/specterops/dawgs/graph"
 	"github.com/specterops/bloodhound/packages/go/bhlog/attr"
+	"github.com/specterops/dawgs/graph"
 )
 
 // Daemon holds data relevant to the tools API daemon
