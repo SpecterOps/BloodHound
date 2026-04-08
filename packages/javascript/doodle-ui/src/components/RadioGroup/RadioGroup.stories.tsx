2affcef52860add938aad1d5a226c322137803e6
Jeff Vacca <jvacca@specterops.io>
2026-04-08 11:56:15 -0700

    BED-7863 - port to fresh branch, focus ring

---
+++ packages/javascript/doodle-ui/src/components/RadioGroup/RadioGroup.stories.tsx
@@ -0,0 +1,50 @@
+// Copyright 2026 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+import type { Meta, StoryObj } from '@storybook/react';
+import { RadioGroup, RadioItem } from './RadioGroup';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+    title: 'Components/RadioGroup',
+    component: RadioGroup,
+    parameters: {
+        layout: 'centered',
+    },
+    // This story will not appear in Storybook's sidebar or docs page: https://storybook.js.org/docs/writing-stories/tags
+    tags: ['autodocs'],
+    // More on argTypes: https://storybook.js.org/docs/api/argtypes
+    argTypes: {},
+    args: {},
+} satisfies Meta<typeof RadioGroup>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Story: Story = {
+    args: {},
+    render: () => {
+        return (
+            <div>
+                <RadioGroup>
+                    <RadioItem value='a' label='Value a' />
+                    <RadioItem value='b' label='Value b' />
+                    <RadioItem value='c' label='Value c' />
+                    <RadioItem value='d' label='Disabled' disabled />
+                </RadioGroup>
+            </div>
+        );
+    },
+};
---
+++ packages/javascript/doodle-ui/src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,68 @@
+// Copyright 2026 Specter Ops, Inc.
+//
+// Licensed under the Apache License, Version 2.0
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
+import React from 'react';
+import { Label } from '../Label/Label';
+import { cn } from '../utils';
+
+interface RadioGroupProps extends React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root> {}
+
+/**
+ * Description for RadioGroup
+ */
+
+const RadioGroup = React.forwardRef<React.ElementRef<typeof RadioGroupPrimitive.Root>, RadioGroupProps>(
+    ({ className, children, ...props }, ref) => (
+        <RadioGroupPrimitive.Root ref={ref} className={cn(className)} {...props}>
+            {children}
+        </RadioGroupPrimitive.Root>
+    )
+);
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName;
+
+interface RadioGroupItemProps extends React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item> {
+    label: string;
+    value: string;
+}
+
+// const indicatorStyle = 'w-4 h-4 rounded rounded-full border';
+// const focusStyles = 'outline outline-2 -outline-offset-2 outline-primary';
+
+const RadioItem = React.forwardRef<React.ElementRef<typeof RadioGroupPrimitive.Item>, RadioGroupItemProps>(
+    ({ className, label, value, ...props }, ref) => (
+        <div className='flex items-center mb-1 px-1 rounded-md focus-within:outline focus-within:outline-2 focus-within:outline-secondary'>
+            <RadioGroupPrimitive.Item
+                value={value}
+                ref={ref}
+                className={cn(
+                    'w-4 h-4 rounded rounded-full border border-neutral-5 dark:border-neutral-light-5 hover:border-secondary dark:hover:border-secondary-variant-2 active:border-primary-variant mr-3 relative data-[disabled]:border-neutral-4 data-[disabled]:bg-neutral-2 peer',
+                    className
+                )}
+                {...props}>
+                <RadioGroupPrimitive.Indicator
+                    className={cn(
+                        'bg-primary dark:bg-secondary-variant-2 w-2 h-2 rounded rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+                    )}
+                />
+            </RadioGroupPrimitive.Item>
+            <Label className='font-normal'>{label}</Label>
+        </div>
+    )
+);
+RadioItem.displayName = RadioGroupPrimitive.Item.displayName;
+
+export { RadioGroup, RadioItem };
