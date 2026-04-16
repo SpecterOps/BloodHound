65401b4944f50a85da880a6d6aef0134d356baf0
Alyx Holms <aholms@specterops.io>
2026-04-16 15:15:01 -0700

    feat: add remaining PFC checks and lightweight PR builds BED-6802 (#2239)
    * feat: remaining PFC checks encoded as workflow
    
    * fix: try again
    
    * fix: missing license check and handle no tags
    
    * fix: fetch depth 2 to allow for diff-index to work
    
    * fix: better diff handling
    
    * fix: formatting
    
    * fix: show all the problems
    
    * chore: try to understand why things are off the rails
    
    * fix: remove file that regenerates only on pipelines
    
    * fix: ignore changes to file, rather than ignoring file for license check
    
    * fix: reset mockServiceWorker.js
    
    * chore: promote workflow to actual triggers
    
    * chore: fixups and new PR build workflow
    
    * chore: CR feedback
    
    * IMPORTANT: revert this commit, only used for testing
    
    * fix: incorrect action config
    
    * Revert "IMPORTANT: revert this commit, only used for testing"
    
    This reverts commit a73793df6808f25d6bb7dc94e2f1e9a747a2c868.
    
    * chore: CR fixes
    
    * chore: cr fixes
    
    * chore+wip: PR feedback
    
    * chore: better slow go workflow because it's breaking in main
    
    * chore: remove parallel because it tends to cause sporadic DB errors

--- cmd/api/src/daemons/changelog/changelog_integration_test.go
+++ cmd/api/src/daemons/changelog/changelog_integration_test.go
@@ -144,8 +144,6 @@ func (s *changelogHarness) assertNodesExistInDB(objectIDs []string, expectedCoun
 }
 
 func TestChangelogIntegration(t *testing.T) {
-	t.Parallel()
-
 	t.Run("coordinator and flag manager startup coordination", func(t *testing.T) {
 		harness := setupChangelogTest(t, defaultTestConfig())
 		defer harness.close()
@@ -233,8 +231,6 @@ func TestChangelogIntegration(t *testing.T) {
 }
 
 func TestCacheClearFunctionality(t *testing.T) {
-	t.Parallel()
-
 	t.Run("ClearCache clears active cache", func(t *testing.T) {
 		harness := setupChangelogTest(t, defaultTestConfig())
 		defer harness.close()
