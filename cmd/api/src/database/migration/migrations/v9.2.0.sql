bafed90b2f7ba3a52fdf11568c293d37134d41b6
sirisjo <ssjoboen@specterops.io>
2026-04-22 12:43:12 -0700

    add defaults to message and data

--- cmd/api/src/database/migration/migrations/v9.2.0.sql
+++ cmd/api/src/database/migration/migrations/v9.2.0.sql
@@ -18,8 +18,8 @@
 CREATE TABLE IF NOT EXISTS events (
   id TEXT PRIMARY KEY NOT NULL,
   type TEXT NOT NULL,
-  message TEXT NOT NULL,
-  data JSONB NOT NULL,
+  message TEXT NOT NULL DEFAULT '',
+  data JSONB NOT NULL DEFAULT '{}'::JSONB,
   created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
   processed_at timestamp with time zone DEFAULT NULL
 );
