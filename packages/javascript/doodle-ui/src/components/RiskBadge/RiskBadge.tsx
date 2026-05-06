992ef8e4c85f7fec1dee63de6600118f943a7f58
Leah Miller <lmiller@specterops.io>
2026-05-06 12:24:27 -0700

    chore:BED-7671 bandaid fix for RiskBadge storybook

--- packages/javascript/doodle-ui/src/components/RiskBadge/RiskBadge.tsx
+++ packages/javascript/doodle-ui/src/components/RiskBadge/RiskBadge.tsx
@@ -36,7 +36,7 @@ const RiskBadgeContentVariants = cva('rounded-full', {
         },
         type: {
             labeled: 'size-max px-6 py-2 border-none text-center',
-            'sm-labeled': 'size-max px-1 py-1 border-none text-center font-bold min-w-14 w-14',
+            'sm-labeled': 'size-max px-1 py-1 border-none text-center font-bold min-w-14 w-14 text-sm',
             'sm-circle': 'size-full shadow-inner1xl',
             'md-circle': 'size-full shadow-inner1xl',
         },
