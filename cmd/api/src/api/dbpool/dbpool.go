5a8bca23237c1fcf41088abbd02441f71a442a15
Stephanie Lamb <slamb@specterops.io>
2026-05-22 11:51:35 -0500

    refactored dbpool

--- cmd/api/src/api/dbpool/dbpool.go
+++ cmd/api/src/api/dbpool/dbpool.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/specterops/bloodhound/cmd/api/src/config"
 	"github.com/specterops/dawgs/drivers/pg"
@@ -14,19 +13,27 @@ const (
 	poolInitConnectionTimeout = time.Second * 10
 )
 
+func getConnectionString(cfg config.DatabaseConfiguration) string {
+	if cfg.EnableRDSIAMAuth {
+		// Only enable the BeforeConnect handler if RDS IAM Auth is enabled
+		cfg.Endpoint = cfg.LookupEndpoint()
+	}
+	return cfg.PostgreSQLConnectionString()
+}
+
 func NewDawgsPool(cfg config.DatabaseConfiguration) (*pgxpool.Pool, error) {
-	return newPool(cfg, true)
+	return pg.NewPool(getConnectionString(cfg))
 }
 
 func NewAppPool(cfg config.DatabaseConfiguration) (*pgxpool.Pool, error) {
-	return newPool(cfg, false)
+	return newPool(getConnectionString(cfg))
 }
 
-func newPool(cfg config.DatabaseConfiguration, enableGraphHooks bool) (*pgxpool.Pool, error) {
+func newPool(connString string) (*pgxpool.Pool, error) {
 	poolCtx, done := context.WithTimeout(context.Background(), poolInitConnectionTimeout)
 	defer done()
 
-	poolCfg, err := pgxpool.ParseConfig(cfg.PostgreSQLConnectionString())
+	poolCfg, err := pgxpool.ParseConfig(connString)
 	if err != nil {
 		return nil, err
 	}
@@ -35,32 +42,5 @@ func newPool(cfg config.DatabaseConfiguration, enableGraphHooks bool) (*pgxpool.
 	poolCfg.MinConns = 5
 	poolCfg.MaxConns = 50
 
-	if enableGraphHooks {
-		// Bind functions to the AfterConnect and AfterRelease hooks to ensure that composite type registration occurs.
-		// Without composite type registration, the pgx connection type will not be able to marshal PG OIDs to their
-		// respective Golang structs.
-		poolCfg.AfterConnect = pg.AfterPooledConnectionEstablished
-		poolCfg.AfterRelease = pg.AfterPooledConnectionRelease
-	}
-
-	if cfg.EnableRDSIAMAuth {
-		// Only enable the BeforeConnect handler if RDS IAM Auth is enabled
-		cfg.Endpoint = cfg.LookupEndpoint()
-		poolCfg.BeforeConnect = func(ctx context.Context, connCfg *pgx.ConnConfig) error {
-			if newPoolCfg, err := pgxpool.ParseConfig(cfg.PostgreSQLConnectionString()); err != nil {
-				return err
-			} else {
-				connCfg.Password = newPoolCfg.ConnConfig.Password
-			}
-
-			return nil
-		}
-	}
-
-	pool, err := pgxpool.NewWithConfig(poolCtx, poolCfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return pool, nil
+	return pgxpool.NewWithConfig(poolCtx, poolCfg)
 }
