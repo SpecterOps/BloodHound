92fb3210be19a95655d791170d80f123bb84ac57
Raymond Laubert <rlaubert@specterops.io>
2026-06-10 14:14:15 -0400

    feat: Created DB migration that adds the GraphDBReadIngest to the permissions and roles. BED-8519

---
+++ cmd/api/src/database/migration/migrations/20260610180916_v9_update_role_permissions_for_file_ingest_sql.sql
@@ -0,0 +1,34 @@
+-- +goose Up
+INSERT INTO permissions (authority, name, created_at, updated_at)
+VALUES ('graphdb',
+        'ReadIngest',
+        current_timestamp,
+        current_timestamp)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO roles_permissions (role_id, permission_id)
+SELECT r.id, p.id
+FROM roles r
+JOIN permissions p
+    ON (
+      (r.name IN ('Auditor') AND (p.authority, p.name) IN (
+            ('graphdb', 'ReadIngest')
+      ))
+)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO roles_permissions (role_id, permission_id)
+SELECT r.role_id, p.id
+FROM roles_permissions r
+JOIN permissions f
+    ON f.id = r.permission_id
+JOIN permissions p
+    ON (p.authority, p.name) = ('graphdb', 'ReadIngest')
+WHERE (f.authority, f.name) = ('graphdb', 'Ingest')
+ON CONFLICT DO NOTHING;
+
+-- +goose Down
+DELETE FROM roles_permissions
+WHERE permission_id = (SELECT id FROM permissions WHERE authority = 'graphdb' AND name = 'ReadIngest');
+
+DELETE FROM permissions WHERE authority = 'graphdb' AND name = 'ReadIngest';
