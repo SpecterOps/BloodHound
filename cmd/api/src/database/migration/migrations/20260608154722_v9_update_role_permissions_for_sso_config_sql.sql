48fbd21d3116024b75a181ca6f331b2f33309319
Raymond Laubert <rlaubert@specterops.io>
2026-06-10 12:41:16 -0400

    feat: Adding new permission AuthReadProviders for the Administrator and Auditor roles. BED-8518

---
+++ cmd/api/src/database/migration/migrations/20260608154722_v9_update_role_permissions_for_sso_config_sql.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+INSERT INTO permissions (authority, name, created_at, updated_at)
+VALUES ('auth',
+        'ReadProviders',
+        current_timestamp,
+        current_timestamp)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO roles_permissions (role_id, permission_id)
+SELECT r.id, p.id
+FROM roles r
+JOIN permissions p
+    ON (
+      (r.name IN ('Administrator', 'Auditor') AND (p.authority, p.name) IN (
+            ('auth', 'ReadProviders')
+      ))
+)
+ON CONFLICT DO NOTHING;
+
+-- +goose Down
+DELETE FROM roles_permissions
+WHERE permission_id = (SELECT id FROM permissions WHERE authority = 'auth' AND name = 'ReadProviders');
+
+DELETE FROM permissions WHERE authority = 'auth' AND name = 'ReadProviders';
