3b1cdf7ca959ebf0d0de43375384db14eb8a2f16
Lawson Willard <46655228+LawsonWillard@users.noreply.github.com>
2026-06-18 12:49:06 -0700

    add opengraph entity panel feature flag

---
+++ cmd/api/src/database/migration/migrations/20260618122642_v9_add_opengraph_entity_panel_feature_flag.sql
@@ -0,0 +1,6 @@
+-- +goose Up
+INSERT INTO feature_flags (created_at, updated_at, key, name, description, enabled, user_updatable)
+VALUES (current_timestamp, current_timestamp, 'opengraph_entity_panel', 'OpenGraph Entity Panel', 'Enable/Disable Customized Entity Panel Support for OpenGraph Extensions', false, true);
+
+-- +goose Down
+DELETE FROM feature_flags WHERE key = 'opengraph_entity_panel';
