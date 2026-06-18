bb845089dbabb26aa7c1c817f2a0b83aecd304b4
Alyx Holms <aholms@specterops.io>
2026-06-18 09:31:16 -0700

    chore: step 7 routes complete

--- server/appcfg/internal/routes/routes.go
+++ server/appcfg/internal/routes/routes.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/specterops/bloodhound/cmd/api/src/api/router"
+	"github.com/specterops/bloodhound/server/appcfg/internal/handlers"
+)
+
+func Register(routerInst *router.Router, handlers *handlers.Handlers) {
+	routerInst.GET("/api/v2/datapipe/status", handlers.GetDatapipeStatus).RequireAuth()
+}
