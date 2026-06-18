5faa8441c45ccc84669039c431103a3747c0b9c7
Lawson Willard <46655228+LawsonWillard@users.noreply.github.com>
2026-06-18 13:18:43 -0700

    update flag to not be user updatable

--- cmd/api/src/database/migration/migrations/20260618122642_v9_add_opengraph_entity_panel_feature_flag.sql
+++ cmd/api/src/database/migration/migrations/20260618122642_v9_add_opengraph_entity_panel_feature_flag.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 INSERT INTO feature_flags (created_at, updated_at, key, name, description, enabled, user_updatable)
-VALUES (current_timestamp, current_timestamp, 'opengraph_entity_panel', 'OpenGraph Entity Panel', 'Enable/Disable Customized Entity Panel Support for OpenGraph Extensions', false, true);
+VALUES (current_timestamp, current_timestamp, 'opengraph_entity_panel', 'OpenGraph Entity Panel', 'Enable/Disable Customized Entity Panel Support for OpenGraph Extensions', false, false);
 
 -- +goose Down
 DELETE FROM feature_flags WHERE key = 'opengraph_entity_panel';
