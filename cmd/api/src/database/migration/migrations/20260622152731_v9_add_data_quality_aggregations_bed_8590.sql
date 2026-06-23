eace9f4b7489e8f9d10d7c33bc8a5ea94ad8d765
Berretta <cberretta@specterops.io>
2026-06-23 09:56:15 -0700

    remove indexes and update kind_id

--- cmd/api/src/database/migration/migrations/20260622152731_v9_add_data_quality_aggregations_bed_8590.sql
+++ cmd/api/src/database/migration/migrations/20260622152731_v9_add_data_quality_aggregations_bed_8590.sql
@@ -23,15 +23,13 @@ CREATE TABLE IF NOT EXISTS data_quality_aggregations (
     metric_type TEXT NOT NULL,
     metric_name TEXT NOT NULL,
     metric_value NUMERIC NOT NULL DEFAULT 0,
-    kind_id INTEGER DEFAULT NULL REFERENCES kind(id),
+    kind_id INTEGER DEFAULT NULL REFERENCES kind(id) ON DELETE SET NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
     );
 
-CREATE INDEX IF NOT EXISTS idx_data_quality_aggregations_run_id ON data_quality_aggregations USING btree (run_id);
 CREATE INDEX IF NOT EXISTS idx_data_quality_aggregations_created_at ON data_quality_aggregations USING btree (created_at);
-CREATE INDEX IF NOT EXISTS idx_data_quality_aggregations_updated_at ON data_quality_aggregations USING btree (updated_at);
 
 -- +goose Down
 DROP TABLE IF EXISTS data_quality_aggregations;
