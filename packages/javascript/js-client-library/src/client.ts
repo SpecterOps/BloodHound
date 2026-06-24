bff3cf3469eff170e870fe4113cb52ad3224cf4a
Catherine Siller <csiller@specterops.io>
2026-06-24 15:31:42 -0500

    chore: update endpoints

--- packages/javascript/js-client-library/src/client.ts
+++ packages/javascript/js-client-library/src/client.ts
@@ -776,9 +776,6 @@ class BHEAPIClient {
     requestSupportBundle = (clientId: string, type: string, options?: RequestOptions) =>
         this.baseClient.post(`/api/v2/clients/${clientId}/management`, { type }, options);
 
-    getSupportBundleArtifacts = (clientId: string, options?: RequestOptions) =>
-        this.baseClient.get(`/api/v2/clients/${clientId}/artifacts`, options);
-
     getSupportBundleArtifactStatus = (clientId: string, artifactId: string, options?: RequestOptions) =>
         this.baseClient.get(`/api/v2/clients/${clientId}/artifacts/${artifactId}/status`, options);
 
