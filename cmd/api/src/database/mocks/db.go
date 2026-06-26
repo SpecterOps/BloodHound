562518a08deffe6708b791156c897901dcb0b65c
Benjamin Sheth <71798265+bsheth711@users.noreply.github.com>
2026-06-26 12:19:31 -0500

    chore: add analysis request steps BED-8276 (#2922)
    * feat: add analysis request steps BED-8276
    
    * code review
    
    * code rabbit suggestion

--- cmd/api/src/database/mocks/db.go
+++ cmd/api/src/database/mocks/db.go
@@ -2980,17 +2980,17 @@ func (mr *MockDatabaseMockRecorder) RegisterSourceKind(ctx any) *gomock.Call {
 }
 
 // RequestAnalysis mocks base method.
-func (m *MockDatabase) RequestAnalysis(ctx context.Context, requester string) error {
+func (m *MockDatabase) RequestAnalysis(ctx context.Context, requester string, analysisMode model.AnalysisMode) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "RequestAnalysis", ctx, requester)
+	ret := m.ctrl.Call(m, "RequestAnalysis", ctx, requester, analysisMode)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
 // RequestAnalysis indicates an expected call of RequestAnalysis.
-func (mr *MockDatabaseMockRecorder) RequestAnalysis(ctx, requester any) *gomock.Call {
+func (mr *MockDatabaseMockRecorder) RequestAnalysis(ctx, requester, analysisMode any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RequestAnalysis", reflect.TypeOf((*MockDatabase)(nil).RequestAnalysis), ctx, requester)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RequestAnalysis", reflect.TypeOf((*MockDatabase)(nil).RequestAnalysis), ctx, requester, analysisMode)
 }
 
 // RequestCollectedGraphDataDeletion mocks base method.
