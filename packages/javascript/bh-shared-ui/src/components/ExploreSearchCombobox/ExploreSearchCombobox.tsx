846270bcd37921998dbf52ad5580d7c020077d41
Dani Cairns <dcairns@specterops.io>
2026-07-14 14:26:53 -0700

    use default TextField if react-hook-form absent

--- packages/javascript/bh-shared-ui/src/components/ExploreSearchCombobox/ExploreSearchCombobox.tsx
+++ packages/javascript/bh-shared-ui/src/components/ExploreSearchCombobox/ExploreSearchCombobox.tsx
@@ -14,12 +14,12 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import { List, ListItem, ListItemText, Paper, TextFieldVariants } from '@mui/material';
+import { List, ListItem, ListItemText, Paper, TextField, TextFieldVariants } from '@mui/material';
 import { FormControl, FormField, FormItem, FormMessage, Input } from 'doodle-ui';
 import { useCombobox } from 'downshift';
 import { useRef } from 'react';
 import { Control } from 'react-hook-form';
-import { SearchResult, getEmptyResultsText, useKeywordAndTypeValues, useSearch } from '../../hooks';
+import { SearchResult, getEmptyResultsText, useKeywordAndTypeValues, useSearch, useTheme } from '../../hooks';
 import { SearchValue } from '../../views/Explore/ExploreSearch/types';
 import { RuleFormInputs } from '../../views/PrivilegeZones/Save/RuleForm/types';
 import NodeIcon from '../NodeIcon';
@@ -47,6 +47,7 @@ const ExploreSearchCombobox: React.FC<{
     control,
 }) => {
     const searchNodesRef = useRef<HTMLInputElement>();
+    const theme = useTheme();
 
     const { keyword, type } = useKeywordAndTypeValues(inputValue);
     const { data, error, isError, isLoading, isFetching } = useSearch(keyword, type);
@@ -86,38 +87,65 @@ const ExploreSearchCombobox: React.FC<{
 
     return (
         <div style={{ position: 'relative' }}>
-            <FormField
-                control={control}
-                name='seeds'
-                render={({ field }) => (
-                    <FormItem>
-                        <FormControl>
-                            <Input
-                                {...field}
-                                ref={(node: HTMLInputElement) => {
-                                    downshiftInputProps.inputRef(node);
-                                    searchNodesRef.current = node;
-                                }}
-                                aria-label={labelText}
-                                name={labelText}
-                                placeholder={labelText}
-                                variant={variant}
-                                size='small'
-                                fullWidth
-                                disabled={disabled}
-                                type='text'
-                                autoComplete='off'
-                                className='pl-2 border-[1px] border-primary rounded-md text-sm dark:bg-neutral-dark-1'
-                                startAdornment={selectedItem?.type && <NodeIcon nodeType={selectedItem?.type} />}
-                                autoFocus={autoFocus}
-                                data-testid='explore_search_input-search'
-                                {...downshiftInputProps}
-                            />
-                        </FormControl>
-                        <FormMessage />
-                    </FormItem>
-                )}
-            />
+            {!control ? (
+                <TextField
+                    placeholder={labelText}
+                    variant={variant}
+                    size='small'
+                    fullWidth
+                    disabled={disabled}
+                    inputProps={{
+                        'aria-label': labelText,
+                    }}
+                    InputProps={{
+                        style: {
+                            backgroundColor: disabled ? theme.neutral.tertiary : 'inherit',
+                            fontSize: '0.875rem',
+                        },
+                        autoFocus,
+                        startAdornment: selectedItem?.type && <NodeIcon nodeType={selectedItem?.type} />,
+                    }}
+                    {...downshiftInputProps}
+                    inputRef={(node) => {
+                        downshiftInputProps.inputRef(node);
+                        searchNodesRef.current = node;
+                    }}
+                    data-testid='explore_search_input-search'
+                />
+            ) : (
+                <FormField
+                    control={control}
+                    name='seeds'
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    {...field}
+                                    ref={(node: HTMLInputElement) => {
+                                        downshiftInputProps.inputRef(node);
+                                        searchNodesRef.current = node;
+                                    }}
+                                    aria-label={labelText}
+                                    name={labelText}
+                                    placeholder={labelText}
+                                    variant={variant}
+                                    size='small'
+                                    fullWidth
+                                    disabled={disabled}
+                                    type='text'
+                                    autoComplete='off'
+                                    className='pl-2 border-[1px] border-primary rounded-md text-sm dark:bg-neutral-dark-1'
+                                    startAdornment={selectedItem?.type && <NodeIcon nodeType={selectedItem?.type} />}
+                                    autoFocus={autoFocus}
+                                    data-testid='explore_search_input-search'
+                                    {...downshiftInputProps}
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+            )}
             <div
                 style={{
                     position: 'absolute',
