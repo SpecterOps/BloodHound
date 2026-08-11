0e76c669b6a49d770766cc86c53b86ddeef3acc2
Stephanie Lamb <slamb@specterops.io>
2026-08-11 13:50:14 -0500

    updated query builder to match exact rows

--- server/identity/internal/appdb/appdb.go
+++ server/identity/internal/appdb/appdb.go
@@ -99,7 +99,7 @@ func (s *Store) GetRole(ctx context.Context, id int32) (services.Role, error) {
 		err      error
 	)
 
-	roleSB.Select("*").From(tableRoles).Where(roleSB.Equal("id", id)).Limit(1)
+	roleSB.Select("id", "name", "description", "created_at", "updated_at").From(tableRoles).Where(roleSB.Equal("id", id)).Limit(1)
 	roleQuery, roleArgs := roleSB.Build()
 
 	roleRows, err = s.db.Query(ctx, roleQuery, roleArgs...)
@@ -290,7 +290,7 @@ func (s *Store) ListRoles(ctx context.Context, queryFilters params.Filters, sort
 		err               error
 	)
 
-	roleSB.Select("*").From(tableRoles)
+	roleSB.Select("id", "name", "description", "created_at", "updated_at").From(tableRoles)
 
 	if err = applyRoleFilters(roleSB, queryFilters); err != nil {
 		return nil, err
@@ -339,7 +339,7 @@ func (s *Store) ListRoles(ctx context.Context, queryFilters params.Filters, sort
 
 func (s *Store) GetPermission(ctx context.Context, id int) (services.Permission, error) {
 	sb := sqlbuilder.PostgreSQL.NewSelectBuilder()
-	sb.Select("*")
+	sb.Select("id", "authority", "name", "created_at", "updated_at")
 	sb.From(tablePermissions)
 	sb.Where(sb.Equal("id", id))
 	sb.Limit(1)
