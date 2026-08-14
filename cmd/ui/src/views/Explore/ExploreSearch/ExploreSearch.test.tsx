1d9b07bbbcadff97d5c40ce557a5c49c72498671
Dani Cairns <dcairns@specterops.io>
2026-08-14 15:44:00 -0700

    fix expect calls

--- cmd/ui/src/views/Explore/ExploreSearch/ExploreSearch.test.tsx
+++ cmd/ui/src/views/Explore/ExploreSearch/ExploreSearch.test.tsx
@@ -312,7 +312,7 @@ describe('ExploreSearch handling of cypher query responses', () => {
 
         expect(await screen.findByRole('button', { name: /run cypher query/i })).not.toBeDisabled();
 
-        expect(await screen.findByTestId('cypher-search-section').toBeInTheDocument());
+        expect(await screen.findByTestId('cypher-search-section')).toBeInTheDocument();
         expect(window.location.search).not.toContain('selectedItem=');
     });
 
@@ -320,7 +320,7 @@ describe('ExploreSearch handling of cypher query responses', () => {
         mockCypherEndpoint(singleNodeGraphResponse);
         const { screen } = await setupCypherSearch();
 
-        expect((await screen.findByTestId('cypher-search-section')).not.toBeInTheDocument());
+        expect(await screen.findByTestId('cypher-search-section')).not.toBeInTheDocument();
     });
 
     it('clears the selected item and closes the search widget when the query returns multiple nodes', async () => {
@@ -328,6 +328,6 @@ describe('ExploreSearch handling of cypher query responses', () => {
         const { screen } = await setupCypherSearch('&selectedItem=999');
 
         expect(window.location.search).not.toContain('selectedItem=');
-        expect((await screen.findByTestId('cypher-search-section')).not.toBeInTheDocument());
+        expect(await screen.findByTestId('cypher-search-section')).not.toBeInTheDocument();
     });
 });
