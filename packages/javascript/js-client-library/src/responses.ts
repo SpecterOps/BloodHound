ed3dd41e2f0741f94e4dc4771adf3fcd48ccb732
Dani Cairns <dcairns@specterops.io>
2026-08-17 10:19:35 -0700

    fix typing

--- packages/javascript/js-client-library/src/responses.ts
+++ packages/javascript/js-client-library/src/responses.ts
@@ -429,7 +429,7 @@ export type Extension = {
 
 export type GetExtensionsResponse = BasicResponse<{ extensions: Extension[] }>;
 
-export type FindingTypeResponse = BasicResponse<{ finding: string; title: string }>;
+export type FindingTypeResponse = BasicResponse<{ finding: string; title: string }[]>;
 
 export type FindingSchema = {
     id: number;
