39d039488faf5735a68f1d72cb36a4714727d711
Jonas Bülow Knudsen <12843299+JonasBK@users.noreply.github.com>
2026-08-20 15:26:52 +0200

    Update AdcsEsc14ScenarioA.tsx

--- packages/javascript/bh-shared-ui/src/components/HelpTexts/AdcsEsc14ScenarioA/AdcsEsc14ScenarioA.tsx
+++ packages/javascript/bh-shared-ui/src/components/HelpTexts/AdcsEsc14ScenarioA/AdcsEsc14ScenarioA.tsx
@@ -197,7 +197,7 @@ export const AdcsEsc14ScenarioAWindows: FC = () => {
                 another PKI provider used in the target environment.
             </Typography>
 
-            <Typography variant='body1'> Execution </Typography>
+            <Typography variant='body1' className='font-medium'>Execution</Typography>
 
             <Typography variant='body2'>
                 Obtain a certificate meeting the above requirements for example by dumping a certificate from a
