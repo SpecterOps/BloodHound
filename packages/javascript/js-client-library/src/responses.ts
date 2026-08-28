513cb16398c16271884a021cec65a834d995c3b4
Lucas Parra <lparra@specterops.io>
2026-08-28 16:44:41 -0400

    rabbit suggestion

--- packages/javascript/js-client-library/src/responses.ts
+++ packages/javascript/js-client-library/src/responses.ts
@@ -493,7 +493,7 @@ export type UnifiedFinding = {
     status: string;
     first_seen: string;
     last_seen: string;
-    prioritization_rank?: number;
+    prioritization_rank?: number | null;
 };
 
 export type UnifiedFindingResponse = PaginatedResponse<UnifiedFinding[]>;
