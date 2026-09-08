ffd3d69eb8caccf40f31f59c7658944c13a91c85
Brandon Faulkner <bfaulkner@specterops.io>
2026-09-08 12:19:03 -0400

    fix: remove unused type imports from client.ts

--- packages/javascript/js-client-library/src/client.ts
+++ packages/javascript/js-client-library/src/client.ts
@@ -14,8 +14,6 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-import type { CollectorJob, CollectorJobHistory, CollectorJobProfile, CollectorJobSchedule } from './types';
-
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import {
     ClearDatabaseRequest,
