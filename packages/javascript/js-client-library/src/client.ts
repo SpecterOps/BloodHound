b0866a59bd7969f4960bc01f7c00763ae54a3683
Jeff Vacca <jvacca@specterops.io>
2026-09-09 09:07:11 -0700

    BED-9495 - wire up retry button (#3262)

--- packages/javascript/js-client-library/src/client.ts
+++ packages/javascript/js-client-library/src/client.ts
@@ -16,6 +16,7 @@
 
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
 import {
+    AlertRetryRequest,
     ClearDatabaseRequest,
     CreateAlertRequest,
     CreateAssetGroupRequest,
@@ -118,6 +119,7 @@ import {
     PostureHistoryResponse,
     PostureResponse,
     PreviewSelectorsResponse,
+    RetryAlertAttemptResponse,
     RotateWebhookSecretResponse,
     RunCollectorJobProfileResponse,
     SavedQuery,
@@ -2946,6 +2948,9 @@ class BHEAPIClient {
             },
             paramsSerializer: { indexes: null },
         });
+
+    retryAlertAttempt = (payload: AlertRetryRequest, options?: RequestOptions) =>
+        this.baseClient.post<RetryAlertAttemptResponse>('api/v2/alert-attempts/retry', payload, options);
 }
 
 export default BHEAPIClient;
