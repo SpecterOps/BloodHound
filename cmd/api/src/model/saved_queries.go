ba593034d38cc86d8f0ee70468d57db66206ff4c
AlyaEru <eschwab@specterops.io>
2026-09-17 18:12:08 -0400

    feat: propagating extension_id/category fields from DB to API response

--- cmd/api/src/model/saved_queries.go
+++ cmd/api/src/model/saved_queries.go
@@ -25,8 +25,8 @@ type SavedQuery struct {
 	Name              string  `json:"name" gorm:"index:,unique,composite:compositeIndex"`
 	Query             string  `json:"query"`
 	Description       string  `json:"description"`
-	Category          string  `json:"-" gorm:"column:category"`
-	SchemaExtensionID *int32  `json:"-" gorm:"column:schema_extension_id;index:,unique,composite:compositeIndex;index:,unique,composite:extensionQueryKey"`
+	Category          string  `json:"category,omitempty" gorm:"column:category"`
+	SchemaExtensionID *int32  `json:"extension_id,omitempty" gorm:"column:schema_extension_id;index:,unique,composite:compositeIndex;index:,unique,composite:extensionQueryKey"`
 	QueryKey          *string `json:"-" gorm:"column:query_key;index:,unique,composite:extensionQueryKey"`
 	BigSerial
 }
