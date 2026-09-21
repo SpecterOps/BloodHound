df2289c39448b179c7ba2d9c07bfcb1345cfdda5
Ibra <iuseinovski@specterops.io>
2026-09-21 11:50:38 -0500

    Remove extra user check

--- cmd/api/src/api/v2/edge.go
+++ cmd/api/src/api/v2/edge.go
@@ -148,8 +148,6 @@ func (s *Resources) GetEdgeACLInheritancePath(response http.ResponseWriter, requ
 		api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusInternalServerError, fmt.Sprintf("Error getting ACL inheritance path for edge: %v", err), request), response)
 	} else if primaryDisplayKinds, err := s.DB.GetPrimaryDisplayKinds(request.Context()); err != nil {
 		api.HandleDatabaseError(request, response, err)
-	} else if user, isUser := auth.GetUserFromAuthCtx(bhctx.FromRequest(request).AuthCtx); !isUser {
-		api.WriteErrorResponse(request.Context(), api.BuildErrorResponse(http.StatusForbidden, "unknown user", request), response)
 	} else {
 		unifiedGraph := model.NewUnifiedGraph()
 		unifiedGraph.AddPathSet(primaryDisplayKinds, pathSet, true)
